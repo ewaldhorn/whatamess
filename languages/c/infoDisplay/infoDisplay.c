943729da1e5a98cf9279ba12dfb3e8fbdff0ae65
Ewald Horn <ewaldhorn@gmail.com>
2023-05-26 12:50:50 +0200

    Some simple C loops

--- languages/c/infoDisplay/infoDisplay.c
+++ languages/c/infoDisplay/infoDisplay.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+void boxIt() {
+    for (int i = 0; i < 20; i++) {
+        printf("=");
+    }
+    
+    printf("\n");
+    printf("=");
+
+    for (int i = 0; i < 18; i++) {
+        printf(" ");
+    }
+
+    printf("=");
+    printf("\n");
+
+    for (int i = 0; i < 20; i++) {
+        printf("=");
+    }
+
+    printf("\n");
+}
+
 int main(void)
 {
     printf("C Info Display Experiments\n");
-
+    boxIt();
     return 0;
 }
