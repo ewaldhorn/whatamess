bff8dd2e8dbc7ce6619a730438c729cc25945f6a
Ewald Horn <ewaldhorn@gmail.com>
2023-05-26 11:58:39 +0200

    Working with State in Go

--- languages/go/learning/state_pattern/main.go
+++ languages/go/learning/state_pattern/main.go
@@ -29,6 +29,30 @@ func (h *HasMoneyState) dispenseItem() {
 	fmt.Println("Item dispensed")
 }
 
-func main() {
+type VendingMachine struct {
+	CurrentState state
+}
 
+func (v *VendingMachine) setState(s state) {
+	v.CurrentState = s
+}
+
+func (v *VendingMachine) insertMoney() {
+	v.CurrentState.insertMoney()
+}
+
+func (v *VendingMachine) dispenseItem() {
+	v.CurrentState.dispenseItem()
+}
+
+func main() {
+	vendingMachine := &VendingMachine{CurrentState: &NoMoneyState{}}
+
+	vendingMachine.insertMoney()
+	vendingMachine.dispenseItem()
+
+	vendingMachine.setState(&HasMoneyState{})
+
+	vendingMachine.insertMoney()
+	vendingMachine.dispenseItem()
 }
