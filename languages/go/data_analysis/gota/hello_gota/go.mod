429e6d5a7fe57c9a18dcc305d6c848430d2d814b
Ewald Horn <ewaldhorn@gmail.com>
2023-05-29 16:38:18 +0200

    Now looking at dataframes

--- languages/go/data_analysis/gota/hello_gota/go.mod
+++ languages/go/data_analysis/gota/hello_gota/go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require github.com/go-gota/gota v0.12.0
 
-require gonum.org/v1/gonum v0.9.1 // indirect
+require (
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	gonum.org/v1/gonum v0.9.1 // indirect
+)
--- languages/go/data_analysis/gota/hello_gota/main.go
+++ languages/go/data_analysis/gota/hello_gota/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/go-gota/gota/dataframe"
 	"github.com/go-gota/gota/series"
 )
 
 func main() {
+	basicSeriesExample()
+	createDataFrameFromSeries()
+}
+
+func basicSeriesExample() {
 	fmt.Println(series.New([]string{"z", "y", "d", "e"}, series.String, "col"))
 
 	a := map[string]series.Type{
@@ -15,4 +21,15 @@ func main() {
 	}
 
 	fmt.Println(a)
+}
+
+func createDataFrameFromSeries() {
+	df := dataframe.New(
+		series.New([]string{"a", "b", "c", "d", "e"}, series.String, "alphas"),
+		series.New([]int{5, 4, 2, 3, 1}, series.Int, "numbers"),
+		series.New([]string{"a1", "b2", "c3", "d4", "e5"}, series.String, "alnums"),
+		series.New([]bool{true, false, true, true, false}, series.Bool, "state"),
+	)
+
+	fmt.Println(df)
 }
