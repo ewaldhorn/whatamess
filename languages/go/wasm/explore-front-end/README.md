8c0d1220e977f0d043d937cd6f06fceec87d8570
Ewald Horn <ewaldhorn@gmail.com>
2023-06-04 13:36:21 +0200

    WIP:

--- languages/go/wasm/explore-front-end/README.md
+++ languages/go/wasm/explore-front-end/README.md
@@ -1,3 +1,6 @@
 # Go and Wasm experiment
 
 Looking into how Go, via wasm, can interact with the browser UI.
+
+Basic idea from <https://tutorialedge.net/golang/writing-frontend-web-framework-webassembly-go/>,
+with some liberties taken along the way.
--- languages/go/wasm/explore-front-end/main.go
+++ languages/go/wasm/explore-front-end/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
+
+// RegisterFunction allows the front-end to call Go functions.
+func RegisterFunction(funcName string, myfunc func(i []js.Value)) {
+	js.Global().Set(funcName, js.NewCallback(myfunc))
+}
 
 func main() {
 	fmt.Println("All fired up!")
