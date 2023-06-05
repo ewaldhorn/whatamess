51ca93113a8a4270f7ecc7bbd18b072a3f628d5d
Ewald Horn <ewaldhorn@gmail.com>
2023-06-05 18:27:27 +0200

    Basic array allocation and display

--- languages/c/udemy/c_programming_2021/arrays/main.c
+++ languages/c/udemy/c_programming_2021/arrays/main.c
@@ -3,5 +3,16 @@
 
 int main() {
 
+    // 1d array
+    int nums[10];
+
+    nums[0] = 1; // 1st element
+    nums[9] = 10; // last element
+
+    printf("\n1d Array of numbers:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("Position %2d contains %d\n", i, nums[i]);
+    }
+
     return 0;
 }
