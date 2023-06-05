db5662fa176018d66d6f87fed0ac292fb8a0594b
Ewald Horn <ewaldhorn@gmail.com>
2023-06-05 17:23:15 +0200

    Basic C data types

---
+++ languages/c/udemy/c_programming_2021/datatypes/datatypes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() {
+
+    int i1 = 0;
+    printf("i1=%d\n", i1);
+
+    i1 = 5;
+    printf("i1=%d\n", i1);
+
+    i1 = -3;
+    printf("i1=%d\n", i1);
+
+    // the following statement generates a compiler warning because of the implicit conversion
+    i1 = 3.5;
+    printf("i1=%d\n", i1);
+
+    double d1 = 4.5;
+    printf("d1=%lf\n", d1);
+
+    d1 = 0;
+    printf("d1=%lf\n", d1);
+
+    float f1 = 1.37;
+    printf("f1=%f\n", f1);
+
+    f1 = 1;
+    printf("f1=%f\n", f1);
+
+    char c1 = 'A';
+    printf("c1='%c'\n", c1);
+
+    c1 = '0';
+    printf("c1='%c'\n", c1);
+
+    return 0;
+}
