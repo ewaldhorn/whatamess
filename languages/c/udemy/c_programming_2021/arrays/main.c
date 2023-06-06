ea92d54bfaf35277b1df33fd077c28161bf6b2a7
Ewald Horn <ewaldhorn@gmail.com>
2023-06-06 08:23:58 +0200

    Working on C arrays refreshers

--- languages/c/udemy/c_programming_2021/arrays/main.c
+++ languages/c/udemy/c_programming_2021/arrays/main.c
@@ -6,12 +6,21 @@ int main() {
     // 1d array
     int nums[10];
 
-    nums[0] = 1; // 1st element
-    nums[9] = 10; // last element
+    nums[0] = 123; // 1st element
+    nums[4] = 456; // 5th element
+    nums[9] = 789; // last element
 
     printf("\n1d Array of numbers:\n");
     for (int i = 0; i < 10; i++) {
         printf("Position %2d contains %d\n", i, nums[i]);
+    }
+
+    // more 1d arrays
+    int totals[] = {78,65,44,82,81,81,74,70,55};
+
+    printf("\nMore 1d array (first 5 elements):\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Position %d contains %d\n", i, totals[i]);
     }
 
     return 0;
