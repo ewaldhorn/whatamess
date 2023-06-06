ca4e73ae3db580b9b9d200f625d65c4a523f4c54
Ewald Horn <ewaldhorn@gmail.com>
2023-06-06 09:19:22 +0200

    C strings refresher continues

--- languages/c/udemy/c_programming_2021/strings/main.c
+++ languages/c/udemy/c_programming_2021/strings/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main() {
-    printf("C Strings:\n");
+    printf("C Strings:\n\n");
+
+    char firstName[] = {"Ben"};
+    char lastName[] = {"Benjamin"};
+
+    printf("All together now: %s %s\n", firstName, lastName);
+    printf("%s is %lu characters long\n", firstName, strlen(firstName));
+
+    printf("\n");
     return 0;
 }
