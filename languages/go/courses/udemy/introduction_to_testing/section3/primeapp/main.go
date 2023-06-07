abba35ee3cc2908976084a24930a21faf040f8ad
Ewald Horn <ewaldhorn@gmail.com>
2023-06-07 16:14:36 +0200

    Make the prime number checker interactive

--- languages/go/courses/udemy/introduction_to_testing/section3/primeapp/main.go
+++ languages/go/courses/udemy/introduction_to_testing/section3/primeapp/main.go
@@ -1,12 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	n := 2
+	// print a welcome message
+	printIntro()
 
-	_, msg := isPrime(n)
-	fmt.Println(msg)
+	// create a channel to indicate user wants to exit
+	userMonitorChannel := make(chan bool)
+
+	// start goroutine to get / check numbers
+	go readUserInput(userMonitorChannel)
+
+	// wait until user indicates they are done
+	<-userMonitorChannel
+
+	// close channel
+	close(userMonitorChannel)
+
+	// print a goodbye message
+	printOuttro()
+}
+
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	// read the user input
+	scanner.Scan()
+
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	// now try to convert the string to a number
+	numToCheck, err := strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		return "Please enter a whole number!", false
+	}
+
+	_, msg := isPrime(numToCheck)
+
+	return msg, false
+}
+
+func prompt() {}
+
+func printIntro() {
+	fmt.Println("Hello!")
+	fmt.Println("Enter whole numbers to check if they are prime (q to quit).")
+	prompt()
+}
+
+func printOuttro() {
+	fmt.Println("Ok, all done. Goodbye!")
 }
 
 func isPrime(n int) (bool, string) {
