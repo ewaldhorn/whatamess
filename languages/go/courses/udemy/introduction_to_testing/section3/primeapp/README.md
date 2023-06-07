6724eda4bb44b7e1eee93346f4ad8574880da081
Ewald Horn <ewaldhorn@gmail.com>
2023-06-07 17:41:09 +0200

    Run individual or grouped tests

--- languages/go/courses/udemy/introduction_to_testing/section3/primeapp/README.md
+++ languages/go/courses/udemy/introduction_to_testing/section3/primeapp/README.md
@@ -7,3 +7,5 @@ test with `go test .`
 get coverage percentage with `go test -cover .`
 get coverage information with `go test -coverprofile=coverage.out .`
 to use coverage information `go tool cover -html=coverage.out`
+to run just a particular test `go test -run Test_alpha_isPrime`
+to run a related set of tests `go test -run Test_alpha`
--- languages/go/courses/udemy/introduction_to_testing/section3/primeapp/main_test.go
+++ languages/go/courses/udemy/introduction_to_testing/section3/primeapp/main_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func Test_isPrime(t *testing.T) {
+func Test_alpha_isPrime(t *testing.T) {
 	primeTests := []struct {
 		name     string
 		testNum  int
@@ -39,7 +39,7 @@ func Test_isPrime(t *testing.T) {
 	}
 }
 
-func Test_prompt(t *testing.T) {
+func Test_alpha_prompt(t *testing.T) {
 	// keep a copy of Stdout around
 	oldOut := os.Stdout
 
