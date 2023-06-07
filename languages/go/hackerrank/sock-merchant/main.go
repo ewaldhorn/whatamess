33bf7ef291565478cb9580f1842c522e1efbbc68
Ewald Horn <ewaldhorn@gmail.com>
2023-06-07 11:05:18 +0200

    Hackerrank exercise

---
+++ languages/go/hackerrank/sock-merchant/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    // "sort"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+func sockMerchant(n int32, ar []int32) int32 {
+    // Write your code here
+    // sort.Slice(ar, func(i, j int) bool { return ar[i] < ar[j] })
+    mymap := make(map[int32]int)
+    for _,v := range(ar) {
+        mymap[v] += 1
+    }
+
+    var count int32 = 0
+
+    for _, v := range mymap {
+        if v >= 2 {
+            count += int32(v / 2)
+        }
+    }
+
+    return count
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var ar []int32
+
+    for i := 0; i < int(n); i++ {
+        arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+        checkError(err)
+        arItem := int32(arItemTemp)
+        ar = append(ar, arItem)
+    }
+
+    result := sockMerchant(n, ar)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
