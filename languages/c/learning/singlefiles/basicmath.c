172d021e91beab8c8b2b82b4473bb5f26b91bbb9
Ewald Horn <ewaldhorn@gmail.com>
2023-06-08 16:43:26 +0200

    C read CSV and display it

---
+++ languages/c/learning/singlefiles/basicmath.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1, b = 2, c = 3;
+
+    int sumOfAll = a + b + c;
+
+    printf("The values are a=%d, b=%d, c=%d\n", a, b, c);
+    printf("The sum total is %d\n", sumOfAll);
+
+    return 0;
+}
