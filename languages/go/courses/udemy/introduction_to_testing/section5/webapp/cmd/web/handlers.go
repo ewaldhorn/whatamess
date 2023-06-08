a737d56c8ee89d8f2add9af740b4064f4765e2fd
Ewald Horn <ewaldhorn@gmail.com>
2023-06-08 11:19:42 +0200

    Adding a handler and Go template for the home page

--- languages/go/courses/udemy/introduction_to_testing/section5/webapp/cmd/web/handlers.go
+++ languages/go/courses/udemy/introduction_to_testing/section5/webapp/cmd/web/handlers.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "this is the home page")
+	app.render(w, r, "home.page.gohtml", &TemplateData{})
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+	// read and parse the Go template
+	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	// now execute the template with the passed data
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		http.Error(w, "server error", http.StatusInternalServerError)
+		return err
+	}
+
+	return nil
 }
