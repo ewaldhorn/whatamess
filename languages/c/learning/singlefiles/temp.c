8e178b49e6dfd8244b60e3c242762080d9180796
Ewald Horn <ewaldhorn@gmail.com>
2023-06-10 17:24:10 +0200

    Better accuracy with temp conversions using floats

---
+++ languages/c/learning/singlefiles/temp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float fahr, celsius, lower = 0, upper = 500, step = 20;
+
+    fahr = lower;
+
+
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr - 32);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr += step;
+    }
+
+    return 0;
+}
