dda9c368f44e80058f8b6cc83f7616c1eb5e2ff2
Ewald Horn <ewaldhorn@gmail.com>
2023-06-13 14:42:02 +0200

    Simpe Zig while loop

--- languages/zig/simple/src/main.zig
+++ languages/zig/simple/src/main.zig
@@ -1,24 +1,17 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var birthYear: u32 = 1978;
+    var currentYear: u32 = 2023;
+    var counter: u8 = 0;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    std.debug.print("The current state:\n", .{});
+    std.debug.print("birthYear: {}\ncurrentYear: {}\ncounter: {}\n", .{ birthYear, currentYear, counter });
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    while (counter < (currentYear - birthYear)) {
+        counter += 1;
+    }
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    std.debug.print("\nThe new state:\n", .{});
+    std.debug.print("birthYear: {}\ncurrentYear: {}\ncounter: {}\n", .{ birthYear, currentYear, counter });
 }
