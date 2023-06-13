cc9e06ab8089e66e41ca6c3372eb91dbe3614ddd
Ewald Horn <ewaldhorn@gmail.com>
2023-06-13 16:13:21 +0200

    Clock example

---
+++ languages/kotlin/web/udemy/build_website_from_scratch/clock/build.gradle.kts
@@ -0,0 +1,33 @@
+import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+    maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+}
+
+group = "clock"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    configAsKobwebApplication()
+
+    @Suppress("UNUSED_VARIABLE") // Suppress spurious warnings about sourceset variables not being used
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk.core)
+                implementation(libs.kobweb.silk.icons.fa)
+            }
+        }
+    }
+}
