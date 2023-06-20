4d5878e7a9b3ea6a16f678ab9dd53bcb0803ac7b
Ewald Horn <ewaldhorn@gmail.com>
2023-06-20 14:48:28 +0200

    Add some content to the screen

--- languages/go/courses/udemy/build_with_fyne/hello_fyne/main.go
+++ languages/go/courses/udemy/build_with_fyne/hello_fyne/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+type App struct {
+	output *widget.Label
+}
+
+var myApp App
 
 func main() {
 	application := app.New()
@@ -12,7 +19,21 @@ func main() {
 	mainWindow := application.NewWindow(APP_MAIN_WINDOW_TITLE)
 	mainWindow.Resize(fyne.NewSize(640.0, 480.0))
 	mainWindow.CenterOnScreen()
-	mainWindow.SetContent(widget.NewLabel("Hello, world."))
+
+	output, entry, btn := myApp.makeUI()
+
+	mainWindow.SetContent(container.NewVBox(output, entry, btn))
 
 	mainWindow.ShowAndRun()
+}
+
+func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
+	output := widget.NewLabel("Hello, world.")
+	entry := widget.NewEntry()
+	entry.PlaceHolder = "Type Something"
+	btn := widget.NewButton("Update", func() { app.output.SetText(entry.Text) })
+
+	app.output = output
+
+	return output, entry, btn
 }
