b078e97f9c422dce2fd3b70cd02edbf69dd651bc
Ewald Horn <ewaldhorn@gmail.com>
2023-06-25 18:24:28 +0200

    Adjust values a tad

--- languages/go/learning/ebitengine/stars/main.go
+++ languages/go/learning/ebitengine/stars/main.go
@@ -26,43 +26,43 @@ import (
 const (
 	screenWidth  = 640
 	screenHeight = 480
-	scale        = 64
-	starsCount   = 512
+	scale        = 16
+	starsCount   = 2048
 )
 
 type Star struct {
-	fromx, fromy, tox, toy, brightness float32
+	fromX, fromY, toX, toY, brightness float32
 }
 
 func (s *Star) Init() {
-	s.tox = rand.Float32() * screenWidth * scale
-	s.fromx = s.tox
-	s.toy = rand.Float32() * screenHeight * scale
-	s.fromy = s.toy
+	s.toX = rand.Float32() * screenWidth * scale
+	s.fromX = s.toX
+	s.toY = rand.Float32() * screenHeight * scale
+	s.fromY = s.toY
 	s.brightness = rand.Float32() * 0xff
 }
 
 func (s *Star) Update(x, y float32) {
-	s.fromx = s.tox
-	s.fromy = s.toy
-	s.tox += (s.tox - x) / 32
-	s.toy += (s.toy - y) / 32
+	s.fromX = s.toX
+	s.fromY = s.toY
+	s.toX += (s.toX - x) / 32
+	s.toY += (s.toY - y) / 32
 	s.brightness += 1
 	if 0xff < s.brightness {
 		s.brightness = 0xff
 	}
-	if s.fromx < 0 || screenWidth*scale < s.fromx || s.fromy < 0 || screenHeight*scale < s.fromy {
+	if s.fromX < 0 || screenWidth*scale < s.fromX || s.fromY < 0 || screenHeight*scale < s.fromY {
 		s.Init()
 	}
 }
 
 func (s *Star) Draw(screen *ebiten.Image) {
 	c := color.RGBA{
-		R: uint8(0xbb * s.brightness / 0xff),
-		G: uint8(0xdd * s.brightness / 0xff),
-		B: uint8(0xff * s.brightness / 0xff),
+		R: uint8(0xaa * s.brightness / 0xff),
+		G: uint8(0xcc * s.brightness / 0xff),
+		B: uint8(0xee * s.brightness / 0xff),
 		A: 0xff}
-	vector.StrokeLine(screen, s.fromx/scale, s.fromy/scale, s.tox/scale, s.toy/scale, 1, c, true)
+	vector.StrokeLine(screen, s.fromX/scale, s.fromY/scale, s.toX/scale, s.toY/scale, 1, c, true)
 }
 
 type Game struct {
