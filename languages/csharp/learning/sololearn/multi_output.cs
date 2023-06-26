19129f91695581968ceb32037a0c3a6e514089fc
Ewald Horn <ewaldhorn@gmail.com>
2023-06-26 13:14:38 +0200

    C# catchup

---
+++ languages/csharp/learning/sololearn/multi_output.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program
+{
+public static void Main(string[] args)
+    {
+        Console.WriteLine("Hey!");
+        Console.WriteLine("You are doing great!");
+        Console.WriteLine("Keep it on!");
+    }
+}
