c638a3b34fa4bd65c656aad574ab98774e9a9664
Ewald Horn <ewaldhorn@gmail.com>
2023-06-27 12:01:05 +0200

    Updating progress

---
+++ languages/python/learning/sololearn/dictionaries.py
@@ -0,0 +1,17 @@
+ages = {
+   "Dave": 24,
+   "Mary": 42,
+   "John": 58
+}
+print(ages["Dave"])
+print(ages["Mary"])
+
+car = {
+    'brand':'BMW',
+    'year': 2018,
+    'color': 'red',
+    'mileage': 15000
+}
+
+data = input()
+print(car[data])
