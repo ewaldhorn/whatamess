cb3286f5fdbe74be01b7813ee4c70c94506098e2
Ewald Horn <ewaldhorn@gmail.com>
2023-06-27 09:39:45 +0200

    PHP functions

---
+++ languages/php/learning/sololearn/functions.php
@@ -0,0 +1,21 @@
+<?php
+    function functionName() {    
+        //code to be executed
+    }
+
+    function sayHello() {
+        echo "Hello!";
+    }
+
+    sayHello();
+
+
+    function summer($left, $right) {
+        $answer = $left + $right;
+        echo $answer;
+    }
+
+    function withDefault($left=10) {
+        echo "The left value is ".$left;
+    }
+?>
