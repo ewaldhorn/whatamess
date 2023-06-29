7ff2ca512c6781d66bbe1c012d45ce1fc4473b4e
Ewald Horn <ewaldhorn@gmail.com>
2023-06-29 08:54:07 +0200

    Python again

---
+++ languages/python/learning/sololearn/decorator_practice.py
@@ -0,0 +1,14 @@
+#your code goes here
+def decor(func):
+    def wrap(num):
+        print("***")
+        func(num)
+        print("***")
+        print("END OF PAGE")
+    return wrap
+
+@decor
+def invoice(num):
+    print("INVOICE #" +num)
+
+invoice(input())
