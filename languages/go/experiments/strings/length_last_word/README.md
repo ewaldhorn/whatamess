444d518e67e5d5a33051cf3ead2422740f797e3e
Ewald Horn <ewaldhorn@gmail.com>
2023-06-30 14:48:44 +0200

    Optimise solution

--- languages/go/experiments/strings/length_last_word/README.md
+++ languages/go/experiments/strings/length_last_word/README.md
@@ -10,3 +10,7 @@ Build a Go function that will find the length of the last word in a string.
 ```   Ano ther bi t of a challenge      ` - 9
 
 So basically, find the last word, irrespective of spacing, and calculate its length.
+
+## Bonus points - don't create new data structures
+
+In other words, don't create a list of words...
--- languages/go/experiments/strings/length_last_word/main.go
+++ languages/go/experiments/strings/length_last_word/main.go
@@ -13,20 +13,33 @@ func lengthLastWord(data string) int {
 	return len(words[len(words)-1])
 }
 
+// Trim all spaces
+// Find last index of " " (space)
+// Figure out how far from the NEXT character to the end of the string it is
+// Return that value, which is the length of the last word
+func moreEfficientLengthLastWord(data string) int {
+	tmp := strings.TrimSpace(data)
+	return len(tmp[strings.LastIndex(tmp, " ")+1:])
+}
+
 func main() {
 	str := "This is easy"
 	expected := len("easy")
 	fmt.Printf("%s - %d (%d)\n", str, lengthLastWord(str), expected)
+	fmt.Printf("%s - %d (%d)\n\n", str, moreEfficientLengthLastWord(str), expected)
 
 	str = "Not   much harder"
 	expected = len("harder")
 	fmt.Printf("%s - %d (%d)\n", str, lengthLastWord(str), expected)
+	fmt.Printf("%s - %d (%d)\n\n", str, moreEfficientLengthLastWord(str), expected)
 
 	str = "Now, this probably    a bit  harder now "
 	expected = len("now")
 	fmt.Printf("%s - %d (%d)\n", str, lengthLastWord(str), expected)
+	fmt.Printf("%s - %d (%d)\n\n", str, moreEfficientLengthLastWord(str), expected)
 
 	str = "   Ano ther bi t of a challenge      "
 	expected = len("challenge")
 	fmt.Printf("%s - %d (%d)\n", str, lengthLastWord(str), expected)
+	fmt.Printf("%s - %d (%d)\n\n", str, moreEfficientLengthLastWord(str), expected)
 }
