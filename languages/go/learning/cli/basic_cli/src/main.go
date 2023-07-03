2ddc66e266a57383b5176fdef40836fe8496e216
Ewald Horn <ewaldhorn@gmail.com>
2023-07-03 11:39:02 +0200

    Get arguments passed through to runner

--- languages/go/learning/cli/basic_cli/src/main.go
+++ languages/go/learning/cli/basic_cli/src/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello Go CLI!")
+	arguments := os.Args[1:]
+
+	if len(arguments) == 0 {
+		fmt.Println("Nothing to do.")
+		listCommands()
+		os.Exit(1)
+	}
+
+	cmd := arguments[0]
+
+	switch cmd {
+	case "hello":
+		fmt.Println("Hello, Go CLI!")
+	default:
+		fmt.Printf("Unknown command: '%s'.\n", cmd)
+	}
+}
+
+func listCommands() {
+	fmt.Println("The commands are:")
+
+	fmt.Printf("%-8s - Gives a greeting.\n", "hello")
+
+	fmt.Println()
 }
