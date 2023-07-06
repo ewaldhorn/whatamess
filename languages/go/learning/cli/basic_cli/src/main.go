a810b28c4c8b83adb5210cc0205a8bb1c8542de6
Ewald Horn <ewaldhorn@gmail.com>
2023-07-06 11:28:13 +0200

    CLI arguments

--- languages/go/learning/cli/basic_cli/src/main.go
+++ languages/go/learning/cli/basic_cli/src/main.go
@@ -6,32 +6,30 @@ import (
 	"os"
 )
 
+const (
+	HelloCmd = "hello"
+)
+
 func main() {
-	greetCommand := flag.NewFlagSet("hello", flag.ExitOnError)
-	greetNamePointer := greetCommand.String("name", "Go CLI", "name to greet")
+	greetCmd := flag.NewFlagSet(HelloCmd, flag.ExitOnError)
+	greetFormalCmd := flag.NewFlagSet("formal", flag.ExitOnError)
+	greetNamePtr := greetFormalCmd.String("name", "Go CLI", "name to greet")
 
 	if len(os.Args) < 2 {
-		fmt.Println("Nothing to do.")
-		listCommands()
-		os.Exit(1)
-	}
-
-	cmd := os.Args[1]
-
-	switch cmd {
-	case "hello":
-		greetCommand.Parse(os.Args[2:])
-		fmt.Printf("Hello, %s!\n", *greetNamePointer)
-	default:
-		fmt.Printf("Unknown command: '%s'.\n", cmd)
+		fmt.Println("No command provided")
 		os.Exit(2)
 	}
-}
 
-func listCommands() {
-	fmt.Println("The commands are:")
-
-	fmt.Printf("%-8s - Gives a greeting.\n", "hello")
-
-	fmt.Println()
+	switch os.Args[1] {
+	case HelloCmd:
+		greetCmd.Parse(os.Args[2:])
+		fmt.Println("Hi there!")
+		if greetCmd.Arg(0) == "formal" {
+			greetFormalCmd.Parse(os.Args[3:])
+			fmt.Printf("How do you do, %s?\n", *greetNamePtr)
+		}
+	default:
+		fmt.Println("Unknown command")
+		os.Exit(2)
+	}
 }
