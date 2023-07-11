a3547dcc4c87371ff34c6b305a46c9e90ce69b15
Ewald Horn <ewaldhorn@gmail.com>
2023-07-11 15:45:18 +0200

    Get basic tests running

--- languages/go/learning/gorilla/mux_testing/main_test.go
+++ languages/go/learning/gorilla/mux_testing/main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -15,8 +19,12 @@ func TestMain(m *testing.M) {
 		os.Getenv("APP_DB_NAME"))
 
 	ensureTableExists()
+
 	code := m.Run()
+
 	clearTable()
+
+	fmt.Printf("Received code [%d]\n", code)
 	os.Exit(code)
 }
 
@@ -29,4 +37,45 @@ func ensureTableExists() {
 func clearTable() {
 	testApp.DB.Exec("DELETE FROM products")
 	testApp.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	testApp.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
+func TestGetNonExistentProduct(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/product/11", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "Product not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'Product not found'. Got '%s'", m["error"])
+	}
 }
