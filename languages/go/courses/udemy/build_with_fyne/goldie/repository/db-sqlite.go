dee7d84c37175031a9877fff59a3a8f5194f72ef
Ewald Horn <ewaldhorn@gmail.com>
2023-07-14 14:10:59 +0200

    Adding database handline code

--- languages/go/courses/udemy/build_with_fyne/goldie/repository/db-sqlite.go
+++ languages/go/courses/udemy/build_with_fyne/goldie/repository/db-sqlite.go
@@ -1,6 +1,9 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+)
 
 type SQLiteRepository struct {
 	Conn *sql.DB
@@ -23,4 +26,47 @@ func (repo *SQLiteRepository) Migrate() error {
 
 	_, err := repo.Conn.Exec(query)
 	return err
+}
+
+func (repo *SQLiteRepository) InsertHolding(holding Holding) (*Holding, error) {
+	stmt := "INSERT INTO holdings(amount, purchase_date, purchase_price) values (?,?,?)"
+
+	res, err := repo.Conn.Exec(stmt, holding.Amount, holding.PurchaseDate.Unix(), holding.PurchasePrice)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	holding.ID = id
+	return &holding, nil
+}
+
+func (repo *SQLiteRepository) AllHoldings() ([]Holding, error) {
+	query := "SELECT id, amount, purchase_date, purchase_price FROM holdings ORDER BY purchase_date"
+	rows, err := repo.Conn.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var all []Holding
+
+	for rows.Next() {
+		var h Holding
+		var unixTime int64
+
+		err := rows.Scan(&h.ID, &h.Amount, &unixTime, &h.PurchasePrice)
+		if err != nil {
+			return nil, err
+		}
+
+		h.PurchaseDate = time.Unix(unixTime, 0)
+		all = append(all, h)
+	}
+
+	return all, nil
 }
--- languages/go/courses/udemy/build_with_fyne/goldie/repository/repository.go
+++ languages/go/courses/udemy/build_with_fyne/goldie/repository/repository.go
@@ -12,14 +12,14 @@ var (
 
 type Repository interface {
 	Migrate() error
-	InsertHolding(h Holdings) (*Holdings, error)
-	AllHoldings() (*Holdings, error)
-	GetHoldingByID(id int64) (*Holdings, error)
-	UpdateHolding(id int64, updated Holdings) error
+	InsertHolding(h Holding) (*Holding, error)
+	AllHoldings() (*Holding, error)
+	GetHoldingByID(id int64) (*Holding, error)
+	UpdateHolding(id int64, updated Holding) error
 	DeleteHolding(id int64) error
 }
 
-type Holdings struct {
+type Holding struct {
 	ID            int64     `json:"id"`
 	Amount        int       `json:"amount"`
 	PurchaseDate  time.Time `json:"purchase_date"`
