896c041e7d0b2dd04c99885d2c2e467bdae3e2e4
Ewald Horn <ewaldhorn@gmail.com>
2023-07-16 14:21:41 +0200

    Smoother animation with edge bounce

--- languages/javascript/experiments/slimer/app.js
+++ languages/javascript/experiments/slimer/app.js
@@ -13,17 +13,25 @@ class Ball {
         this.effect = effect;
         this.x = this.effect.width * 0.5;
         this.y = this.effect.height * 0.5;
-        this.radius = 50;
-        this.speedX = 1;
-        this.speedY = 1;
+        this.radius = (Math.random() * 40) + 10;
+        this.speedX = Math.random() - 0.5;
+        this.speedY = Math.random() - 0.5;
     }
 
     update() {
+        if (this.x < this.radius || this.x > (canvas.width-this.radius)) {
+            this.speedX *= -1;
+        }
+        if (this.y < this.radius || this.y > (canvas.height-this.radius)) {
+            this.speedY *= -1;
+        }
+
         this.x += this.speedX;
         this.y += this.speedY;
     }
 
     draw(context) {
+        context.fillStyle = "white";
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2.0);
         context.fill();
@@ -37,11 +45,29 @@ class MetaBallsEffect {
         this.metaBallsArray = [];
     }
 
-    init() {}
-    update() {}
-    draw(context) {}
+    init(numberOfBalls) {
+        for (let i = 0; i < numberOfBalls; i++) {
+            this.metaBallsArray.push(new Ball(this));
+        }
+    }
+
+    update() {
+        this.metaBallsArray.forEach(m => m.update());
+    }
+
+    draw(context) {
+        this.metaBallsArray.forEach(m => m.draw(context));
+    }
 }
+
+const effect = new MetaBallsEffect(canvas.width, canvas.height);
+effect.init(20);
 
 function animate() {
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    effect.update();
+    effect.draw(ctx);
+    requestAnimationFrame(animate);
 }
+
+animate();
