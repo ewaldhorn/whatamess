065a8c98bab12780ee5a2b5df4809a4a92ca5143
Ewald Horn <ewaldhorn@gmail.com>
2023-07-18 08:58:01 +0200

    Start adding infrastructure to support testing

---
+++ languages/go/courses/udemy/build_with_fyne/goldie/repository/setup_test.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/glebarez/go-sqlite"
+)
+
+var testRepo *SQLiteRepository
+
+func TestMain(m *testing.M) {
+	path := "./testdata/sql.db"
+	_ = os.Remove(path)
+
+	db, err := sql.Open("sqlite", path)
+	if err != nil {
+		log.Println(err)
+	}
+
+	testRepo = NewSQLiteRepository(db)
+	os.Exit(m.Run())
+}
