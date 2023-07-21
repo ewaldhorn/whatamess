1dec8ce19d4fd7f6f8ba7bf2a88ad0b1583e3426
Ewald Horn <ewaldhorn@gmail.com>
2023-07-21 09:31:15 +0200

    Looking at minifiers to make JS smaller and thus faster to load

---
+++ languages/javascript/experiments/minifiers/basic/run_uglify.sh
@@ -0,0 +1,2 @@
+uglifyjs sample.js -o sample.min.js
+uglifyjs sample.js -c -m -o sample.mangled.min.js
---
+++ languages/javascript/experiments/minifiers/basic/sample.js
@@ -0,0 +1,15 @@
+var input_user = 1;
+var input_system = 2;
+
+function plus(a, b) {
+    return a + b;
+}
+
+function times(a, b) {
+    return a * b;
+}
+
+let message = "This is a message";
+
+console.log(plus(input_user, input_system));
+console.log(message)
