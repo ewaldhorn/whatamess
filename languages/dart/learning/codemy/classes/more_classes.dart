4e259d1d1ef566f782607b91136f3525fc11baa6
Ewald Horn <ewaldhorn@gmail.com>
2023-07-27 16:31:00 +0200

    Dart classes refresher

--- languages/dart/learning/codemy/classes/more_classes.dart
+++ languages/dart/learning/codemy/classes/more_classes.dart
@@ -1,25 +1,22 @@
 void main() {
-    Person p1 = Person("Briana", "F", 28);
+    Person p1 = Person("Frank", "M", 38);
     p1.showData();
 
-    Person p2 = Person("Karl", "M", 58);
+    Person p2 = Person("Lola", "F", 40);
     p2.showData();
 }
 
 class Person {
-    // properties
     String? name, sex;
-    int? age;
+    int age = 0;
 
-    // constructor
     Person(String name, sex, int age) {
         this.name = name;
         this.sex = sex;
         this.age = age;
     }
 
-    // methods
     void showData() {
-        print("$name is $age years old, and $sex.");
+        print("$name is $age years old and $sex.");
     }
 }
