87c0ff8010c6fa03068dd75616e35e3dce8d760e
Ewald Horn <ewaldhorn@gmail.com>
2023-07-27 11:29:10 +0200

    Finals and Const

--- languages/dart/learning/codemy/variables/variables.dart
+++ languages/dart/learning/codemy/variables/variables.dart
@@ -14,6 +14,21 @@ void main() {
     print("$myNumber / $anyInteger = ${myNumber/anyInteger}");
 
     // Dynamic variables
+    dynamic thisIsADynamic = 50;
+
+    print("The dynamic contains $thisIsADynamic");
+    thisIsADynamic = "Yolo";
+    print("The dynamic contains $thisIsADynamic");
+
+    // Const and Final
+    // Const for compile-time constants, known at compile time
+    // Final for runtime constants, unknown at compile time
+    const String aintChanging = "This is it!";
+    // aintChanging = "Nope"; // can't do this. Nope.
+
+    final String changeable = "Bobby";
+    print("changeable is $changeable");
+    // changeable = "Billy"; // can't do this either...
 
     print("");
 }
