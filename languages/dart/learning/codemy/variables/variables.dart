fa50672479465bfc7b209f5ec8508bf72ff8a60d
Ewald Horn <ewaldhorn@gmail.com>
2023-07-27 11:16:58 +0200

    Looking at variables in Dart

---
+++ languages/dart/learning/codemy/variables/variables.dart
@@ -0,0 +1,19 @@
+void main() {
+    print("\nDart Variables:\n===============\n");
+
+    // Strings
+    var thisPerson = "Bob";
+    String thisIsAString = "This is a string!";
+
+    print("The variable thisPerson contains '$thisPerson' and thisIsAString == '$thisIsAString'.");
+
+    // Numbers
+    var myNumber = 14;
+    int anyInteger = 7;
+
+    print("$myNumber / $anyInteger = ${myNumber/anyInteger}");
+
+    // Dynamic variables
+
+    print("");
+}
