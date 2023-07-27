f347ce80411a3276c416b3381490aadb5a0dc0c7
Ewald Horn <ewaldhorn@gmail.com>
2023-07-27 16:51:24 +0200

    Dart exercise

---
+++ languages/dart/learning/singles/relational_operators.dart
@@ -0,0 +1,18 @@
+void main() {
+  /*
+    Arithmetic + - / * % 
+    Relational < > <= >= == !=
+    Assignment = += -= *= /= %=
+    Type is as
+    Logical && || !
+    */
+
+  var one = 200, two = 400;
+
+  print(one == two); // False
+  print(one != two); //True
+  print(one > two); // False
+  print(one < two); // True
+  print(one >= two); // False
+  print(one <= two); // True
+}
