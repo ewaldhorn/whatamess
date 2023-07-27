c4c7bd30868c946a5aa0daf26fd0eb68e6f31a60
Ewald Horn <ewaldhorn@gmail.com>
2023-07-27 14:26:47 +0200

    Dart logic refresher

---
+++ languages/dart/learning/codemy/logic/logic.dart
@@ -0,0 +1,28 @@
+void main() {
+    var num1 = 5;
+    var num2 = 7;
+    // If
+    if (num1 > num2) {
+        print("num1 is bigger than num2");
+    }
+
+    if (num1 < num2) {
+        print("num1 is smaller than num2");
+    }
+
+    // If Else
+    if (num1 > num2) {
+        print("num1 is bigger than num2");
+    } else {
+        print("num1 is smaller than num2");
+    }
+
+    // If Else If
+    if (num1 > num2) {
+        print("num1 is bigger than num2");
+    } else if (num1 < num2) {
+        print("num1 is smaller than num2");
+    } else {
+        print("num1 is equal to num2");
+    }
+}
