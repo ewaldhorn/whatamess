2ab87daa4f49dbbdc5d654297422b47236cd6d06
Ewald Horn <ewaldhorn@gmail.com>
2023-07-28 14:32:14 +0200

    Working on main dashboard layout

--- languages/dart/flutter/web/hello_web/lib/dashboard.dart
+++ languages/dart/flutter/web/hello_web/lib/dashboard.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:hello_web/config/size_config.dart';
+import 'package:hello_web/style/colours.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              height: SizeConfig.screenHeight,
+              color: AppColours.secondaryBg,
+            ),
+          ),
+          Expanded(
+            flex: 10,
+            child: Container(
+              width: double.infinity,
+              height: SizeConfig.screenHeight,
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              width: double.infinity,
+              height: SizeConfig.screenHeight,
+              color: AppColours.secondaryBg,
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
