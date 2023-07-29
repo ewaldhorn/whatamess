8a5e61a8dcf289902b329ac1ad21dad0bea2a7d1
Ewald Horn <ewaldhorn@gmail.com>
2023-07-29 17:43:49 +0200

    More Dart refreshers

---
+++ languages/dart/learning/video_courses/hello_dart/long_string.dart
@@ -0,0 +1,8 @@
+void main() {
+    var myString = '\nThis is a long string'
+    ' and it stretches over'
+    ' many lines, because it makes it more readable.'
+    ' Notice how there\'s no line-breaks except\nWHERE\nyou put them. Pretty handy!\n';
+
+    print(myString);
+}
