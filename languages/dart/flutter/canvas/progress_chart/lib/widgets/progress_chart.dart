ac2ec2f8e53d75f56dad86d8c0b5b031cd09661a
Ewald Horn <ewaldhorn@gmail.com>
2023-07-31 11:05:49 +0200

    Custom painter working

--- languages/dart/flutter/canvas/progress_chart/lib/widgets/progress_chart.dart
+++ languages/dart/flutter/canvas/progress_chart/lib/widgets/progress_chart.dart
@@ -11,7 +11,24 @@ class ProgressChartState extends State<ProgressChart> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Placeholder(),
+      child: CustomPaint(
+        child: Container(),
+        painter: ChartPainter(),
+      ),
     );
+  }
+}
+
+class ChartPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final c = Offset(size.width / 2, size.height / 2);
+    final paint = Paint()..color = Colors.black;
+    canvas.drawCircle(c, size.width / 12, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
