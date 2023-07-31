3e147fd835c7792f61696ac9f610559385093436
Ewald Horn <ewaldhorn@gmail.com>
2023-07-31 10:49:09 +0200

    Getting project skeleton in place

---
+++ languages/dart/flutter/canvas/progress_chart/lib/widgets/progress_chart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProgressChart extends StatefulWidget {
+  const ProgressChart({super.key});
+
+  @override
+  ProgressChartState createState() => ProgressChartState();
+}
+
+class ProgressChartState extends State<ProgressChart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Placeholder(),
+    );
+  }
+}
