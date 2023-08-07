5f876431e694d47c4ca311885630338b674767dc
Ewald Horn <ewaldhorn@gmail.com>
2023-08-07 16:35:48 +0200

    Added a gradient background

--- languages/dart/flutter/udemy/flutter_dart/dice_app/lib/main.dart
+++ languages/dart/flutter/udemy/flutter_dart/dice_app/lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Yolo')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow[800],
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 243, 136, 61),
+            Color.fromARGB(255, 220, 110, 40),
+            Color.fromARGB(255, 181, 88, 31),
+          ])),
+          child: const Center(
+            child: Text('Yolo'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
