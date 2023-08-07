1ef65611027aaa1c522c1be5ac0c83e77aff4c5e
Ewald Horn <ewaldhorn@gmail.com>
2023-08-07 17:27:30 +0200

    Refactor code to a cleaner structure

--- languages/dart/flutter/udemy/flutter_dart/dice_app/lib/main.dart
+++ languages/dart/flutter/udemy/flutter_dart/dice_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dice_app/widgets/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,24 +20,4 @@ void main() {
           )),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  final Widget child;
-
-  const GradientContainer(this.child, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 243, 136, 61),
-          Color.fromARGB(255, 220, 110, 40),
-          Color.fromARGB(255, 181, 88, 31),
-        ], begin: Alignment.centerLeft, end: Alignment.bottomRight),
-      ),
-      child: child,
-    );
-  }
 }
---
+++ languages/dart/flutter/udemy/flutter_dart/dice_app/lib/widgets/gradient_container.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  final Widget child;
+
+  const GradientContainer(this.child, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 243, 136, 61),
+          Color.fromARGB(255, 220, 110, 40),
+          Color.fromARGB(255, 181, 88, 31),
+        ], begin: Alignment.centerLeft, end: Alignment.bottomRight),
+      ),
+      child: child,
+    );
+  }
+}
