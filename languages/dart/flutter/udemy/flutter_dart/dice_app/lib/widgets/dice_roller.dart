3b3eab379dc48c816b5e03af239860447f65664e
Ewald Horn <ewaldhorn@gmail.com>
2023-08-09 11:01:26 +0200

    Handle random numbers a bit more efficiently

--- languages/dart/flutter/udemy/flutter_dart/dice_app/lib/widgets/dice_roller.dart
+++ languages/dart/flutter/udemy/flutter_dart/dice_app/lib/widgets/dice_roller.dart
@@ -10,6 +10,7 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
+  final random = Random();
   var activeImageNumber = 1;
   var activeImagePath = 'dice-images/dice-1.png';
 
@@ -30,7 +31,7 @@ class _DiceRollerState extends State<DiceRoller> {
         ),
         ElevatedButton(
           onPressed: () {
-            activeImageNumber = Random().nextInt(6) + 1;
+            activeImageNumber = random.nextInt(6) + 1;
             setState(() {
               activeImagePath = 'dice-images/dice-$activeImageNumber.png';
             });
