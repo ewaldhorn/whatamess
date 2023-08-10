df061a3db513e32ca69f4bea54c555e62c5a01a2
Ewald Horn <ewaldhorn@gmail.com>
2023-08-10 17:06:24 +0200

    Working on quiz screen

--- languages/dart/flutter/udemy/flutter_dart/quizz_app/lib/screens/quiz_screen.dart
+++ languages/dart/flutter/udemy/flutter_dart/quizz_app/lib/screens/quiz_screen.dart
@@ -10,6 +10,26 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text(
+            "Question",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: () {}, child: Text("Answer One")),
+          SizedBox(height: 10),
+          ElevatedButton(onPressed: () {}, child: Text("Answer Two")),
+          SizedBox(height: 10),
+          ElevatedButton(onPressed: () {}, child: Text("Answer Three")),
+          SizedBox(height: 10),
+          ElevatedButton(onPressed: () {}, child: Text("Answer Four"))
+        ],
+      ),
+    );
   }
 }
