0fa97537d6f772d1bd9da4b1d76f33ac96dbfbf7
Ewald Horn <ewaldhorn@gmail.com>
2023-08-11 08:56:56 +0200

    Working on quiz app styling

--- languages/dart/flutter/udemy/flutter_dart/quizz_app/lib/widgets/answer_button.dart
+++ languages/dart/flutter/udemy/flutter_dart/quizz_app/lib/widgets/answer_button.dart
@@ -4,14 +4,19 @@ class AnswerButton extends StatelessWidget {
   final String text;
   final void Function() onPressed;
 
-  const AnswerButton(this.text, this.onPressed, {super.key});
+  const AnswerButton({required this.text, required this.onPressed, super.key});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-          textStyle: const TextStyle(color: Colors.white)),
+          textStyle: const TextStyle(fontSize: 20),
+          backgroundColor: Colors.amber[200],
+          foregroundColor: Colors.black,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15)),
       child: Text(text),
     );
   }
