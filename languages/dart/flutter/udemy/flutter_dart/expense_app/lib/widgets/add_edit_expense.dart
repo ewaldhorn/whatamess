a479d381a314d65add19faa8427adb231d633056
Ewald Horn <ewaldhorn@gmail.com>
2023-08-15 11:24:16 +0200

    Add a cancel button and an amount input

--- languages/dart/flutter/udemy/flutter_dart/expense_app/lib/widgets/add_edit_expense.dart
+++ languages/dart/flutter/udemy/flutter_dart/expense_app/lib/widgets/add_edit_expense.dart
@@ -8,16 +8,42 @@ class AddEditExpense extends StatefulWidget {
 }
 
 class _AddEditExpenseState extends State<AddEditExpense> {
+  final _descriptionController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _descriptionController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _descriptionController,
             maxLength: 100,
             keyboardType: TextInputType.text,
-            decoration: InputDecoration(label: Text('Description')),
+            decoration: const InputDecoration(label: Text('Description')),
+          ),
+          TextField(
+            controller: _amountController,
+            maxLength: 10,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(label: Text('Amount')),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(onPressed: () {}, child: const Text('Cancel')),
+              ElevatedButton(
+                  onPressed: () {}, child: const Text('Save Expense'))
+            ],
           ),
         ],
       ),
