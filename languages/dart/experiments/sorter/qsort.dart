6ec093033a9350b30ad8b207fcc5e8be7a7af55f
Ewald Horn <ewaldhorn@gmail.com>
2023-08-18 08:59:52 +0200

    Added sorting

--- languages/dart/experiments/sorter/qsort.dart
+++ languages/dart/experiments/sorter/qsort.dart
@@ -8,7 +8,7 @@ void main() {
   List numbers = createList();
   print('');
   displayList('Unsorted list', numbers);
-  sortList();
+  sortList(numbers, 0, numbers.length - 1);
   displayList('\nSorted list  ', numbers);
   print('\n');
 }
@@ -29,4 +29,33 @@ void displayList(String description, List numbers) {
   }
 }
 
-void sortList() {}
+void sortList(List values, int left, int right) {
+  if (left < right) {
+    int pivotIndex = _partition(values, left, right);
+    sortList(values, left, pivotIndex - 1);
+    sortList(values, pivotIndex, right);
+  }
+}
+
+int _partition(List numbers, int left, int right) {
+  int pivotIndex = left + (right - left) ~/ 2;
+  int pivotValue = numbers[pivotIndex];
+  int i = left, j = right;
+  int temp;
+  while (i <= j) {
+    while (numbers[i] < pivotValue) {
+      i++;
+    }
+    while (numbers[j] > pivotValue) {
+      j--;
+    }
+    if (i <= j) {
+      temp = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = temp;
+      i++;
+      j--;
+    }
+  }
+  return i;
+}
