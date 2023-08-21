f8aedad348b8d6ae4edba58e0522d60d163b223e
Ewald Horn <ewaldhorn@gmail.com>
2023-08-21 10:58:39 +0200

    Set up a colour scheme

--- languages/dart/flutter/udemy/flutter_dart/expense_app/lib/main.dart
+++ languages/dart/flutter/udemy/flutter_dart/expense_app/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:expense_app/app/expense_app.dart';
 import 'package:flutter/material.dart';
 
+var defaultColourScheme = ColorScheme.fromSeed(seedColor: Colors.orange);
+
 void main() {
   runApp(
     MaterialApp(
-        theme: ThemeData().copyWith(useMaterial3: true),
+        theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: defaultColourScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: defaultColourScheme.onPrimaryContainer,
+              foregroundColor: defaultColourScheme.primaryContainer),
+        ),
         home: const ExpenseApp()),
   );
 }
