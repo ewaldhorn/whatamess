8944470ad618be4698888f3c98857fee0170bb5e
Ewald Horn <ewaldhorn@gmail.com>
2023-08-22 09:26:44 +0200

    Working on grid view

--- languages/dart/flutter/udemy/flutter_dart/meals/lib/categories.dart
+++ languages/dart/flutter/udemy/flutter_dart/meals/lib/categories.dart
@@ -5,6 +5,18 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: const [Text('1'), Text('2'), Text('3'), Text('4'), Text('5')],
+      ),
+    );
   }
 }
