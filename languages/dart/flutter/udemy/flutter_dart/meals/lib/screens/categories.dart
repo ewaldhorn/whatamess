3d2b2312a37e4ab3024eca4cb5fe6843b86fb2ed
Ewald Horn <ewaldhorn@gmail.com>
2023-08-23 17:03:27 +0200

    List filtering working

--- languages/dart/flutter/udemy/flutter_dart/meals/lib/screens/categories.dart
+++ languages/dart/flutter/udemy/flutter_dart/meals/lib/screens/categories.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: 'A Title', meals: []),
+        builder: (ctx) => MealsScreen(
+            title: category.title,
+            meals: dummyMeals
+                .where((element) => element.categories.contains(category.id))
+                .toList()),
       ),
     );
   }
@@ -31,7 +36,7 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onCategorySelected: () => _selectCategory(context),
+              onCategorySelected: () => _selectCategory(context, category),
             )
         ],
       ),
