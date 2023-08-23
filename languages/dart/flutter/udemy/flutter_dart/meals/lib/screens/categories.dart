b2e6fc791b096c3ab442964702e4c1daac0b441a
Ewald Horn <ewaldhorn@gmail.com>
2023-08-23 11:07:12 +0200

    Adding categories

--- languages/dart/flutter/udemy/flutter_dart/meals/lib/screens/categories.dart
+++ languages/dart/flutter/udemy/flutter_dart/meals/lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -15,7 +17,10 @@ class CategoriesScreen extends StatelessWidget {
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
-        children: const [Text('1'), Text('2'), Text('3'), Text('4'), Text('5')],
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
+        ],
       ),
     );
   }
