d2f127ae54de86c42c27133c060b9bfe61019bf5
Ewald Horn <ewaldhorn@gmail.com>
2023-08-27 16:39:37 +0200

    Update

---
+++ languages/dart/flutter/udemy/flame_flutter/README.md
@@ -0,0 +1,3 @@
+# Sadly, course is very out of date
+
+I'm not completing this one, it's incredibly out of date and relies on obsolete Dart and Flutter foundations.
--- languages/dart/flutter/udemy/flame_flutter/flame_game_loop_example/lib/main.dart
+++ languages/dart/flutter/udemy/flame_flutter/flame_game_loop_example/lib/main.dart
@@ -1,8 +1,50 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+
   runApp(GameWidget(game: MyFirstGame()));
 }
 
-class MyFirstGame extends FlameGame {}
+class MyFirstGame extends FlameGame with TapDetector, FPSCounter {
+  static final fpsTextPaint =
+      TextPaint(style: const TextStyle(color: Colors.black, fontSize: 12));
+
+  @override
+  Future<void>? onLoad() {
+    //print('<game loop> onLoad() called');
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    //print('<game loop> update called at delta time $dt');
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    canvas.drawPaint(Paint()..color = Colors.red);
+
+    //
+    // format and show the FPS for the game loop
+    var stringFormatterFPS = NumberFormat('##', "en_US");
+    String fpsString = "fps: " + stringFormatterFPS.format(fps(60));
+    fpsTextPaint.render(canvas, fpsString, Vector2(10, 10));
+    //print('<game loop> render called');
+  }
+
+  @override
+  void onTapUp(TapUpInfo info) {
+    print(
+        '<game loop> onTap location: (${info.eventPosition.game.x}, ${info.eventPosition.game.y}');
+  }
+}
