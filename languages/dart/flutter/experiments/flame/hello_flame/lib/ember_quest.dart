4de3241aa7cf9bed84633bb2859b22a1b40cb5bd
Ewald Horn <ewaldhorn@gmail.com>
2023-08-27 17:20:21 +0200

    Basic assets loading

---
+++ languages/dart/flutter/experiments/flame/hello_flame/lib/ember_quest.dart
@@ -0,0 +1,34 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'actors/ember.dart';
+
+class EmberQuestGame extends FlameGame {
+  EmberQuestGame();
+
+  late EmberPlayer _ember;
+
+  final world = World();
+  late final CameraComponent cameraComponent;
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+
+    cameraComponent = CameraComponent(world: world);
+    cameraComponent.viewfinder.anchor = Anchor.topLeft;
+    addAll([cameraComponent, world]);
+
+    _ember = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_ember);
+  }
+}
