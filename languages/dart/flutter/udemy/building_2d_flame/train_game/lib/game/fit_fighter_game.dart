0bf766eb7af0da3265462407108bfd4145bc575f
Ewald Horn <ewaldhorn@gmail.com>
2023-08-30 10:38:00 +0200

    Add dependencies, clean up code, add base game widget

---
+++ languages/dart/flutter/udemy/building_2d_flame/train_game/lib/game/fit_fighter_game.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+
+import 'package:flame/game.dart';
+
+class FitFighterGame extends FlameGame {
+  @override
+  FutureOr<void> onLoad() async {
+    return super.onLoad();
+  }
+}
