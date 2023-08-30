0595af11025bfb3f8cb88875a3bbc27b55cffdb3
Ewald Horn <ewaldhorn@gmail.com>
2023-08-30 11:02:12 +0200

    Define some statics for the assets

---
+++ languages/dart/flutter/udemy/building_2d_flame/train_game/lib/constants/globals.dart
@@ -0,0 +1,25 @@
+class Global {
+  // Private constructor, we don't want an instance of this class floating
+  // around for any reason.
+  Global._();
+
+  // -------------------------------------------------------------------- IMAGES
+  static const String backgroundSprite = "background.jpg";
+  static const String gameLogoSprite = "game_logo.png";
+
+  static const String dumbbellSprite = "dumbbell.png";
+  static const String proteinShakeSprite = "protein_shake.png";
+  static const String vaccineSprite = "vaccine.png";
+  static const String virusSprite = "virus.png";
+
+  static const String playerFeverSprite = "player_fever.png";
+  static const String playerFitSprite = "player_fit.png";
+  static const String playerMuscularSprite = "player_muscular.png";
+  static const String playerSkinnySprite = "player_skinny.png";
+
+  // --------------------------------------------------------------------- AUDIO
+  static const String dumbbellSound = "dumbbell_sound.mp3";
+  static const String proteinSound = "protein_sound.mp3";
+  static const String vaccineSound = "vaccine_sound.mp3";
+  static const String virusSound = "virus_sounds.mp3";
+}
