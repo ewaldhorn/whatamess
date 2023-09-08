ccdf38413961f8301b6cd70de1b75c05a7c748b9
Ewald Horn <ewaldhorn@gmail.com>
2023-09-08 17:00:15 +0200

    Drawing triangles with depth

--- languages/rust/experiments/wasm/walk-the-dog/src/lib.rs
+++ languages/rust/experiments/wasm/walk-the-dog/src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 use wasm_bindgen::JsCast;
+use web_sys::console;
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -12,11 +12,7 @@ pub fn main_js() -> Result<(), JsValue> {
 
     // Your code goes here!
     console::log_1(&JsValue::from_str("Ok... Hello world!"));
-    draw_triangle();
-    Ok(())
-}
 
-fn draw_triangle() {
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
     let canvas = document
@@ -30,12 +26,37 @@ fn draw_triangle() {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-    context.move_to(300.0, 0.0); // top of triangle
+
+    sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], 6);
+
+    Ok(())
+}
+
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]) {
+    let [top, left, right] = points;
+    context.move_to(top.0, top.1);
     context.begin_path();
-    context.line_to(0.0, 500.0); // bottom left of triangle
-    context.line_to(600.0, 500.0); // bottom right of triangle
-    context.line_to(300.0, 0.0); // back to top of triangle
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
     context.close_path();
     context.stroke();
-    context.fill();
+}
+
+fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], depth: u8) {
+    draw_triangle(&context, points);
+    let depth = depth - 1;
+    let [top, left, right] = points;
+    if depth > 0 {
+        let left_middle = midpoint(top, left);
+        let right_middle = midpoint(top, right);
+        let bottom_middle = midpoint(left, right);
+        sierpinski(&context, [top, left_middle, right_middle], depth);
+        sierpinski(&context, [left_middle, left, bottom_middle], depth);
+        sierpinski(&context, [right_middle, bottom_middle, right], depth);
+    }
+}
+
+fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
+    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
 }
