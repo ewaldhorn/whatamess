c6e181609537c73f4e3dfb70c9612c2b7d91cc7e
Ewald Horn <ewaldhorn@gmail.com>
2023-09-09 18:33:13 +0200

    looking into json parsing to load sprite sheets

--- languages/rust/experiments/wasm/walk-the-dog-ii/src/lib.rs
+++ languages/rust/experiments/wasm/walk-the-dog-ii/src/lib.rs
@@ -1,7 +1,29 @@
+use std::rc::Rc;
+use std::sync::Mutex;
+
 use rand::prelude::*;
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::console;
+
+#[derive(Serialize, Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Cell {
+    frame: Rect,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -10,9 +32,6 @@ pub fn main_js() -> Result<(), JsValue> {
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Err... Hello?"));
 
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
@@ -38,19 +57,32 @@ pub fn main_js() -> Result<(), JsValue> {
     // all of this is to wait for the image to be loaded before displaying it
     // lots of back and forth between rust and javascript
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
         let image = web_sys::HtmlImageElement::new().unwrap();
         image.set_src("assets/images/rhb/Idle (1).png");
 
         // wait for the image to load, let us know when it has
-        let callback = Closure::once(|| {
-            web_sys::console::log_1(&JsValue::from_str("loaded"));
-            _ = success_tx.send(());
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                _ = success_tx.send(Ok(()));
+            }
+        });
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                _ = error_tx.send(Err(err));
+            }
         });
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
 
         _ = success_rx.await;
         _ = context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        // read sprite sheet data for rhb
+        let json = fetch_json("assets/sprite_sheets/rhb.json").await.unwrap();
+        let sheet: Sheet = serde_wasm_bindgen::from_value(json).unwrap(); // TODO: Rather handle the potential error
 
         sierpinski(
             &context,
@@ -125,4 +157,14 @@ fn sierpinski(
 
 fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
     ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
+}
+
+/**
+ * Responsible for loading a json file, for example to be able to use sprite sheets.
+ */
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
+    let resp: web_sys::Response = resp_value.dyn_into()?;
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
