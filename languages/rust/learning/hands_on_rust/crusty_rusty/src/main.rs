e9420de7a5550a981af7c377d8c275d9df690bd4
Ewald Horn <ewaldhorn@gmail.com>
2023-09-11 17:25:57 +0200

    Getting skeleton for part II in place

---
+++ languages/rust/learning/hands_on_rust/crusty_rusty/src/main.rs
@@ -0,0 +1,12 @@
+#![warn(clippy::all, clippy::pedantic)]
+mod game;
+
+use bracket_lib::prelude::*;
+use game::State;
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Crusty Rusty")
+        .build()?;
+    main_loop(context, State::new())
+}
