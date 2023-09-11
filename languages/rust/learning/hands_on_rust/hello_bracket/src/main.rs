78117a0be0758c81b44f9989be4ec58001316340
Ewald Horn <ewaldhorn@gmail.com>
2023-09-11 13:53:55 +0200

    Importing bracket lib

---
+++ languages/rust/learning/hands_on_rust/hello_bracket/src/main.rs
@@ -0,0 +1,16 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1,1,"Howdy there Bracket Terminal!");
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
