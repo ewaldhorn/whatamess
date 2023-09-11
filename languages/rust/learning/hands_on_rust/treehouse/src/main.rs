5e427d9ffaf8ef84edfab8adbaffcf383a372570
Ewald Horn <ewaldhorn@gmail.com>
2023-09-11 10:02:03 +0200

    Working on logic

--- languages/rust/learning/hands_on_rust/treehouse/src/main.rs
+++ languages/rust/learning/hands_on_rust/treehouse/src/main.rs
@@ -6,5 +6,25 @@ fn main() {
     println!("Who goes there?");
 
     let name = prompts::get_user_name();
+    let mut allowed = false;
     println!("Oh, hello there {name}!");
+
+    let allowed_visitors = ["dan", "sally", "rico", "tammy"];
+
+    for visitor in &allowed_visitors {
+        if &name == visitor {
+            allowed = true;
+        }
+    }
+
+    if allowed {
+        println!("Please come in!");
+    } else {
+        println!("Seems you aren't on the list, here's an application form.");
+    }
+
+    println!(
+        "\nThe first name in allowed_visitors is {}.",
+        allowed_visitors[0]
+    );
 }
