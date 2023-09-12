ff39cdce193c2c606e214a04750190899ebea44a
Ewald Horn <ewaldhorn@gmail.com>
2023-09-12 09:31:32 +0200

    Introduce Rayon for parallel workloads

--- languages/rust/experiments/cli/generic/src/main.rs
+++ languages/rust/experiments/cli/generic/src/main.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 fn calculate_sum(numbers: &[i32]) -> Result<i32, String> {
     let mut sum = 0;
     for num in numbers.iter().cloned() {
@@ -11,6 +13,11 @@ fn calculate_sum_better(numbers: &[i32]) -> Result<i32, String> {
     Ok(sum)
 }
 
+fn calculate_sum_parallel(numbers: &[i32]) -> Result<i32, String> {
+    let sum = numbers.par_iter().sum();
+    Ok(sum)
+}
+
 fn main() {
     let numbers = vec![1, 2, 3, 4, 5];
     let sum = calculate_sum(&numbers).unwrap();
@@ -18,6 +25,11 @@ fn main() {
 
     match calculate_sum_better(&numbers) {
         Ok(sum) => println!("Better sum is also {sum}."),
+        Err(err) => eprintln!("The error is: {err}"),
+    }
+
+    match calculate_sum_parallel(&numbers) {
+        Ok(sum) => println!("Parallel sum is also {sum}."),
         Err(err) => eprintln!("The error is: {err}"),
     }
 }
