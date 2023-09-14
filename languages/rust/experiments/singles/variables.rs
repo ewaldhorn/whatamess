4b51b5aa52824f84cfd43735302edb61f8755593
Ewald Horn <ewaldhorn@gmail.com>
2023-09-14 13:28:57 +0200

    Testing variable immutability

---
+++ languages/rust/experiments/singles/variables.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let a = 0.5;
+    let b = 0.75;
+    let c = a;
+    let d = b;
+    let e = a;
+    let a = 0.25;
+    let b = 0.35;
+
+
+    println!("We have {a}, {b}, {c}, {d} and {e}.");
+}
