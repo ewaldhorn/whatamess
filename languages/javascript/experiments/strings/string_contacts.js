87457ba07753b20d753f74b322bcabeaa050f8f1
Ewald Horn <ewaldhorn@gmail.com>
2023-09-21 10:56:21 +0200

    Basic JavaScript string concatenation experiment

---
+++ languages/javascript/experiments/strings/string_contacts.js
@@ -0,0 +1,12 @@
+console.log('');
+console.log('Some simple String Concatenation experiments.');
+console.log('');
+
+const part_one = 'This is part one';
+const part_two = 'and this is part two.';
+
+console.log('Part1: ', part_one);
+console.log('Part2: ', part_two);
+
+const together = part_one + ' ' + part_two;
+console.log('Together: ', together);
