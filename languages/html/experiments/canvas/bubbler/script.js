025c444b867dc08c79345e1d8cd7939853aab3f2
Ewald Horn <ewaldhorn@gmail.com>
2023-09-22 08:57:18 +0200

    Changes R Us

--- languages/html/experiments/canvas/bubbler/script.js
+++ languages/html/experiments/canvas/bubbler/script.js
@@ -1,36 +1,39 @@
-let canvas, context;
-const particleArray = [];
+let canvas, context, info;
+let particleArray = [];
 
 class Particle {
     constructor(x = 0, y = 0) {
         this.x = x;
         this.y = y;
         this.radius = randomNumberMinMax(10, 20);
+        this.ttl = randomNumberMinMax(50, 1000);
         this.dx = (1 + Math.random() * 4) * (Math.floor(Math.random() * 100) > 50) ? 1 : -1;
         this.dy = (1 + Math.random() * 4) * (Math.floor(Math.random() * 100) > 50) ? 1 : -1;
         this.color = 'white';
     }
 
     draw() {
-        context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        context.strokeStyle = `hsl(${this.hue} 100% 50%)`;
-        context.stroke();
+        if (this.radius > 0) {
+            context.beginPath();
+            context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+            context.strokeStyle = `hsl(${this.hue} 100% 50%)`;
+            context.stroke();
 
-        const gradient = context.createRadialGradient(
-            this.x,
-            this.y,
-            1,
-            this.x + 0.5,
-            this.y + 0.5,
-            this.radius
-        );
+            const gradient = context.createRadialGradient(
+                this.x,
+                this.y,
+                1,
+                this.x + 0.5,
+                this.y + 0.5,
+                this.radius
+            );
 
-        gradient.addColorStop(0.3, "rgba(255, 255, 255, 0.3)");
-        gradient.addColorStop(0.95, "#e7feff");
+            gradient.addColorStop(0.3, "rgba(255, 255, 255, 0.3)");
+            gradient.addColorStop(0.95, "#e7feff");
 
-        context.fillStyle = gradient;
-        context.fill();
+            context.fillStyle = gradient;
+            context.fill();
+        }
     }
 
     update() {
@@ -43,6 +46,12 @@ class Particle {
 
         if (this.y < 20 || this.y > (canvas.height - 20)) {
             this.dy *= -1;
+        }
+
+        if (this.ttl > 0) {
+            this.ttl -= 1;
+        } else {
+            this.radius -= 1;
         }
     }
 }
@@ -75,12 +84,14 @@ const handleDrawCircle = (event) => {
 
 const init_variables = () => {
     canvas = document.getElementById('main-canvas');
-    canvas.width = 640;
-    canvas.height = 480;
+    canvas.width = window.innerWidth / 3;
+    canvas.height = window.innerHeight / 2;
     context = canvas.getContext('2d');
 
     canvas.style.backgroundColor = '#00b4ff';
     canvas.addEventListener('click', handleDrawCircle);
+
+    info = document.getElementById('info');
 }
 
 const animate = () => {
@@ -91,6 +102,8 @@ const animate = () => {
         particle?.draw();
     });
 
+    particleArray = particleArray.filter((p) => { return p.radius > 0; })
+    info.innerHTML = `Active bubbles: ${particleArray.length}`;
     requestAnimationFrame(animate);
 }
 
