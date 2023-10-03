e0d791222716e2edf859db16ffbffa03ef2dbaef
Ewald Horn <ewaldhorn@gmail.com>
2023-10-03 17:04:07 +0200

    Fix layout

--- languages/go/tinygo/lessons/hello_wasm/main.go
+++ languages/go/tinygo/lessons/hello_wasm/main.go
@@ -14,6 +14,7 @@ func add(x, y int) int
 
 // This function is exported to JavaScript, so can be called using
 // exports.multiply() in JavaScript.
+//
 //export multiply
 func multiply(x, y int) int {
     return x * y;
