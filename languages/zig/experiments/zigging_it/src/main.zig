1d7c4ae9fdae22ea2e0b49527b9dc1b36908c717
Ewald Horn <ewaldhorn@gmail.com>
2023-10-03 11:13:35 +0200

    Playing with Zig generics

--- languages/zig/experiments/zigging_it/src/main.zig
+++ languages/zig/experiments/zigging_it/src/main.zig
@@ -1,24 +1,24 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // Integers
+    var userCount: i32 = 10;
+    _ = try reportSize("userCount", @TypeOf(userCount));
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // Floating-points
+    const userAverage: f64 = 0.001;
+    _ = try reportSize("userAverage", @TypeOf(userAverage));
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // Booleans
+    const isSelected = false;
+    _ = try reportSize("isSelected", @TypeOf(isSelected));
 
-    try bw.flush(); // don't forget to flush!
+    // Structs
+    const User = struct { age: i8 };
+
+    _ = try reportSize("User", @TypeOf(User));
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn reportSize(name: []const u8, comptime input: type) !void {
+    std.debug.print("{s} is: {}, Size of {s} is {} in byte(s).\n", .{ name, input, name, @sizeOf(input) });
 }
