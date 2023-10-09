78b03477ea052aec01fc5a03d00614c07eade118
Ewald Horn <ewaldhorn@gmail.com>
2023-10-09 11:29:03 +0200

    Simple WASM experiments

--- languages/rust/learning/books/practical_webassembly/chapter7/hello_wasm/src/lib.rs
+++ languages/rust/learning/books/practical_webassembly/chapter7/hello_wasm/src/lib.rs
@@ -7,3 +7,12 @@ pub fn add(left: i32, right: i32) -> i32 {
 pub fn double(num: i32) -> i32 {
     num + num
 }
+
+#[no_mangle]
+pub fn fibonacci(num: i32) -> i32 {
+    match num {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(num - 1) + fibonacci(num - 2),
+    }
+}
