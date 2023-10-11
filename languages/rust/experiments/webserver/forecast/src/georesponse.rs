65a7caca7be8629afcb4a5ab1a3dab01404d6fd1
Ewald Horn <ewaldhorn@gmail.com>
2023-10-11 11:33:06 +0200

    WIP

--- languages/rust/experiments/webserver/forecast/src/georesponse.rs
+++ languages/rust/experiments/webserver/forecast/src/georesponse.rs
@@ -8,7 +8,7 @@ pub struct GeoResponse {
     pub results: Vec<LatLong>,
 }
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(sqlx::FromRow, Deserialize, Debug, Clone)]
 pub struct LatLong {
     pub latitude: f64,
     pub longitude: f64,
--- languages/rust/experiments/webserver/forecast/src/main.rs
+++ languages/rust/experiments/webserver/forecast/src/main.rs
@@ -1,16 +1,42 @@
 pub mod georesponse;
 
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::from_utf8};
 
 use anyhow::Context;
 use askama::Template;
-use axum::{extract::Query, routing::get, Router};
+use axum::{
+    async_trait,
+    extract::{FromRequestParts, Query, State},
+    http::request::Parts,
+    routing::get,
+    Router,
+};
 use georesponse::{AppError, GeoResponse, LatLong, WeatherDisplay, WeatherResponse};
+use reqwest::StatusCode;
 use serde::Deserialize;
+use sqlx::PgPool;
 
 #[derive(Deserialize)]
 pub struct WeatherQuery {
     pub city: String,
+}
+
+#[derive(Template)]
+#[template(path = "stats.html")]
+struct StatsTemplate {
+    pub cities: Vec<City>,
+}
+
+async fn get_last_cities(pool: &PgPool) -> Result<Vec<City>, AppError> {
+    let cities = sqlx::query_as::<_, City>("SELECT name FROM cities ORDER BY id DESC LIMIT 10")
+        .fetch_all(pool)
+        .await?;
+    Ok(cities)
+}
+
+async fn stats(_user: User, State(pool): State<PgPool>) -> Result<StatsTemplate, AppError> {
+    let cities = get_last_cities(&pool).await?;
+    Ok(StatsTemplate { cities })
 }
 
 #[derive(Template)]
@@ -21,8 +47,11 @@ async fn index() -> IndexTemplate {
     IndexTemplate
 }
 
-async fn weather(Query(params): Query<WeatherQuery>) -> Result<WeatherDisplay, AppError> {
-    let lat_long = fetch_lat_long(&params.city).await?;
+async fn weather(
+    Query(params): Query<WeatherQuery>,
+    State(pool): State<PgPool>,
+) -> Result<WeatherDisplay, AppError> {
+    let lat_long = get_lat_long(&pool, &params.city).await?;
     let weather = fetch_weather(lat_long).await?;
     Ok(WeatherDisplay::new(params.city, weather))
 }
@@ -30,6 +59,53 @@ async fn weather(Query(params): Query<WeatherQuery>) -> Result<WeatherDisplay, A
 #[axum_macros::debug_handler]
 async fn stats() -> &'static str {
     "Stats"
+}
+
+/// A user that is authorized to access the stats endpoint.
+///
+/// No fields are required, we just need to know that the user is authorized. In
+/// a production application you would probably want to have some kind of user
+/// ID or similar here.
+struct User;
+
+#[async_trait]
+impl<S> FromRequestParts<S> for User
+where
+    S: Send + Sync,
+{
+    type Rejection = axum::http::Response<axum::body::Body>;
+
+    async fn from_request_parts(parts: &mut Parts, _: &S) -> Result<Self, Self::Rejection> {
+        let auth_header = parts
+            .headers
+            .get("Authorization")
+            .and_then(|header| header.to_str().ok());
+
+        if let Some(auth_header) = auth_header {
+            if auth_header.starts_with("Basic ") {
+                let credentials = auth_header.trim_start_matches("Basic ");
+                let decoded = base64::decode(credentials).unwrap_or_default();
+                let credential_str = from_utf8(&decoded).unwrap_or("");
+
+                // Our username and password are hardcoded here.
+                // In a real app, you'd want to read them from the environment.
+                if credential_str == "forecast:forecast" {
+                    return Ok(User);
+                }
+            }
+        }
+
+        let reject_response = axum::http::Response::builder()
+            .status(StatusCode::UNAUTHORIZED)
+            .header(
+                "WWW-Authenticate",
+                "Basic realm=\"Please enter your credentials\"",
+            )
+            .body(axum::body::Body::from("Unauthorized"))
+            .unwrap();
+
+        Err(reject_response)
+    }
 }
 
 async fn fetch_lat_long(city: &str) -> Result<LatLong, anyhow::Error> {
@@ -51,6 +127,29 @@ async fn fetch_weather(lat_long: LatLong) -> Result<WeatherResponse, anyhow::Err
         .json::<WeatherResponse>()
         .await?;
     Ok(response)
+}
+
+async fn get_lat_long(pool: &PgPool, name: &str) -> Result<LatLong, anyhow::Error> {
+    let lat_long = sqlx::query_as::<_, LatLong>(
+        "SELECT lat AS latitude, long AS longitude FROM cities WHERE name = $1",
+    )
+    .bind(name)
+    .fetch_optional(pool)
+    .await?;
+
+    if let Some(lat_long) = lat_long {
+        return Ok(lat_long);
+    }
+
+    let lat_long = fetch_lat_long(name).await?;
+    sqlx::query("INSERT INTO cities (name, lat, long) VALUES ($1, $2, $3)")
+        .bind(name)
+        .bind(lat_long.latitude)
+        .bind(lat_long.longitude)
+        .execute(pool)
+        .await?;
+
+    Ok(lat_long)
 }
 
 #[tokio::main]
