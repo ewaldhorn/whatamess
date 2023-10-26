4474fc7a76904320d5ff0215be0fd33455cc9764
Ewald Horn <ewaldhorn@gmail.com>
2023-10-26 14:52:49 +0200

    Refreshing some Zig before the next project

--- languages/zig/learning/ziglearn/chap1/rework.zig
+++ languages/zig/learning/ziglearn/chap1/rework.zig
@@ -84,3 +84,117 @@ test "for" {
 
     for (string) |_| {}
 }
+
+///////////////////////////////////////////////////////////////////////////////////////// FUNCTIONS
+fn double(x: u32) u32 {
+    return x + x;
+}
+
+test "function" {
+    const y = double(2);
+    try expect(@TypeOf(y) == u32);
+    try expect(y == 4);
+}
+
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+test "function recursion" {
+    const x = fibonacci(10);
+    try expect(x == 55);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////// DEFER
+test "defer" {
+    var x: i16 = 5;
+    {
+        defer x += 2;
+        try expect(x == 5);
+    }
+    try expect(x == 7);
+}
+
+// multiple refers execute in reverse order (LIFO)
+test "multi defer" {
+    var x: f32 = 5;
+    {
+        defer x += 2;
+        defer x /= 2;
+    }
+    try expect(x == 4.5);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////// ERRORS
+const FileOpenError = error{
+    AccessDenied,
+    OutOfMemory,
+    FileNotFound,
+};
+
+const AllocationError = error{OutOfMemory};
+
+test "coerce error from a subset to a superset" {
+    const err: FileOpenError = AllocationError.OutOfMemory;
+    try expect(err == FileOpenError.OutOfMemory);
+}
+
+test "error union" {
+    const maybe_error: AllocationError!u16 = 10;
+    const no_error = maybe_error catch 0;
+
+    try expect(@TypeOf(no_error) == u16);
+    try expect(no_error == 10);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////// SWITCH
+test "switch statement" {
+    var x: i8 = 10;
+    switch (x) {
+        -1...1 => {
+            x = -x;
+        },
+        10, 100 => {
+            //special considerations must be made
+            //when dividing signed integers
+            x = @divExact(x, 10);
+        },
+        else => {},
+    }
+    try expect(x == 1);
+}
+
+test "switch expression" {
+    var x: i8 = 10;
+    x = switch (x) {
+        -1...1 => -x,
+        10, 100 => @divExact(x, 10),
+        else => x,
+    };
+    try expect(x == 1);
+}
+
+//////////////////////////////////////////////////////////////////////////////////// RUNTIME SAFETY
+// test "out of bounds" {
+//     const a = [3]u8{ 1, 2, 3 };
+//     var index: u8 = 5;
+//     const b = a[index];
+//     _ = b;
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////// UNREACHABLE
+fn asciiToUpper(x: u8) u8 {
+    return switch (x) {
+        'a'...'z' => x + 'A' - 'a',
+        'A'...'Z' => x,
+        else => unreachable,
+    };
+}
+
+test "unreachable switch" {
+    try expect(asciiToUpper('a') == 'A');
+    try expect(asciiToUpper('A') == 'A');
+}
+
+////////////////////////////////////////////////////////////////////////////////////////// POINTERS
