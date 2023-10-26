1dfdb0b63e4db4352596f979273e83aed9fc475c
Ewald Horn <ewaldhorn@gmail.com>
2023-10-26 13:57:26 +0200

    Cleaning up a bit

---
+++ languages/zig/learning/ziglearn/chap1/rework.zig
@@ -0,0 +1,86 @@
+const expect = @import("std").testing.expect;
+
+//////////////////////////////////////////////////////////////////////////////////////////////// IF
+test "if statement" {
+    const a = true;
+    var x: u16 = 0;
+    if (a) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+    try expect(x == 1);
+}
+
+test "if statement expression" {
+    const a = true;
+    var x: u16 = 0;
+    x += if (a) 1 else 2;
+    try expect(x == 1);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////// ARRAY
+test "array length" {
+    // create an array of characters, then get the length of said array
+    const a = [_]u8{ 't', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', 'n', ' ', 'a', 'r', 'r', 'a', 'y' };
+    try expect(a.len == 16);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////// WHILE
+test "while" {
+    var i: u8 = 2;
+    while (i < 100) {
+        i *= 2;
+    }
+    try expect(i == 128);
+}
+
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+    try expect(sum == 55);
+}
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+    try expect(sum == 4);
+}
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += i;
+    }
+    try expect(sum == 1);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////// FOR
+test "for" {
+    //character literals are equivalent to integer literals
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string, 0..) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string, 0..) |_, index| {
+        _ = index;
+    }
+
+    for (string) |_| {}
+}
