c00af9f25f14f9190cd00ceb87a9a5f399a1b9ec
Ewald Horn <ewaldhorn@gmail.com>
2023-10-26 17:53:21 +0200

    Showing deeper module nesting

--- languages/zig/learning/learning_zig/structure/main.zig
+++ languages/zig/learning/learning_zig/structure/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const User = @import("models/user.zig").User;
 const circle = @import("constants/constants.zig");
+const adder = @import("math/adders/adder.zig");
 
 pub fn main() void {
     const user = User{
@@ -13,4 +14,6 @@ pub fn main() void {
     const circle1 = circle.Circle{ .radius = 4.87, .sort_order = 2 };
 
     std.debug.print("The circle has a radius of {}.\n", .{circle1.radius});
+
+    std.debug.print("The sum of 5 and 6 is {d}.\n", .{adder.add(5, 6)});
 }
---
+++ languages/zig/learning/learning_zig/structure/math/adders/adder.zig
@@ -0,0 +1,7 @@
+//! Deeply nested module.
+
+/// Add function that can be called from anywhere.
+/// Documentation example.
+pub fn add(a: i64, b: i64) i64 {
+    return a + b;
+}
