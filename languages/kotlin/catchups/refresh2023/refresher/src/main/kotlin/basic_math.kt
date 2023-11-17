cecd48a8a2052311112e3fc23b92dfd61eb486f1
Ewald Horn <ewaldhorn@gmail.com>
2023-11-17 11:57:19 +0200

    Catching up on Kotlin changes

---
+++ languages/kotlin/catchups/refresh2023/refresher/src/main/kotlin/basic_math.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val pricePerBox = 11;
+    val numberOfBoxes = 10;
+
+    println("Buying $numberOfBoxes boxes at R${pricePerBox} each will cost R${pricePerBox * numberOfBoxes}.");
+}
