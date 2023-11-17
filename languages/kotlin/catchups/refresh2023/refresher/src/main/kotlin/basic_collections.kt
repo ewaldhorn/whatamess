2e3753240125813fba32a4919af58ac34e5afe99
Ewald Horn <ewaldhorn@gmail.com>
2023-11-17 12:18:36 +0200

    Some more Kotlin

--- languages/kotlin/catchups/refresh2023/refresher/src/main/kotlin/basic_collections.kt
+++ languages/kotlin/catchups/refresh2023/refresher/src/main/kotlin/basic_collections.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("Basic collections in Kotlin.");
+    println("Basic collections in Kotlin.")
+
+    println("\n Lists:")
+    // Read only list
+    val readOnlyShapes = listOf("triangle", "square", "circle")
+    println(readOnlyShapes) // [triangle, square, circle]
+
+    // Mutable list with explicit type declaration
+    val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+    println(shapes) // [triangle, square, circle]
+
+    // Create a read-only view of a mutable list
+    val shapesLocked: List<String> = shapes
+    shapes.add("rectangle")
+    println(shapes)
+    // can't modify shapesLocked, but the underlying list can still be modified
+    println(shapesLocked)
 }
--- languages/kotlin/catchups/refresh2023/refresher/src/main/kotlin/basic_math.kt
+++ languages/kotlin/catchups/refresh2023/refresher/src/main/kotlin/basic_math.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val pricePerBox = 11;
-    val numberOfBoxes = 10;
+    val pricePerBox = 11
+    val numberOfBoxes = 10
 
-    println("Buying $numberOfBoxes boxes at R${pricePerBox} each will cost R${pricePerBox * numberOfBoxes}.");
+    println("Buying $numberOfBoxes boxes at R${pricePerBox} each will cost R${pricePerBox * numberOfBoxes}.")
 }
