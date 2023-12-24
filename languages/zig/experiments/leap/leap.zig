f28d084768f4ec2651433c95beff69ccf77b7dca
Ewald Horn <ewaldhorn@gmail.com>
2023-12-24 18:32:20 +0200

    Adding tests to a function

---
+++ languages/zig/experiments/leap/leap.zig
@@ -0,0 +1,45 @@
+const expect = @import("std").testing.expect;
+
+pub fn isLeapYear(year: u32) bool {
+    return (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0));
+}
+
+test "year 1700" {
+    const result = isLeapYear(1700);
+    try expect(result == false);
+}
+
+test "year 1800" {
+    const result = isLeapYear(1800);
+    try expect(result == false);
+}
+
+test "year 1900" {
+    const result = isLeapYear(1900);
+    try expect(result == false);
+}
+
+test "year 2000" {
+    const result = isLeapYear(2000);
+    try expect(result == true);
+}
+
+test "year 2004" {
+    const result = isLeapYear(2004);
+    try expect(result == true);
+}
+
+test "year 2008" {
+    const result = isLeapYear(2008);
+    try expect(result == true);
+}
+
+test "year 2012" {
+    const result = isLeapYear(2012);
+    try expect(result == true);
+}
+
+test "year 2048" {
+    const result = isLeapYear(2048);
+    try expect(result == true);
+}
