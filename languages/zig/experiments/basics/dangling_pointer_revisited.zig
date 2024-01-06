caec92a808d450dd41edec428e98db80f9051710
Ewald Horn <ewaldhorn@gmail.com>
2024-01-06 11:38:49 +0200

    Adding a test for a dangling pointer

--- languages/zig/experiments/basics/dangling_pointer_revisited.zig
+++ languages/zig/experiments/basics/dangling_pointer_revisited.zig
@@ -31,3 +31,25 @@ pub fn main() !void {
 const User = struct {
 	power: i32,
 };
+
+// ========================================================================================== TESTS
+const testing = std.testing;
+test "test for dangling pointer" {
+	var lookup = std.StringHashMap(User).init(testing.allocator);
+	defer lookup.deinit();
+
+
+	const goku = User{.power = 9001};
+
+	try lookup.put("Goku", goku);
+
+	// returns an optional, .? would panic if "Goku"
+	// wasn't in our hashmap
+	const entry = lookup.getPtr("Goku").?;
+
+	_ = lookup.remove("Goku");
+
+	// this should fail, because entry has been removed
+	// !! not easy to detect though !!
+	try testing.expectEqual(@as(i32, 9001), entry.power);
+}
