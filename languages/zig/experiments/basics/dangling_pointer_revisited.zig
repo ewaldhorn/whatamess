c3acc48a8f33e2524d0b35863f3692e5df1b96f9
Ewald Horn <ewaldhorn@gmail.com>
2024-01-06 11:33:50 +0200

    More about dangling pointers

---
+++ languages/zig/experiments/basics/dangling_pointer_revisited.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+pub fn main() !void {
+	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+	const allocator = gpa.allocator();
+
+	var lookup = std.StringHashMap(User).init(allocator);
+	defer lookup.deinit();
+
+	const goku = User{.power = 9001};
+
+	try lookup.put("Goku", goku);
+
+	// returns an optional, .? would panic if "Goku"
+	// wasn't in our hashmap
+	const entry = lookup.getPtr("Goku").?;
+
+	std.debug.print("Goku's power is: {d}\n", .{entry.power});
+
+	// returns true/false depending on if the item was removed
+    // entry points to the copy stored in the hashmap
+	_ = lookup.remove("Goku");
+
+    // this now accesses memory that's already been freed.
+	std.debug.print("Goku's power is: {d}\n", .{entry.power});
+
+    // the original struct is still fine
+    std.debug.print("Goku's power is: {d}\n", .{goku.power});
+}
+
+const User = struct {
+	power: i32,
+};
