5fb81c8a18c08487b5c6ba103dbbf8bfa225ecaa
Ewald Horn <ewaldhorn@gmail.com>
2024-01-07 22:08:01 +0200

    Zig Wasm

---
+++ languages/zig/experiments/wasm/canvas/README.md
@@ -0,0 +1,2 @@
+# Original from https://github.com/Pyrolistical/zig-wasm-canvas/tree/master
+
---
+++ languages/zig/experiments/wasm/canvas/build.sh
@@ -0,0 +1,1 @@
+zig build-lib src/main.zig -target wasm32-freestanding -dynamic -rdynamic -femit-bin=out/main.wasm
