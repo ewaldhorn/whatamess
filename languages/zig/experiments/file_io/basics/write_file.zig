3e97a3bb119f6b683fa053471e1e0b0ca266d025
Ewald Horn <ewaldhorn@gmail.com>
2024-01-10 17:14:33 +0200

    Zig writing to a file experiment

---
+++ languages/zig/experiments/file_io/basics/write_file.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+test "create and write to a file" {
+    var file = try std.fs.cwd().createFile("test.txt", .{});
+    defer file.close();
+
+    const wrote = try file.write("A bunch of bytes that makes sense to human readers.\n");
+
+
+    std.debug.print("We ended up writing {d} bytes to the file just now.\n", .{wrote});
+
+    const Writer = file.writer();
+
+    const  phone_number= 911;
+
+    try Writer.print("Help! Someone, please call {d}!\n\n", .{phone_number});
+}
