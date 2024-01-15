214ab18317cd84412d9fab2007067fcae6ebe8d5
Ewald Horn <ewaldhorn@gmail.com>
2024-01-15 07:20:01 +0200

    Fix formatting

--- languages/zig/experiments/file_io/basics/write_file.zig
+++ languages/zig/experiments/file_io/basics/write_file.zig
@@ -6,12 +6,11 @@ test "create and write to a file" {
 
     const wrote = try file.write("A bunch of bytes that makes sense to human readers.\n");
 
-
     std.debug.print("We ended up writing {d} bytes to the file just now.\n", .{wrote});
 
     const Writer = file.writer();
 
-    const  phone_number= 911;
+    const phone_number = 911;
 
     try Writer.print("Help! Someone, please call {d}!\n\n", .{phone_number});
 }
