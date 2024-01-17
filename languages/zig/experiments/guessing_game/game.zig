eae74e070c857a4b761edd03807bb6f4edb841c3
Ewald Horn <ewaldhorn@gmail.com>
2024-01-17 12:42:46 +0200

    Working on Zig random numbers

--- languages/zig/experiments/guessing_game/game.zig
+++ languages/zig/experiments/guessing_game/game.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
 
     // use the 'undefined' value (random memory bytes as found by the OS)
     // convert that to a number, it should be relatively random.
@@ -12,6 +13,34 @@ pub fn main() !void {
     var prng = std.rand.DefaultPrng.init(seed);
     const rand = &prng.random();
 
-    try stdout.print("A sort-of random number {d}\n", 
-        .{rand.intRangeAtMost(u8, 1, 100)});
+    const guess_me = rand.intRangeAtMost(u8, 1, 100);
+    try stdout.print("Alright, try to guess a number between 1-100: ", .{});
+    var guess_count : i32 = 0;
+
+    while (true) {
+        guess_count += 1;
+        const raw_line = try stdin.readUntilDelimiterAlloc(std.heap.page_allocator, '\n', 8192);
+        defer std.heap.page_allocator.free(raw_line);
+        const line = std.mem.trim(u8, raw_line, "\r");
+
+        const guess = std.fmt.parseInt(u8, line, 10) catch |err| switch (err) {
+            error.Overflow => {
+                try stdout.writeAll("Please enter a small positive number\n");
+                continue;
+            },
+            error.InvalidCharacter => {
+                try stdout.writeAll("Please enter a valid number\n");
+                continue;
+            },
+        };
+
+        if (guess > guess_me) {
+            try stdout.print("Too high! Try again: ", .{});
+        } else if (guess < guess_me) {
+            try stdout.print("Too low! Try again: ", .{});
+        } else {
+            try stdout.print("\n\nNicely done!\n{} is indeed the correct number!\nYou took {d} guesses!\n\n", .{guess, guess_count});
+            break;
+        }
+    }
 }
