50dbb009a8397bd3c2ac5f4febe286bbaafb0823
Ewald Horn <ewaldhorn@gmail.com>
2024-01-17 12:19:50 +0200

    Basic random numbers being generated

--- languages/zig/experiments/guessing_game/game.zig
+++ languages/zig/experiments/guessing_game/game.zig
@@ -4,8 +4,13 @@ const std = @import("std");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    var prng = std.rand.DefaultPrng.init(1919191919);
-    const rand = prng.random();
+    // use the 'undefined' value (random memory bytes as found by the OS)
+    // convert that to a number, it should be relatively random.
+    var seed: u64 = undefined;
+    try std.os.getrandom(std.mem.asBytes(&seed));
+
+    var prng = std.rand.DefaultPrng.init(seed);
+    const rand = &prng.random();
 
     try stdout.print("A sort-of random number {d}\n", 
         .{rand.intRangeAtMost(u8, 1, 100)});
