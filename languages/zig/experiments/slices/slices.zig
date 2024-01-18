fbd48fb059555bdb38f641c5caa85ef642297ef1
Ewald Horn <ewaldhorn@gmail.com>
2024-01-18 10:23:40 +0200

    Zigging

---
+++ languages/zig/experiments/slices/slices.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+fn total(values: []const u8) usize {
+    var sum: usize = 0;
+    for (values) |v| sum += v;
+    return sum;
+}
+
+test "slices - empty" {
+    const array = [_]u8{};
+    const slice = array[0..0];
+    try expect(total(slice) == 0);
+}
+
+test "slices" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    try expect(total(slice) == 6);
+}
+
+test "slices 2" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    try expect(@TypeOf(slice) == *const [3]u8);
+}
+
+test "slices 3" {
+    var array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..];
+    _ = slice;
+}
