01ffcacbede59b981674a7bb9732f95d9cf071a5
Ewald Horn <ewaldhorn@gmail.com>
2024-01-20 17:02:33 +0200

    Zig comptime

--- languages/zig/experiments/basics/comptime.zig
+++ languages/zig/experiments/basics/comptime.zig
@@ -3,3 +3,49 @@ const std = @import("std");
 pub fn main() void {
     std.debug.print("{any}\n", .{@TypeOf(.{ .year = 2023, .month = 8 })});
 }
+
+test "comptime_int" {
+    const a = 12;
+    const b = a + 10;
+
+    const c: u4 = a;
+    _ = c;
+    const d: f32 = b;
+    _ = d;
+}
+
+test "branching on types" {
+    const a = 5;
+    const b: if (a < 10) f32 else i32 = 5;
+
+    try std.testing.expect(@TypeOf(b) == f32);
+}
+
+fn Matrix(
+    comptime T: type,
+    comptime width: comptime_int,
+    comptime height: comptime_int,
+) type {
+    return [height][width]T;
+}
+
+test "returning a type" {
+    try std.testing.expect(Matrix(f32, 4, 4) == [4][4]f32);
+}
+
+fn addSmallInts(comptime T: type, a: T, b: T) T {
+    return switch (@typeInfo(T)) {
+        .ComptimeInt => a + b,
+        .Int => |info| if (info.bits <= 16)
+            a + b
+        else
+            @compileError("ints too large"),
+        else => @compileError("only ints accepted"),
+    };
+}
+
+test "typeinfo switch" {
+    const x = addSmallInts(u16, 20, 30);
+    try std.testing.expect(@TypeOf(x) == u16);
+    try std.testing.expect(x == 50);
+}
