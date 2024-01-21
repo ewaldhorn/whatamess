41db3fddb16787b0c35245a942c581731131892d
Ewald Horn <ewaldhorn@gmail.com>
2024-01-21 16:50:00 +0200

    Zig reall all alloc to read entire contents of file into memory

--- languages/zig/experiments/basics/standard_io.zig
+++ languages/zig/experiments/basics/standard_io.zig
@@ -14,7 +14,31 @@ test "io writer usage" {
     const bytes_written = try list.writer().write(
         "Hello World!",
     );
-    
+
     try expect(bytes_written == 12);
     try expect(eql(u8, list.items, "Hello World!"));
+}
+
+// Here we will use a reader to copy the file's contents into an allocated buffer. The second 
+// argument of readAllAlloc is the maximum size that it may allocate; if the file is larger than 
+// this, it will return error.StreamTooLong.
+test "io reader usage" {
+    const message = "Hello File!";
+
+    const file = try std.fs.cwd().createFile(
+        "junk_file2.txt",
+        .{ .read = true },
+    );
+    defer file.close();
+
+    try file.writeAll(message);
+    try file.seekTo(0);
+
+    const contents = try file.reader().readAllAlloc(
+        test_allocator,
+        message.len,
+    );
+    defer test_allocator.free(contents);
+
+    try expect(eql(u8, contents, message));
 }
