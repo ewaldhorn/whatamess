3fe687fbdce0f51ed0bdad8e084c816d764a780a
Ewald Horn <ewaldhorn@gmail.com>
2024-01-21 17:34:35 +0200

    More hashmaps

--- languages/zig/experiments/basics/hashmaps.zig
+++ languages/zig/experiments/basics/hashmaps.zig
@@ -44,3 +44,36 @@ test "hashing string key" {
 
     try expect(std.mem.eql(u8, found, "momo"));
 }
+
+// .fetchPut puts a value in the hash map, returning a value if there was previously a value for that key.
+test "fetchPut" {
+    var map = std.AutoHashMap(u8, f32).init(
+        test_allocator,
+    );
+    defer map.deinit();
+
+    try map.put(255, 10);
+    const old = try map.fetchPut(255, 100);
+
+    try expect(old.?.value == 10);
+    try expect(map.get(255).? == 100);
+}
+
+// std.StringHashMap is also provided for when you need strings as keys.
+test "string hashmap" {
+    var map = std.StringHashMap(enum { cool, uncool }).init(
+        test_allocator,
+    );
+    defer map.deinit();
+
+    try map.put("loris", .uncool);
+    try map.put("me", .cool);
+
+    try expect(map.get("me").? == .cool);
+    try expect(map.get("loris").? == .uncool);
+}
+
+// std.StringHashMap and std.AutoHashMap are just wrappers for std.HashMap. If these two do not 
+// fulfill your needs, using std.HashMap directly gives you much more control.
+// If having your elements backed by an array is wanted behaviour, try std.ArrayHashMap and 
+// its wrapper std.AutoArrayHashMap.
