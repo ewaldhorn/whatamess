92a2d38a688d930fa19ec9f0c8ea7aaa1ac13163
Ewald Horn <ewaldhorn@gmail.com>
2024-01-21 16:55:43 +0200

    Looking into std.io.Writer

--- languages/zig/experiments/basics/standard_io.zig
+++ languages/zig/experiments/basics/standard_io.zig
@@ -72,3 +72,47 @@ test "read until next line" {
         .{input},
     );
 }
+
+// An std.io.Writer type consists of a context type, error set, and a write function. The write 
+// function must take in the context type and a byte slice. The write function must also return an 
+// error union of the Writer type's error set and the number of bytes written. Let's create a type 
+// that implements a writer.
+// ------------------------------------------------------------------------------------------------
+// Don't create a type like this! Use an arraylist with a fixed buffer allocator
+const MyByteList = struct {
+    data: [100]u8 = undefined,
+    items: []u8 = &[_]u8{},
+
+    const Writer = std.io.Writer(
+        *MyByteList,
+        error{EndOfBuffer},
+        appendWrite,
+    );
+
+    fn appendWrite(
+        self: *MyByteList,
+        data: []const u8,
+    ) error{EndOfBuffer}!usize {
+        if (self.items.len + data.len > self.data.len) {
+            return error.EndOfBuffer;
+        }
+        std.mem.copy(
+            u8,
+            self.data[self.items.len..],
+            data,
+        );
+        self.items = self.data[0 .. self.items.len + data.len];
+        return data.len;
+    }
+
+    fn writer(self: *MyByteList) Writer {
+        return .{ .context = self };
+    }
+};
+
+test "custom writer" {
+    var bytes = MyByteList{};
+    _ = try bytes.writer().write("Hello");
+    _ = try bytes.writer().write(" Writer!");
+    try expect(eql(u8, bytes.items, "Hello Writer!"));
+}
