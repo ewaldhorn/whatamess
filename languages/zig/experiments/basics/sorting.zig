abc5bd41390a2783257ecb7385417909c23cdfd2
Ewald Horn <ewaldhorn@gmail.com>
2024-01-21 17:49:29 +0200

    Also sort floats

--- languages/zig/experiments/basics/sorting.zig
+++ languages/zig/experiments/basics/sorting.zig
@@ -10,6 +10,13 @@ test "sorting" {
     try expect(std.mem.eql(u8, &data, &[_]u8{ 240, 10, 10, 5, 0, 0 }));
 }
 
+test "sorting floats" {
+    var data = [_]f32{1.2, 2.4, 1.3, 1.1, 1.4};
+    std.mem.sort(f32, &data, {}, comptime std.sort.desc(f32));
+
+    try expect(std.mem.eql(f32, &data, &[_]f32{2.4, 1.4, 1.3, 1.2, 1.1}));
+}
+
 // std.sort.asc and .desc create a comparison function for the given type at comptime; 
 // if non-numerical types should be sorted, the user must provide their own comparison function.
 // std.sort.sort has a best case of O(n), and an average and worst case of O(n*log(n)).
