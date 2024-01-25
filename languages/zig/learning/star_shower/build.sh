fe9ecb53280265ec53627489bebfd05fb580f56d
Ewald Horn <ewaldhorn@gmail.com>
2024-01-25 15:43:21 +0200

    Adding some docs to the build script for future me

--- languages/zig/learning/star_shower/build.sh
+++ languages/zig/learning/star_shower/build.sh
@@ -1,5 +1,7 @@
-# 0.11.x
+# with 0.11.x
+# exports are automatic
 # zig build-lib src/main.zig -target wasm32-freestanding -dynamic -rdynamic -femit-bin=out/main.wasm
 # 
-# 0.12.x
+# with 0.12.x
+# seems I need to specify exports, need to create a proper build.zig file at some stage
 zig build-exe src/main.zig -target wasm32-freestanding -fno-entry --export=_draw --export=allocUint8 --export=free -femit-bin=out/main.wasm
