b0f793edbce28c0f9d729110e549a744d11851d3
Ewald Horn <ewaldhorn@gmail.com>
2024-01-25 17:54:33 +0200

    Moving code around

--- languages/zig/learning/star_shower/README.md
+++
@@ -1,8 +0,0 @@
-# Star Shower
-
-Following the tutorial `How To Code: Star Shower` by Chris Courses, more or less. I've taken some
-liberties here and there, because, well, why not?
-
-## But I see some Zig - WOT
-
-Yes, I'm using some Zig here and there - that's the whole point of the exercise!
--- languages/zig/learning/star_shower/build.sh
+++
@@ -1,7 +0,0 @@
-# with 0.11.x
-# exports are automatic
-# zig build-lib src/main.zig -target wasm32-freestanding -dynamic -rdynamic -femit-bin=out/main.wasm
-# 
-# with 0.12.x
-# seems I need to specify exports, need to create a proper build.zig file at some stage
-zig build-exe src/main.zig -target wasm32-freestanding -fno-entry --export=_draw --export=allocUint8 --export=free -femit-bin=out/main.wasm
