505e88945d3bfe4c859a880ce9cd201394b743aa
Ewald Horn <ewaldhorn@gmail.com>
2024-01-27 15:46:19 +0200

    Added explainer note

--- languages/zig/learning/comptime/max_value.zig
+++ languages/zig/learning/comptime/max_value.zig
@@ -9,5 +9,7 @@ fn add_up(left: i32, comptime right: i32) i32 {
 pub fn main() void {
     std.debug.print("Ten and Ten is {d}.\n", .{add_up(10, 10)});
     std.debug.print("Ten and Fifty is {d}.\n", .{add_up(10, 50)});
+
+    // looks like legit code but won't compile
     std.debug.print("Ten and Sixty is {d}.\n", .{add_up(10, 60)});
 }
