f156e2d5fc89bdcde941993622f5a89d422a5555
Ewald Horn <ewaldhorn@gmail.com>
2024-01-28 08:53:54 +0200

    WIP:  Updating to 0.12.x

--- languages/zig/experiments/file_io/read_file/read_file.zig
+++ languages/zig/experiments/file_io/read_file/read_file.zig
@@ -5,7 +5,7 @@ pub fn mmap_file(path: []const u8) ![]align(std.mem.page_size) u8 {
     defer file.close();
     const size = try file.getEndPos();
 
-    var mapping = try std.os.mmap(null, size, std.os.PROT_READ | std.os.PROT_WRITE, std.os.MAP_SHARED, file.handle, 0);
+    var mapping = try std.os.mmap(null, size, std.os.PROT.READ | std.os.PROT.WRITE, std.os.MAP.SHARED, file.handle, 0);
     errdefer std.os.munmap(mapping);
 
     if (size < @sizeOf(u64)) {
@@ -14,11 +14,18 @@ pub fn mmap_file(path: []const u8) ![]align(std.mem.page_size) u8 {
 
     const data_end = size - @sizeOf(u64);
     const actual_hash = std.hash.CityHash64.hash(mapping[0..data_end]);
-    const expected_hash = std.mem.readIntLittle(u64, mapping[data_end..]);
-    
+    const expected_hash = std.mem.readInt(u64, mapping[data_end..], std.builtin.Endian.little);
+
     if (actual_hash != expected_hash) {
         return error.FileSignatureInvalid;
     }
 
     return mapping;
+}
+
+const expect = std.testing.expect;
+
+test "file is too small" {
+    const result = try mmap_file("./small.txt");
+    try expect(std.mem.eql(u8, result, [_]u8{}));
 }
