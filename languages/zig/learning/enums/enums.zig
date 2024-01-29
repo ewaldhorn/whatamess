10a92861e49063f376a96c9389ccc70cc7f8d48f
Ewald Horn <ewaldhorn@gmail.com>
2024-01-29 10:55:57 +0200

    Enums

--- languages/zig/learning/enums/enums.zig
+++ languages/zig/learning/enums/enums.zig
@@ -4,7 +4,11 @@ const State = enum {
     Pending,
     Processing,
     Packaging,
-    Shipped
+    Shipped,
+
+    pub fn hasShipped(self: State) bool {
+        return self == .Shipped;
+    }
 };
 
 // ________________________________________________________________________________________________ 
@@ -17,4 +21,24 @@ test "can create an enum" {
 
     try expect(status == State.Pending);
     try expect(status_alternate == State.Processing);
+}
+
+test "enums have int values" {
+    const status = State.Packaging;
+    const value = @intFromEnum(status);
+
+    try expect(value == 2);
+}
+
+test "can use int value to get to enum value" {
+    const value : State = @enumFromInt(3);
+
+    try expect(value == State.Shipped);
+}
+
+test "can call bound functions on enum" {
+    const status = State.Packaging;
+
+    try expect(status.hasShipped() == false);
+    try expect(State.hasShipped(status) == false);
 }
