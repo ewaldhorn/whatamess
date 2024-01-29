929cd4fbd3ef9e5795731c9469fa3c99b8841e79
Ewald Horn <ewaldhorn@gmail.com>
2024-01-29 16:26:12 +0200

    Working on sorting out a memory bug in the word counter

--- exercism/zig/word_count.zig
+++ exercism/zig/word_count.zig
@@ -14,12 +14,13 @@ pub fn countWords(allocator: mem.Allocator, s: []const u8) !std.StringHashMap(u3
     
     while (splitter.next()) |tmp_s| {
         // std.debug.print("Now looking for: {s}\n", .{tmp});
-        // need to make our own copy of the tmp string, or it will get freed before we are done
-        const tmp = std.fmt.allocPrint(allocator, "{s}", .{tmp_s}) catch "ERROR";
-        const value = result.get(tmp);
+        const value = result.get(tmp_s);
         if (value) |v| {
-            try result.put(tmp, v + 1);
+            try result.put(tmp_s, v + 1);
         } else {
+            // need to make our own copy of the tmp string, or it will get freed before we are done
+            // only needs to be done ONCE per unique key, or we'll have a memory leak
+            const tmp = std.fmt.allocPrint(allocator, "{s}", .{tmp_s}) catch "ERROR";
             try result.put(tmp, 1);
         }
     }
@@ -68,4 +69,16 @@ test "count one of each word" {
     try std.testing.expectEqual(@as(?u32, 1), map.get("one"));
     try std.testing.expectEqual(@as(?u32, 1), map.get("of"));
     try std.testing.expectEqual(@as(?u32, 1), map.get("each"));
+}
+
+test "multiple occurrences of a word" {
+    const s = "one fish two fish red fish blue fish";
+    var map = try countWords(test_allocator, s);
+    defer freeKeysAndDeinit(&map);
+    try std.testing.expectEqual(@as(u32, 5), map.count());
+    try std.testing.expectEqual(@as(?u32, 1), map.get("one"));
+    try std.testing.expectEqual(@as(?u32, 4), map.get("fish"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("two"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("red"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("blue"));
 }
