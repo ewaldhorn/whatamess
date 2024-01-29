935bce88ece186b96c3ee7ae05d5e55024a98b59
Ewald Horn <ewaldhorn@gmail.com>
2024-01-29 16:50:45 +0200

    Word Count in Zig done

--- exercism/zig/word_count.zig
+++ exercism/zig/word_count.zig
@@ -7,40 +7,42 @@ pub fn countWords(allocator: mem.Allocator, s: []const u8) !std.StringHashMap(u3
     const working_string = try std.ascii.allocLowerString(allocator, s);
     defer allocator.free(working_string);
 
-    var splitter = mem.splitAny(u8, working_string, " :!?\t\n,.");
+    var splitter = mem.splitAny(u8, working_string, " :!?\t\n,.&@$%^");
     var result = std.StringHashMap(u32).init(allocator);
-    
+
     // std.debug.print("\n\nWorking with: \"{s}\"\n\n", .{working_string});
-    
+
     while (splitter.next()) |tmp_s| {
-        // std.debug.print("Now looking for: {s}\n", .{tmp});
-        const value = result.get(tmp_s);
+        // std.debug.print("Now looking for: {s}\n", .{tmp_s});
+        if (tmp_s.len == 0) {
+            // nothing to do this iteration, move along
+            continue;
+        }
+
+        const word_start: usize = if (tmp_s[0] == '\'') 1 else 0;
+        const word_end: usize = if (tmp_s[tmp_s.len - 1] == '\'') tmp_s.len - 1 else tmp_s.len;
+
+        if (word_start >= word_end) {
+            // nothing to do, empty
+            continue;
+        }
+
+        const value = result.get(tmp_s[word_start..word_end]);
         if (value) |v| {
-            try result.put(tmp_s, v + 1);
+            try result.put(tmp_s[word_start..word_end], v + 1);
         } else {
             // need to make our own copy of the tmp string, or it will get freed before we are done
             // only needs to be done ONCE per unique key, or we'll have a memory leak
-            const tmp = std.fmt.allocPrint(allocator, "{s}", .{tmp_s}) catch "ERROR";
+            const tmp = std.fmt.allocPrint(allocator, "{s}", .{tmp_s[word_start..word_end]}) catch "ERROR";
             try result.put(tmp, 1);
         }
     }
 
-    // var iterator = result.keyIterator();
-    // while (iterator.next()) |entry| {
-    //     std.debug.print("Entry: {s}\n", .{entry.*});
-    // }
-
-    // std.debug.print("\n>>>>>>>>>>>>>>>>>>>\nResult: Count: {d}\n", .{result.count()});
-    // const word = result.get("word");
-    // if (word) |w| {
-    //     std.debug.print("Found {d} occurence(s) of 'word'.\n", .{w});
-    // }
-    
     return result;
 }
 
-// ________________________________________________________________________________________________ 
-// ========================================================================================== TESTS 
+// ________________________________________________________________________________________________
+// ========================================================================================== TESTS
 const test_allocator = std.testing.allocator;
 
 fn freeKeysAndDeinit(self: *std.StringHashMap(u32)) void {
@@ -75,10 +77,62 @@ test "multiple occurrences of a word" {
     const s = "one fish two fish red fish blue fish";
     var map = try countWords(test_allocator, s);
     defer freeKeysAndDeinit(&map);
+
     try std.testing.expectEqual(@as(u32, 5), map.count());
     try std.testing.expectEqual(@as(?u32, 1), map.get("one"));
     try std.testing.expectEqual(@as(?u32, 4), map.get("fish"));
     try std.testing.expectEqual(@as(?u32, 1), map.get("two"));
     try std.testing.expectEqual(@as(?u32, 1), map.get("red"));
     try std.testing.expectEqual(@as(?u32, 1), map.get("blue"));
+}
+
+test "handles cramped lists" {
+    const s = "one,two,three";
+    var map = try countWords(test_allocator, s);
+    defer freeKeysAndDeinit(&map);
+
+    try std.testing.expectEqual(@as(u32, 3), map.count());
+    try std.testing.expectEqual(@as(?u32, 1), map.get("one"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("two"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("three"));
+}
+
+test "handles expanded lists" {
+    const s = "one,\ntwo,\nthree";
+    var map = try countWords(test_allocator, s);
+    defer freeKeysAndDeinit(&map);
+
+    try std.testing.expectEqual(@as(u32, 3), map.count());
+    try std.testing.expectEqual(@as(?u32, 1), map.get("one"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("two"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("three"));
+}
+
+test "ignore punctuation" {
+    const s = "car: carpet as java: javascript!!&@$%^&";
+    var map = try countWords(test_allocator, s);
+    defer freeKeysAndDeinit(&map);
+
+    try std.testing.expectEqual(@as(u32, 5), map.count());
+    try std.testing.expectEqual(@as(?u32, 1), map.get("car"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("carpet"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("as"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("java"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("javascript"));
+}
+
+test "with apostrophes" {
+    const s = "'First: don't laugh. Then: don't cry. You're getting it.'";
+    var map = try countWords(test_allocator, s);
+    defer freeKeysAndDeinit(&map);
+
+    try std.testing.expectEqual(@as(u32, 8), map.count());
+    try std.testing.expectEqual(@as(?u32, 1), map.get("first"));
+    try std.testing.expectEqual(@as(?u32, 2), map.get("don't"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("laugh"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("then"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("cry"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("you're"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("getting"));
+    try std.testing.expectEqual(@as(?u32, 1), map.get("it"));
 }
