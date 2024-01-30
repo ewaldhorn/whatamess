586b3f36e67d46a04fa8db46241f5977db678a58
Ewald Horn <ewaldhorn@gmail.com>
2024-01-30 17:43:57 +0200

    Working through array lists

--- languages/zig/experiments/array_list/array_lists.zig
+++ languages/zig/experiments/array_list/array_lists.zig
@@ -87,3 +87,53 @@ test "can do stuff in reverse" {
     try expect(my_second_list.items[my_second_list.items.len-5] == ' ');
     try expect(my_second_list.items[my_second_list.items.len-6] == 'i');
 }
+
+test "can append a slice" {
+    var my_list = std.ArrayList(u8).init(allocator);
+    defer my_list.deinit();
+
+    try my_list.appendSlice("This is a slice");
+    try my_list.appendSlice(" ");
+    try my_list.appendSlice("into a whole.");
+
+    try expect(my_list.items.len == "This is a slice into a whole.".len);
+}
+
+test "remove and swap in an array list" {
+    var my_list = std.ArrayList(i32).init(allocator);
+    defer my_list.deinit();
+
+    var n : i32 = 1;
+     while (n <= 10) : (n += 1){
+        try my_list.append(n);
+    }
+
+    try expect(my_list.items.len == 10);
+
+    _ = my_list.orderedRemove(5);
+
+    try expect(my_list.items.len == 9);
+    try expect(my_list.items[0] == 1);
+    try expect(my_list.items[8] == 10);
+
+    const result = my_list.swapRemove(0);
+
+    try expect(result == 1);
+    try expect(my_list.items.len == 8);
+    try expect(my_list.items[0] == 10);
+}
+
+test "pre-allocat memory for the array list" {
+    var my_list = try std.ArrayList(u8).initCapacity(allocator, 12);
+    defer my_list.deinit();
+
+    try expect(my_list.capacity == 12);
+    try expect(my_list.items.len == 0);
+
+    for ("YOLO") |c| {
+        my_list.appendAssumeCapacity(c);
+    }
+
+    try expect(my_list.capacity == 12);
+    try expect(my_list.items.len == 4);
+}
