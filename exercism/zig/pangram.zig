e86d3c5ec06c87990bbec8ddd0b50557a7c4b3f0
Ewald Horn <ewaldhorn@gmail.com>
2024-01-31 13:57:11 +0200

    Zig and Exercism

--- exercism/zig/pangram.zig
+++ exercism/zig/pangram.zig
@@ -9,10 +9,15 @@ pub fn isPangram(str: []const u8) bool {
         return false;
     }
 
+    const diff : u8 = 'a' - 'A';
+
     for (str) |c| {
         switch (c) {
             'a'...'z' => {
                 counter[c - 'a'] += 1;
+            },
+            'A'...'Z' => {
+                counter[c + diff - 'a'] += 1;
             },
             else => continue
         }
@@ -29,6 +34,8 @@ pub fn isPangram(str: []const u8) bool {
 
 // ________________________________________________________________________________________________ 
 // ========================================================================================== TESTS 
+// A 65
+// a 97 32
 const testing = std.testing;
 
 test "empty sentence" {
@@ -47,30 +54,30 @@ test "missing the letter 'x'" {
     try testing.expect(!isPangram("a quick movement of the enemy will jeopardize five gunboats"));
 }
 
-// test "missing the letter 'h'" {
-//     try testing.expect(!isPangram("five boxing wizards jump quickly at it"));
-// }
+test "missing the letter 'h'" {
+    try testing.expect(!isPangram("five boxing wizards jump quickly at it"));
+}
 
-// test "with underscores" {
-//     try testing.expect(isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
-// }
+test "with underscores" {
+    try testing.expect(isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
+}
 
-// test "with numbers" {
-//     try testing.expect(isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
-// }
+test "with numbers" {
+    try testing.expect(isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
+}
 
-// test "missing letters replaced by numbers" {
-//     try testing.expect(!isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"));
-// }
+test "missing letters replaced by numbers" {
+    try testing.expect(!isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"));
+}
 
-// test "mixed case and punctuation" {
-//     try testing.expect(isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
-// }
+test "mixed case and punctuation" {
+    try testing.expect(isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
+}
 
-// test "a-m and A-M are 26 different characters but not a pangram" {
-//     try testing.expect(!isPangram("abcdefghijklm ABCDEFGHIJKLM"));
-// }
+test "a-m and A-M are 26 different characters but not a pangram" {
+    try testing.expect(!isPangram("abcdefghijklm ABCDEFGHIJKLM"));
+}
 
-// test "non-alphanumeric printable ASCII" {
-//     try testing.expect(!isPangram(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"));
-// }
+test "non-alphanumeric printable ASCII" {
+    try testing.expect(!isPangram(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"));
+}
