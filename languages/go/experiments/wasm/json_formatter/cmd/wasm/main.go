4bff20a71de613eef89bbc7cc37f893bc9668a6a
Ewald Horn <ewaldhorn@gmail.com>
2024-02-11 18:10:13 +0200

    Adding some simple smoke tests

--- languages/go/experiments/wasm/json_formatter/cmd/wasm/main.go
+++ languages/go/experiments/wasm/json_formatter/cmd/wasm/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Go Web Assembly")
+}
+
+const PREFIX = ""
+
+func prettyJson(input string) (string, error) {
+	var raw any
+	if err := json.Unmarshal([]byte(input), &raw); err != nil {
+		return "", err
+	}
+	
+	pretty, err := json.MarshalIndent(raw, PREFIX, "  ")
+	if err != nil {
+		return "", err
+	}
+
+	return string(pretty), nil
 }
