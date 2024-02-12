f2b269fa3873884d615309d1338c3ecb0f341422
Ewald Horn <ewaldhorn@gmail.com>
2024-02-12 14:09:41 +0200

    Alternative solutions

---
+++ exercism/zig/isogram_ii.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+const allocator = std.heap.page_allocator;
+
+pub fn isIsogram(str: []const u8) bool {
+    var result = true;
+
+    if (str.len < 2) {
+        return true;
+    }
+
+    const target_str = std.ascii.allocLowerString(allocator, str) catch "ERROR Creating lower-case string";
+    defer allocator.free(target_str);
+
+    for (target_str) |character| {
+        if (character == ' ' or character == '-') {
+            // spaces and hyphens do not count, skip them.
+            continue;
+        }
+
+        const needle = std.fmt.allocPrint(allocator, "{c}", .{character}) catch "ERROR creating needle";
+        defer allocator.free(needle);
+
+        if (std.mem.count(u8, target_str, needle) > 1) {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+
+// ________________________________________________________________________________________________
+// ========================================================================================== TESTS
+const expect = std.testing.expect;
+
+test "empty string - TRUE" {
+    const result = isIsogram("");
+
+    try expect(result == true);
+}
+
+test "one character string - TRUE" {
+    const result = isIsogram("a");
+
+    try expect(result == true);
+}
+
+test "isogram with only lower case characters - TRUE" {
+    try expect(isIsogram("isogram"));
+}
+
+test "word with one duplicated character - FALSE" {
+    try expect(!isIsogram("eleven"));
+}
+
+test "word with one duplicated character from the end of the alphabet - FALSE" {
+    try expect(!isIsogram("zzyzx"));
+}
+
+test "word with duplicated character in mixed case - FALSE" {
+    try expect(!isIsogram("Alphabet"));
+}
---
+++ exercism/zig/pangram_ii.zig
@@ -0,0 +1,78 @@
+const std = @import("std");
+
+pub fn isPangram(str: []const u8) bool {
+    var counter = [_]u8{0} ** 26;
+    
+    
+    if (str.len < 26) {
+        // don't bother, this will never work
+        return false;
+    }
+
+    for (str) |c| {
+        switch (std.ascii.toLower(c)) {
+            'a'...'z' => {
+                counter[std.ascii.toLower(c) - 'a'] += 1;
+            },
+            else => continue
+        }
+    }
+
+    var answer = true;
+
+    for (counter) |v| {
+        answer = answer and (v >= 1);
+    }
+
+    return answer;
+}
+
+// ________________________________________________________________________________________________ 
+// ========================================================================================== TESTS 
+// A 65
+// a 97 32
+const testing = std.testing;
+
+test "empty sentence" {
+    try testing.expect(!isPangram(""));
+}
+
+test "perfect lower case" {
+    try testing.expect(isPangram("abcdefghijklmnopqrstuvwxyz"));
+}
+
+test "only lower case" {
+    try testing.expect(isPangram("the quick brown fox jumps over the lazy dog"));
+}
+
+test "missing the letter 'x'" {
+    try testing.expect(!isPangram("a quick movement of the enemy will jeopardize five gunboats"));
+}
+
+test "missing the letter 'h'" {
+    try testing.expect(!isPangram("five boxing wizards jump quickly at it"));
+}
+
+test "with underscores" {
+    try testing.expect(isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
+}
+
+test "with numbers" {
+    try testing.expect(isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
+}
+
+test "missing letters replaced by numbers" {
+    try testing.expect(!isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"));
+}
+
+test "mixed case and punctuation" {
+    try testing.expect(isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
+}
+
+test "a-m and A-M are 26 different characters but not a pangram" {
+    try testing.expect(!isPangram("abcdefghijklm ABCDEFGHIJKLM"));
+}
+
+test "non-alphanumeric printable ASCII" {
+    try testing.expect(!isPangram(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"));
+}
