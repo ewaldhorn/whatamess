c0e697adc1b7fca4d14a799a30baae768a6ba43b
Ewald Horn <ewaldhorn@gmail.com>
2024-02-17 08:18:13 +0200

    Zig

--- languages/zig/learning/strings/strings.zig
+++ languages/zig/learning/strings/strings.zig
@@ -29,6 +29,7 @@ test "combining strings" {
 test "finding stuff in strings" {
     const haystack = "this string contains a few words";
 
+    try expect(std.mem.indexOf(u8, haystack, "this") == 0);
     try expect(std.mem.indexOf(u8, haystack, "string") == 5);
     try expect(std.mem.lastIndexOf(u8, haystack, "s") == haystack.len - 1);
 }
