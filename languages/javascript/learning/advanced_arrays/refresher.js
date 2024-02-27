39c4b8af74e5dcadbb3ca275829060ff5232409c
Ewald Horn <ewaldhorn@gmail.com>
2024-02-27 08:47:04 +0200

    Array refresher

---
+++ languages/javascript/learning/advanced_arrays/refresher.js
@@ -0,0 +1,17 @@
+/*
+    Some basic JavaScript array refreshers.
+ */
+console.log("\n");
+
+const items = ["cat", "dog"];
+console.log("Current:", items, " (" + items.length + ")");
+items.push("parrot");
+console.log("Updated:", items, " (" + items.length + ")");
+
+console.log("\nItems:");
+for (let i = 0; i < items.length; i++) {
+    console.log((i + 1), ":", items[i]);
+}
+
+
+console.log("\n");
