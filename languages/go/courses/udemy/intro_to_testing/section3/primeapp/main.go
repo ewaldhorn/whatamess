2427b2e75b3a88ce0c05f603947d588ce5d4ffa3
Ewald Horn <ewaldhorn@gmail.com>
2024-02-29 14:28:51 +0200

    Go refreshing

---
+++ languages/go/courses/udemy/intro_to_testing/section3/primeapp/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 2
+	_, msg := isPrime(n)
+
+	fmt.Println(msg)
+}
+
+func isPrime(number int) (bool, string) {
+	if number < 2 {
+		return false, fmt.Sprintf("%d is not a prime number.", number)
+	}
+
+	// use mod to figure out if it's a prime
+	for i := 2; i <= number/2; i++ {
+		if number%i == 0 {
+			return false, fmt.Sprintf("%d is not a prime number, divisible by %d.", number, i)
+		}
+	}
+
+	return true, fmt.Sprintf("%d is a prime number!", number)
+}
