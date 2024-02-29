305e51ab89e1a24e28adaa31989a4a56c83276aa
Ewald Horn <ewaldhorn@gmail.com>
2024-02-29 13:25:01 +0200

    Basic Go and Ebitengine config working

--- languages/go/ebitengine/bouncy/main.go
+++ languages/go/ebitengine/bouncy/main.go
@@ -1,30 +1,115 @@
 package main
 
 import (
+	"image/color"
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+const (
+	screenWidth  = 800
+	screenHeight = 600
+	count        = 2000
+)
+
+type Entity struct {
+	xPos, yPos int32
+	xDir, yDir bool
+	colour     color.RGBA
+}
+
+func (s *Entity) Init() {
+	s.xPos = int32(rand.Intn(screenWidth))
+	s.yPos = int32(rand.Intn(screenHeight))
+	s.xDir = rand.Intn(50) > 25
+	s.yDir = rand.Intn(50) > 25
+
+	s.colour = color.RGBA{R: uint8(rand.Intn(255)), G: uint8(rand.Intn(255)), B: uint8(rand.Intn(255)), A: 0xff}
+}
+
+func (s *Entity) RefreshColour() {
+	s.colour = color.RGBA{R: uint8(rand.Intn(255)), G: uint8(rand.Intn(255)), B: uint8(rand.Intn(255)), A: 0xff}
+}
+
+func (s *Entity) Update() {
+	if s.xDir {
+		s.xPos += 1
+	} else {
+		s.xPos -= 1
+	}
+	if s.yDir {
+		s.yPos += 1
+	} else {
+		s.yPos -= 1
+	}
+
+	if s.xPos < 1 || screenWidth < s.xPos {
+		s.xDir = !s.xDir
+	}
+
+	if s.yPos < 1 || screenHeight < s.yPos {
+		s.yDir = !s.yDir
+	}
+
+}
+
+func (s *Entity) Draw(screen []byte) {
+	if s.xPos < 1 || s.xPos >= screenWidth || s.yPos < 1 || s.yPos >= screenHeight {
+		s.RefreshColour()
+	} else {
+		offset := s.xPos + (s.yPos * screenWidth)
+
+		screen[4*offset] = s.colour.R
+		screen[4*offset+1] = s.colour.G
+		screen[4*offset+2] = s.colour.B
+		screen[4*offset+3] = s.colour.A
+	}
+}
+
+type Game struct {
+	stars  [count]Entity
+	pixels []byte
+}
+
+func NewGame() *Game {
+	g := &Game{}
+	for i := 0; i < count; i++ {
+		g.stars[i].Init()
+	}
+	return g
+}
 
 func (g *Game) Update() error {
+	for i := 0; i < count; i++ {
+		g.stars[i].Update()
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	if g.pixels == nil {
+		g.pixels = make([]byte, screenWidth*screenHeight*4)
+	}
+
+	for i := 0; i < count; i++ {
+		g.stars[i].Draw(g.pixels)
+	}
+
+	screen.WritePixels(g.pixels)
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	rand.Seed(time.Now().UnixNano())
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Bouncy (Ebitengine Experiment)")
+	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
 }
