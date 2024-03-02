be723e2c02114db5c2960cea7beec673e6732260
Ewald Horn <ewaldhorn@gmail.com>
2024-03-02 16:14:45 +0200

    Accepting user input

--- languages/go/courses/udemy/intro_to_testing/section3/primeapp/main.go
+++ languages/go/courses/udemy/intro_to_testing/section3/primeapp/main.go
@@ -1,8 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
+	sayWelcome()
+
+	// create channel to indicate when the user wants to quit
+	allDoneChannel := make(chan bool)
+
+	// start a goroutine to read user input and run program
+	go readUserInput(allDoneChannel)
+	// block until the doneChannel gets a value
+	<-allDoneChannel
+
+	// close channel
+	close(allDoneChannel)
+
+	sayGoodbye()
+}
+
+func sayWelcome() { fmt.Println("\nHello! Pick a number.\nOr (Q)uit : "); prompt() }
+func sayGoodbye() { fmt.Println("\nThat was fun, let's do it again soon!") }
+
+func prompt() {
+	fmt.Print("-> ")
+}
+
+func readUserInput(signal chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			signal <- true
+			return
+		}
+
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	scanner.Scan()
+
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	number, err := strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		return "Please enter a whole, positive number", false
+	}
+
+	_, msg := isPrime(number)
+
+	return msg, false
 }
 
 func isPrime(number int) (bool, string) {
