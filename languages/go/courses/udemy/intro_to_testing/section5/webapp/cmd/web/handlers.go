3f07e9de00e4067ed6eda173060f9be767a2a292
Ewald Horn <ewaldhorn@gmail.com>
2024-03-04 12:05:16 +0200

    Introduce page templates

--- languages/go/courses/udemy/intro_to_testing/section5/webapp/cmd/web/handlers.go
+++ languages/go/courses/udemy/intro_to_testing/section5/webapp/cmd/web/handlers.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is a page with (%s) embedded!", "embedded")
+	_ = app.render(w, r, "pages/home.page.gohtml", &TemplateData{})
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, pageTemplate string, data *TemplateData) error {
+	parsedTemplate, err := template.ParseFiles("./templates/" + pageTemplate)
+	if err != nil {
+		http.Error(w, "Error - Bad Request", http.StatusBadRequest)
+		return err
+	}
+
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
