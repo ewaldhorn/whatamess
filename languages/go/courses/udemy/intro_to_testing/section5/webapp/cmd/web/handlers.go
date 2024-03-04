c5c996bc684dbf2973ee6ce631ead3cb694de5dd
Ewald Horn <ewaldhorn@gmail.com>
2024-03-04 11:42:11 +0200

    Add and register a handler for the home page (/) route

---
+++ languages/go/courses/udemy/intro_to_testing/section5/webapp/cmd/web/handlers.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this is a page with (%s) embedded!", "embedded")
+}
