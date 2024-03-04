8ac5f1ac72f8d2649ab7441d26b8c34b0f727507
Ewald Horn <ewaldhorn@gmail.com>
2024-03-04 15:16:58 +0200

    Making handlers testable

---
+++ languages/go/courses/udemy/intro_to_testing/section5/webapp/cmd/web/handlers_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_application_handlers(t *testing.T) {
+	var tests = []struct {
+		name               string
+		url                string
+		expectedStatusCode int
+	}{
+		{"home", "/", http.StatusOK},
+		{"not found", "/thisIsNotAvailable", http.StatusNotFound},
+	}
+
+	var app application
+	routes := app.routes()
+
+	testServer := httptest.NewTLSServer(routes)
+	defer testServer.Close()
+
+	pathToTemplates = "./../../templates/"
+
+	for _, test := range tests {
+		rsp, err := testServer.Client().Get(testServer.URL + test.url)
+		if err != nil {
+			t.Log(err)
+			t.Fatal(err)
+		}
+
+		if rsp.StatusCode != test.expectedStatusCode {
+			t.Errorf("%s failed with a status code of %d, expected %d", test.name, rsp.StatusCode, test.expectedStatusCode)
+		}
+	}
+}
