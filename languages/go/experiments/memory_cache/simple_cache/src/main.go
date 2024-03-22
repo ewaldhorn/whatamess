c33db59a6621b55589f2bd3f7d2d075ea0583b9e
Ewald Horn <ewaldhorn@gmail.com>
2024-03-22 13:44:56 +0200

    Different cache strategies

--- languages/go/experiments/memory_cache/simple_cache/src/main.go
+++ languages/go/experiments/memory_cache/simple_cache/src/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"simple_cache/src/cache"
+	"time"
 )
 
 func main() {
 	println("Testing Cache.\n")
 	testCache()
 	println("\nDone.")
+	println("\nTesting Timed Cache.\n")
+	testTimedCache()
+	print("\nDone.\n")
 }
 
 func testCache() {
@@ -46,4 +50,49 @@ func testCache() {
 	} else {
 		fmt.Println("Key 'one' not found in the cache (after removal)")
 	}
+}
+
+func testTimedCache() {
+	// Create a new TTLCache instance
+	myTTLCache := cache.NewTTL[string, int]()
+
+	// Set key-value pairs with TTL in the cache
+	myTTLCache.Set("one", 1, 5*time.Second)
+	myTTLCache.Set("two", 2, 10*time.Second)
+	myTTLCache.Set("three", 3, 15*time.Second)
+
+	// Retrieve values from the cache
+	value, found := myTTLCache.Get("two")
+	if found {
+		fmt.Printf("Value for key 'two': %v\n", value)
+	} else {
+		fmt.Println("Key 'two' not found in the cache or has expired")
+	}
+
+	// Wait for a while to allow some items to expire
+	println("Wait for 7 seconds...")
+	time.Sleep(7 * time.Second)
+
+	// Try to retrieve an expired key
+	expiredValue, found := myTTLCache.Get("one")
+	if found {
+		fmt.Printf("Value for key 'one': %v\n", expiredValue)
+	} else {
+		fmt.Println("Key 'one' not found in the cache or has expired")
+	}
+
+	// Pop a key from the cache
+	poppedValue, found := myTTLCache.Pop("two")
+	if found {
+		fmt.Printf("Popped value for key 'two': %v\n", poppedValue)
+	} else {
+		fmt.Println("Key 'two' not found in the cache or has expired")
+	}
+
+	// Wait for a while to allow all items to expire
+	println("Wait another 15 seconds...")
+	time.Sleep(15 * time.Second)
+
+	// Remove a key from the cache
+	println("Cache is empty:", myTTLCache.IsEmpty())
 }
