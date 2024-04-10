0d4c002508334320cfae9c0839a7ffd54ece07a1
Ewald Horn <ewaldhorn@gmail.com>
2024-04-10 22:11:23 +0200

    Added tests

--- languages/go/learning/strings/palindrome/main.go
+++ languages/go/learning/strings/palindrome/main.go
@@ -1,19 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"unicode/utf8"
 )
-
-func main() {
-	s1 := "pop"
-	s2 := "topspot"
-	s3 := "nope"
-
-	fmt.Printf("%v is a palindrome: %t\n", s1, isPalindrome(s1))
-	fmt.Printf("%v is a palindrome: %t\n", s2, isPalindrome(s2))
-	fmt.Printf("%v is a palindrome: %t\n", s3, isPalindrome(s3))
-}
 
 func isPalindrome(s string) bool {
 	runeCount := utf8.RuneCountInString(s)
---
+++ languages/go/learning/strings/palindrome/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{name: "mom", input: "mom", expected: true},
+		{name: "topspot", input: "topspot", expected: true},
+		{name: "nope", input: "nope", expected: false},
+	}
+
+	for _, test := range tests {
+		if test.expected != isPalindrome(test.input) {
+			t.Errorf("%s failed: expected %t, got %t", test.name, test.expected, isPalindrome(test.input))
+		}
+	}
+}
