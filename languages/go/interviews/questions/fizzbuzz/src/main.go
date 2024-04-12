368959274e9f4e5156ba0e761c78d58e94e2c6d9
Ewald Horn <ewaldhorn@gmail.com>
2024-04-12 16:36:46 +0200

    Add a solution that passes all the tests

--- languages/go/interviews/questions/fizzbuzz/src/main.go
+++ languages/go/interviews/questions/fizzbuzz/src/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func check(input int) string {
-	return ""
+	isThrees := input%3 == 0
+	isFives := input%5 == 0
+
+	if isThrees && isFives {
+		return "Fizz Buzz"
+	}
+
+	if isThrees {
+		return "Fizz"
+	}
+
+	if isFives {
+		return "Buzz"
+	}
+
+	return fmt.Sprintf("%d", input)
 }
