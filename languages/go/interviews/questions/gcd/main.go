2beb25e7750c77df11cb1f44776f1bc3a613a606
Ewald Horn <ewaldhorn@gmail.com>
2024-05-13 10:14:55 +0200

    Added more solutions and tests to GCD

--- languages/go/interviews/questions/gcd/main.go
+++ languages/go/interviews/questions/gcd/main.go
@@ -1,12 +1,36 @@
 package main
 
 /*
-Calculate the GCD of two numbers using recursion.
+Calculate the GCD of two numbers using various algorithms.
 */
-func calculateGCD(left int, right int) int {
+
+// -------------------------------------------------------------------------------------- Recursion
+func calculateGCD_Recursion(left int, right int) int {
 	if right != 0 {
-		return calculateGCD(right, left%right)
+		return calculateGCD_Recursion(right, left%right)
 	} else {
 		return left
 	}
+}
+
+// -------------------------------------------------------------------------------------- Euclidian
+func calculateGCD_Euclidean(left, right int) int {
+	for left != right {
+		if left > right {
+			left -= right
+		} else {
+			right -= left
+		}
+	}
+
+	return left
+}
+
+// -------------------------------------------------------------------------------------- Remainder
+func calculateGCD_Remainder(left, right int) int {
+	for right != 0 {
+		left, right = right, left%right
+	}
+
+	return left
 }
