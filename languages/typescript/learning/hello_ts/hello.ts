3a0bbd9b0721f90cfcc5c37f51828ca41affc6e5
Ewald Horn <ewaldhorn@gmail.com>
2024-05-14 12:58:43 +0200

    Ensure TSC is working

---
+++ languages/typescript/learning/hello_ts/hello.ts
@@ -0,0 +1,12 @@
+let user = { name: "Bob", age: 30 };
+
+console.log();
+console.log("Hello, world!");
+console.log(`User name: ${user.name} (${user.age}).`);
+
+// ------------------------------------------------------------------------------------------------
+let welcomeMessage: string;
+
+welcomeMessage = "This is a welcome message";
+
+console.log(`The message is "${welcomeMessage}".\n`);
