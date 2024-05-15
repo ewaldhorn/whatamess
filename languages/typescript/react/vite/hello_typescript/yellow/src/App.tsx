86de5318eb0fad1ef997dd74df5a777b373bcd4c
Ewald Horn <ewaldhorn@gmail.com>
2024-05-15 19:33:04 +0200

    React Revisit

--- languages/typescript/react/vite/hello_typescript/yellow/src/App.tsx
+++ languages/typescript/react/vite/hello_typescript/yellow/src/App.tsx
@@ -1,14 +1,51 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 interface AppProperties {
   title: string;
 }
 
+interface Users {
+  name: {
+    first: string;
+    last: string;
+  };
+  login: {
+    uuid: string;
+  };
+  email: string;
+}
+
 const App: FC<AppProperties> = ({ title }) => {
+  const [users, setUsers] = useState<Users[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const { data } = await axios.get('https://randomuser.me/api/?results=10');
+        setUsers(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+
   return <div>
     <h1>{title}</h1>
     And this is my app.
+    <ul className="userList">
+      {users.map(({ login, name, email }) => {
+        return <li key={login.uuid}>
+          <div>
+            Name: {name.first} {name.last}
+          </div>
+          <div>Email: {email}</div>
+          <hr />
+        </li>
+      })}
+    </ul>
   </div>;
 }
 
