114fe3e2cc6035bcb1801ea722f73a2377b003f2
Ewald Horn <ewaldhorn@gmail.com>
2024-05-18 12:39:47 +0200

    Added support for an external dictionary

---
+++ languages/go/courses/udemy/programming_with_go/section8/hangman/dictionary.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var dictionary = []string{}
+
+// --------------------------------------------------------------------------------- loadDictionary
+func loadDictionary() {
+	input, err := os.Open("dictionary/wordlist.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	scanner := bufio.NewScanner(input)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		dictionary = append(dictionary, scanner.Text())
+	}
+
+	input.Close()
+}
+
+// ------------------------------------------------------------------------------- reportDictionary
+func reportDictionary() {
+	for pos, entry := range dictionary {
+		fmt.Printf("%2d: %s\n", pos+1, entry)
+	}
+}
