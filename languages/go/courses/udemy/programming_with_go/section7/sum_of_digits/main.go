6fb77714b4b1036bf2323875c0f1b49ca7a37a92
Ewald Horn <ewaldhorn@gmail.com>
2024-05-25 15:21:06 +0200

    Expanded tests

--- languages/go/courses/udemy/programming_with_go/section7/sum_of_digits/main.go
+++ languages/go/courses/udemy/programming_with_go/section7/sum_of_digits/main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("The sum of the digits for 1    is:", sumOfDigits(1))
-	fmt.Println("The sum of the digits for 15   is:", sumOfDigits(15))
-	fmt.Println("The sum of the digits for 123  is:", sumOfDigits(123))
-	fmt.Println("The sum of the digits for 1234 is:", sumOfDigits(1234))
+	fmt.Println("The sum of the digits for 1      is:", sumOfDigits(1))
+	fmt.Println("The sum of the digits for 15     is:", sumOfDigits(15))
+	fmt.Println("The sum of the digits for 123    is:", sumOfDigits(123))
+	fmt.Println("The sum of the digits for 1234   is:", sumOfDigits(1234))
+	fmt.Println("The sum of the digits for 12345  is:", sumOfDigits(12345))
+	fmt.Println("The sum of the digits for 123456 is:", sumOfDigits(123456))
 }
 
 func sumOfDigits(src int) int {
