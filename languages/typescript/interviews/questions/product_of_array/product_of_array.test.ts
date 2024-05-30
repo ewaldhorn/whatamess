893c03934cef7cc91431eea8cf5a2352a4de3620
Ewald Horn <ewaldhorn@gmail.com>
2024-05-30 21:45:22 +0200

    Using map

--- languages/typescript/interviews/questions/product_of_array/product_of_array.test.ts
+++ languages/typescript/interviews/questions/product_of_array/product_of_array.test.ts
@@ -1,16 +1,41 @@
 import { expect, test, describe } from "bun:test";
-import { productExceptSelfBasic } from "./product_of_array";
+import { productExceptSelfBasic, productExceptSelfImproved, productExceptSelfMapped } from "./product_of_array";
 
 describe("product of array", () => {
   test("[1,2,3,4]", () => {
     let expected = [24, 12, 8, 6];
     let result = productExceptSelfBasic([1, 2, 3, 4]);
-    console.log("Result:", result);
     expect(result).toEqual(expected);
   });
 
   test("[-1, 1, 0, -3, 3]", () => {
     let expected = [0, 0, 9, 0, 0];
     expect(productExceptSelfBasic([-1, 1, 0, -3, 3])).toEqual(expected);
+  });
+});
+
+describe("product of array improved", () => {
+  test("[1,2,3,4]", () => {
+    let expected = [24, 12, 8, 6];
+    let result = productExceptSelfImproved([1, 2, 3, 4]);
+    expect(result).toEqual(expected);
+  });
+
+  test("[-1, 1, 0, -3, 3]", () => {
+    let expected = [0, 0, 9, 0, 0];
+    expect(productExceptSelfImproved([-1, 1, 0, -3, 3])).toEqual(expected);
+  });
+});
+
+describe("product of array using map", () => {
+  test("[1,2,3,4]", () => {
+    let expected = [24, 12, 8, 6];
+    let result = productExceptSelfMapped([1, 2, 3, 4]);
+    expect(result).toEqual(expected);
+  });
+
+  test("[-1, 1, 0, -3, 3]", () => {
+    let expected = [0, 0, 9, 0, 0];
+    expect(productExceptSelfMapped([-1, 1, 0, -3, 3])).toEqual(expected);
   });
 });
--- languages/typescript/interviews/questions/product_of_array/product_of_array.ts
+++ languages/typescript/interviews/questions/product_of_array/product_of_array.ts
@@ -21,3 +21,35 @@ export function productExceptSelfBasic(numbers: number[]): number[] {
 
   return result;
 }
+
+export function productExceptSelfImproved(numbers: number[]): number[] {
+  const count = numbers.length;
+  const result = new Array<number>(count);
+
+  // work from left to right
+  for (let i = 0, left = 1; i < count; ++i) {
+    result[i] = left;
+    left *= numbers[i];
+  }
+
+  // now from right to left
+  for (let i = count - 1, right = 1; i >= 0; --i) {
+    result[i] *= right;
+    right *= numbers[i];
+  }
+
+  // because JavaScript!
+  for (let i = 0; i < count; i++) {
+    if (result[i] == -0) {
+      result[i] = 0;
+    }
+  }
+
+  return result;
+}
+
+export function productExceptSelfMapped(numbers: number[]): number[] {
+  return numbers.map(
+    (_, i) => numbers.reduce((pre, val, j) => pre * (i === j ? 1 : val), 1)).map(
+      (v) => { if (v == -0) { return 0 } else { return v } });
+}
