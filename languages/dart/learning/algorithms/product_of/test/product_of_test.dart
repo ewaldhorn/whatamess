aadbc9332718c5365f51963a1aa17aca6a31fa34
Ewald Horn <ewaldhorn@gmail.com>
2024-06-12 11:30:58 +0200

    Expanded tests

--- languages/dart/learning/algorithms/product_of/test/product_of_test.dart
+++ languages/dart/learning/algorithms/product_of/test/product_of_test.dart
@@ -8,4 +8,18 @@ void main() {
 
     expect(productOfArray(input), answer);
   });
+
+  test("4,3,2,1 => 6,8,12,24", () {
+    var input = [4, 3, 2, 1];
+    var answer = [6, 8, 12, 24];
+
+    expect(productOfArray(input), answer);
+  });
+
+  test("10,3,5,6,2 => 180,600,360,300,900", () {
+    var input = [10, 3, 5, 6, 2];
+    var answer = [180, 600, 360, 300, 900];
+
+    expect(productOfArray(input), answer);
+  });
 }
