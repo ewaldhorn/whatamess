a2d5b6032df5a5cff7d18e737b769d6e574015fc
Ewald Horn <ewaldhorn@gmail.com>
2024-06-12 10:31:17 +0200

    Fix a silly issue in the main.dart file

--- languages/dart/learning/algorithms/anagram/bin/anagram.dart
+++ languages/dart/learning/algorithms/anagram/bin/anagram.dart
@@ -1,5 +1,3 @@
-import 'package:anagram/anagram.dart' as anagram;
-
 void main(List<String> arguments) {
-  print('Hello world: ${anagram.calculate()}!');
+  print("Best run the tests instead!");
 }
