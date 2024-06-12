97f44236bed8d8b4d081b76b0b511798cd817213
Ewald Horn <ewaldhorn@gmail.com>
2024-06-12 10:31:35 +0200

    Start a new solution

---
+++ languages/dart/learning/algorithms/product_of/bin/product_of.dart
@@ -0,0 +1,5 @@
+import 'package:product_of/product_of.dart' as product_of;
+
+void main(List<String> arguments) {
+  print('Hello world: ${product_of.calculate()}!');
+}
---
+++ languages/dart/learning/algorithms/product_of/test/product_of_test.dart
@@ -0,0 +1,8 @@
+import 'package:product_of/product_of.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
