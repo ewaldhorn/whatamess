ca7199187f200371bc3735dd4e7c4da2bb8a032d
Ewald Horn <ewaldhorn@gmail.com>
2024-06-12 11:46:41 +0200

    Basic, not optimised solution working

--- languages/dart/learning/algorithms/product_of/lib/product_of.dart
+++ languages/dart/learning/algorithms/product_of/lib/product_of.dart
@@ -1,3 +1,18 @@
+// Basic solution, using nested loops
+// Not optimal, but a good first step to just solve the problem
 List<int> productOfArray(List<int> input) {
-  return input;
+  List<int> result = List.filled(input.length, 0);
+
+  for (int outer = 0; outer < input.length; outer++) {
+    int tmp = 1;
+
+    for (int inner = 0; inner < input.length; inner++) {
+      if (inner != outer) {
+        tmp = tmp * input[inner];
+      }
+    }
+    result[outer] = tmp;
+  }
+
+  return result;
 }
