ff07e76b0d3bb573a345778fe1a2f1ce7ca7a235
Ewald Horn <ewaldhorn@gmail.com>
2024-06-12 09:53:54 +0200

    Complete anagram solution in Dart, following TDD principles

--- languages/dart/learning/algorithms/anagram/lib/anagram.dart
+++ languages/dart/learning/algorithms/anagram/lib/anagram.dart
@@ -1,3 +1,28 @@
 bool isAnagram(String left, String right) {
-  return false;
+  // short-circuit on empty inputs
+  if (left.isEmpty && right.isEmpty) {
+    return true;
+  }
+
+  // different length strings aren't worth testing
+  if (left.length != right.length) {
+    return false;
+  }
+
+  // split into characters
+  List<String> leftArray = left.split("");
+  List<String> rightArray = right.split("");
+
+  // sort them
+  leftArray.sort((l, r) => l.compareTo(r));
+  rightArray.sort((l, r) => l.compareTo(r));
+
+  // if the two arrays are not identical, it's not an anagram
+  for (int pos = 0; pos < leftArray.length; pos++) {
+    if (leftArray[pos] != rightArray[pos]) {
+      return false;
+    }
+  }
+
+  return true;
 }
