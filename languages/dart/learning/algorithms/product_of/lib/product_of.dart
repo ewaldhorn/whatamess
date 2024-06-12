ff94d4af6b435d30564d0e2eb5d25d6f571884fc
Ewald Horn <ewaldhorn@gmail.com>
2024-06-12 12:39:43 +0200

    Better solution

--- languages/dart/learning/algorithms/product_of/lib/product_of.dart
+++ languages/dart/learning/algorithms/product_of/lib/product_of.dart
@@ -19,6 +19,20 @@ List<int> productOfArray(List<int> input) {
 
 // Same output, minus the nested looping
 List<int> productOfArrayNoNesting(List<int> input) {
+  // set up the results list, populate it with 1's
   List<int> result = List.filled(input.length, 1);
+
+  // prepopulate the result with the multipliers for that position
+  for (int pos = 1; pos < input.length; pos++) {
+    result[pos] = input[pos - 1] * result[pos - 1];
+  }
+
+  // now loop through the numbers, calculating the product
+  int tmp = 1;
+
+  for (int pos = input.length; pos > 0; pos--) {
+    result[pos - 1] = result[pos - 1] * tmp;
+    tmp = tmp * input[pos - 1];
+  }
   return result;
 }
