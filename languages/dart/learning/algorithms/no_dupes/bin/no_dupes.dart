ddfb32b94b046cc649d0bdb30c2a5eb243bf5b43
Ewald Horn <ewaldhorn@gmail.com>
2024-06-14 11:59:12 +0200

    Skeleton for a solution in place

---
+++ languages/dart/learning/algorithms/no_dupes/bin/no_dupes.dart
@@ -0,0 +1,5 @@
+import 'package:no_dupes/no_dupes.dart' as no_dupes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${no_dupes.calculate()}!');
+}
---
+++ languages/dart/learning/algorithms/no_dupes/test/no_dupes_test.dart
@@ -0,0 +1,8 @@
+import 'package:no_dupes/no_dupes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
