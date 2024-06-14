67608068015d61e603c86a50607934c1f33af1f7
Ewald Horn <ewaldhorn@gmail.com>
2024-06-14 17:47:08 +0200

    Extract gradient to a custom widget

--- languages/dart/flutter/udemy/academind/learn_flutter_dart/section2/roller/lib/main.dart
+++ languages/dart/flutter/udemy/academind/learn_flutter_dart/section2/roller/lib/main.dart
@@ -8,19 +8,24 @@ MaterialApp buildApp() {
   return MaterialApp(
     home: Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topLeft, colors: [
+        decoration: const BoxDecoration(gradient: DarkGreenGradient()),
+        child: const Center(
+          child: Text('Yolo!',
+              style: TextStyle(color: Colors.white, fontSize: 28)),
+        ),
+      ),
+    ),
+  );
+}
+
+class DarkGreenGradient extends LinearGradient {
+  const DarkGreenGradient()
+      : super(begin: Alignment.topLeft, colors: const [
           Color.fromARGB(255, 33, 95, 33),
           Color.fromARGB(255, 29, 87, 29),
           Color.fromARGB(255, 23, 73, 23),
           Color.fromARGB(255, 23, 72, 24),
           Color.fromARGB(255, 24, 58, 25),
           Color.fromARGB(255, 18, 54, 20),
-        ])),
-        child: const Center(
-          child: Text('Yolo!', style: TextStyle(color: Colors.white)),
-        ),
-      ),
-    ),
-  );
+        ]);
 }
