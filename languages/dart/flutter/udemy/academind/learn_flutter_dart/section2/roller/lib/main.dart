a1bbb0c23883e28d51f096078e05e56458bf3732
Ewald Horn <ewaldhorn@gmail.com>
2024-06-14 17:11:51 +0200

    Playing with styling

--- languages/dart/flutter/udemy/academind/learn_flutter_dart/section2/roller/lib/main.dart
+++ languages/dart/flutter/udemy/academind/learn_flutter_dart/section2/roller/lib/main.dart
@@ -5,7 +5,22 @@ void main() {
 }
 
 MaterialApp buildApp() {
-  return const MaterialApp(
-    home: Scaffold(body: Text('Yolo!')),
+  return MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topLeft, colors: [
+          Color.fromARGB(255, 33, 95, 33),
+          Color.fromARGB(255, 29, 87, 29),
+          Color.fromARGB(255, 23, 73, 23),
+          Color.fromARGB(255, 23, 72, 24),
+          Color.fromARGB(255, 24, 58, 25),
+          Color.fromARGB(255, 18, 54, 20),
+        ])),
+        child: const Center(
+          child: Text('Yolo!', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    ),
   );
 }
