aadf7a07c23e9795519458f865df4ed56b53b508
Ewald Horn <ewaldhorn@gmail.com>
2024-06-18 15:54:27 +0200

    Display background

--- languages/dart/flutter/codelabs/forge2d_game/lib/main.dart
+++ languages/dart/flutter/codelabs/forge2d_game/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:forge2d_game/components/game.dart';
 
 void main() {
   runApp(
     GameWidget.controlled(
-      gameFactory: FlameGame.new,
+      gameFactory: MyPhysicsGame.new,
     ),
   );
 }
