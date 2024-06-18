40dc025b0bb5e8f47bae048eed6e932166aa188e
Ewald Horn <ewaldhorn@gmail.com>
2024-06-18 10:58:49 +0200

    Pattern matching is cleaner

--- languages/dart/flutter/codelabs/patterns_codelab/lib/main.dart
+++ languages/dart/flutter/codelabs/patterns_codelab/lib/main.dart
@@ -30,16 +30,17 @@ class DocumentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final metadataRecord = document.metadata;
+    // unpack variables via pattern matching
+    final (title, modified: modified) = document.metadata;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(metadataRecord.$1),
+        title: Text(title),
       ),
       body: Column(
         children: [
           Center(
-            child: Text('Last modified ${metadataRecord.modified}'),
+            child: Text('Last modified $modified'),
           ),
         ],
       ),
