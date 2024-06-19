49ca862f03f0c09ff876940506353d142a812775
Ewald Horn <ewaldhorn@gmail.com>
2024-06-19 13:01:00 +0200

    Cleaning up code

--- languages/dart/codingame/retro_typewriter_art/retro_typewriter_art.dart
+++ languages/dart/codingame/retro_typewriter_art/retro_typewriter_art.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:math';
 
 String readLineSync() {
   String? s = stdin.readLineSync();
@@ -15,40 +14,41 @@ void main() {
 }
 
 void performInstruction(String instruction) {
-  if (instruction == 'nl') {
-    // new line
-    print('');
-  } else if (isNumber(instruction)) {
+  if (isNumber(instruction)) {
     // all numbers, need to split off last character
     stdout.write(
         '${instruction[instruction.length - 1] * toNumber(instruction.substring(0, instruction.length - 1))}');
   } else {
-    final numeric = RegExp(r'\d+/g');
-    final chars = RegExp(r'[^0-9]/g');
-    var t = numeric.firstMatch(instruction);
-    var t2 = chars.firstMatch(instruction);
-    var count = 0;
-
-    if (t != null) {
-      count = int.parse(t.toString());
-    }
-
-    if (t2 != null) {
-      switch (t2.toString()) {
-        case "sp":
-          stdout.write('${" " * count}');
-          break;
-        case "bS":
-          stdout.write('${"\\" * count}');
-          break;
-        case "sQ":
-          stdout.write('${"'" * count}');
-          break;
-        default:
-          stdout.write('${t2.toString() * count}');
-      }
+    final numericPosition = findNumericPosition(instruction);
+    var count = toNumber(instruction.substring(0, numericPosition));
+    var chars = instruction.substring(count > 0 ? numericPosition : 0);
+    switch (chars) {
+      case "nl":
+        print('');
+        break;
+      case "sp":
+        stdout.write('${" " * count}');
+        break;
+      case "bS":
+        stdout.write('${"\\" * count}');
+        break;
+      case "sQ":
+        stdout.write('${"'" * count}');
+        break;
+      default:
+        stdout.write('${chars * count}');
     }
   }
+}
+
+int findNumericPosition(String string) {
+  int pos = 0;
+
+  while (pos < string.length && isNumber(string.substring(0, pos + 1))) {
+    pos += 1;
+  }
+
+  return pos;
 }
 
 int toNumber(String string) {
