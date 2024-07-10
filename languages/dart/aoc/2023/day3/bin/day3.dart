7097f35601fd24e6b12f2cbd07c5d3dcecd27c00
Ewald Horn <ewaldhorn@gmail.com>
2024-07-10 16:06:31 +0200

    Still getting some foundational code going

--- languages/dart/aoc/2023/day3/bin/day3.dart
+++ languages/dart/aoc/2023/day3/bin/day3.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+final validNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 // ----------------------------------------------------------------------- main
 void main(List<String> arguments) async {
   if (arguments.isEmpty) {
@@ -15,53 +17,23 @@ Future<int> parseFile(String filename) async {
   int total = 0;
 
   await File(filename).readAsLines().then((lines) {
-    for (var line in lines) {
-      total += parseLine(line);
+    int height = lines.length;
+    int width = lines[0].length;
+
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        stdout.write(lines[y][x]);
+      }
+      print('');
     }
   });
 
   return total;
 }
 
-// ------------------------------------------------------------------ parseLine
-int parseLine(String line) {
-  int colonIndex = line.indexOf(':');
-  List<String> resultsPart = line.substring(colonIndex + 2).split(';');
+// -------------------------------------------------------- findAdjacentNumbers
+int findAdjacentNumbers(List<String> lines, int x, int y) {
+  int total = 0;
 
-  return calculateLeastNumbersNeeded(resultsPart);
-}
-
-// ------------------------------------------------ calculateLeastNumbersNeeded
-int calculateLeastNumbersNeeded(List<String> results) {
-  int maxRed = 0;
-  int maxGreen = 0;
-  int maxBlue = 0;
-
-  for (final result in results) {
-    List<String> parts = result.trim().split(',');
-
-    for (final part in parts) {
-      List<String> detail = part.trim().split(' ');
-
-      switch (detail[1]) {
-        case 'red':
-          int tmpRed = int.parse(detail[0]);
-          if (tmpRed > maxRed) {
-            maxRed = tmpRed;
-          }
-        case 'green':
-          int tmpGreen = int.parse(detail[0]);
-          if (tmpGreen > maxGreen) {
-            maxGreen = tmpGreen;
-          }
-        case 'blue':
-          int tmpBlue = int.parse(detail[0]);
-          if (tmpBlue > maxBlue) {
-            maxBlue = tmpBlue;
-          }
-      }
-    }
-  }
-
-  return maxRed * maxGreen * maxBlue;
+  return total;
 }
