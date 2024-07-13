8975edf90e2810c3ad91f61410687e17852fa55e
Ewald Horn <ewaldhorn@gmail.com>
2024-07-13 22:44:35 +0200

    Refine tests

--- languages/dart/experiments/algorithms/sliding_window/test/sliding_window_test.dart
+++ languages/dart/experiments/algorithms/sliding_window/test/sliding_window_test.dart
@@ -7,8 +7,16 @@ void main() {
       expect(findMaxSumOfSubArrayLooped([1, 2, 6, 2, 4, 1], 3), 12);
     });
 
-    test('([1, 2, 6, 2, 4, 1], 3), 12)', () {
+    test('([1, 2, 6, 2, 4, 1], 1), 6)', () {
+      expect(findMaxSumOfSubArrayLooped([1, 2, 6, 2, 4, 1], 1), 6);
+    });
+
+    test('([2, 2, 2, 2, 2, 2], 3), 6)', () {
       expect(findMaxSumOfSubArrayLooped([2, 2, 2, 2, 2, 2], 3), 6);
+    });
+
+    test('([2, 2, 2, 2, 2, 2], 4), 8)', () {
+      expect(findMaxSumOfSubArrayLooped([2, 2, 2, 2, 2, 2], 4), 8);
     });
   });
 
@@ -17,8 +25,16 @@ void main() {
       expect(findMaxSumOfSubArray([1, 2, 6, 2, 4, 1], 3), 12);
     });
 
-    test('([1, 2, 6, 2, 4, 1], 3), 12)', () {
+    test('([1, 2, 6, 2, 4, 1], 1), 6)', () {
+      expect(findMaxSumOfSubArray([1, 2, 6, 2, 4, 1], 1), 6);
+    });
+
+    test('([2, 2, 2, 2, 2, 2], 3), 6)', () {
       expect(findMaxSumOfSubArray([2, 2, 2, 2, 2, 2], 3), 6);
+    });
+
+    test('([2, 2, 2, 2, 2, 2], 4), 8)', () {
+      expect(findMaxSumOfSubArray([2, 2, 2, 2, 2, 2], 4), 8);
     });
   });
 }
