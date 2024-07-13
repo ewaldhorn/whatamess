601be384daefe6a8d81fa103c1bad53e2975a5f2
Ewald Horn <ewaldhorn@gmail.com>
2024-07-13 22:12:01 +0200

    Adding the sliding window while loop

--- languages/dart/experiments/algorithms/sliding_window/lib/sliding_window.dart
+++ languages/dart/experiments/algorithms/sliding_window/lib/sliding_window.dart
@@ -1,4 +1,6 @@
-int findMaxSumOfSubArray(List<int> numbers, int windowSize) {
+// ----------------------------------------------------------------------------
+// use two loops to control the sliding window position
+int findMaxSumOfSubArrayLooped(List<int> numbers, int windowSize) {
   int maxSum = 0;
 
   // don't bother if we don't have enough numbers to start with
@@ -16,6 +18,40 @@ int findMaxSumOfSubArray(List<int> numbers, int windowSize) {
     if (tmp > maxSum) {
       maxSum = tmp;
     }
+  }
+
+  return maxSum;
+}
+
+// ----------------------------------------------------------------------------
+// only iterate over the numbers list once, using a sliding window
+// as the window moves, subtract the number leaving the window and then
+// add the number entering the window (start, end)
+int findMaxSumOfSubArray(List<int> numbers, int windowSize) {
+  // still don't bother if we don't have enough numbers to start with
+  if (numbers.length < windowSize) {
+    return 0;
+  }
+
+  int maxSum = 0, start = 0, end = 0, windowSum = 0;
+
+  // calculate the initial window sum (first [windowSize] numbers)
+  while (end < windowSize) {
+    windowSum += numbers[end];
+    end++;
+  }
+  maxSum = windowSum;
+
+  // now perform the sliding window operation to check the rest.
+  while (start + windowSize < numbers.length) {
+    windowSum = windowSum - numbers[start] + numbers[end];
+
+    if (windowSum > maxSum) {
+      maxSum = windowSum;
+    }
+
+    start++;
+    end++;
   }
 
   return maxSum;
--- languages/dart/experiments/algorithms/sliding_window/test/sliding_window_test.dart
+++ languages/dart/experiments/algorithms/sliding_window/test/sliding_window_test.dart
@@ -2,6 +2,16 @@ import 'package:sliding_window/sliding_window.dart';
 import 'package:test/test.dart';
 
 void main() {
+  group('findMaxSumOfSubArrayLooped', () {
+    test('([1, 2, 6, 2, 4, 1], 3), 12)', () {
+      expect(findMaxSumOfSubArrayLooped([1, 2, 6, 2, 4, 1], 3), 12);
+    });
+
+    test('([1, 2, 6, 2, 4, 1], 3), 12)', () {
+      expect(findMaxSumOfSubArrayLooped([2, 2, 2, 2, 2, 2], 3), 6);
+    });
+  });
+
   group('findMaxSumOfSubArray', () {
     test('([1, 2, 6, 2, 4, 1], 3), 12)', () {
       expect(findMaxSumOfSubArray([1, 2, 6, 2, 4, 1], 3), 12);
