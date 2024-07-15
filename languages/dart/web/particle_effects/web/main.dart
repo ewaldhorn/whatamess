bc0c940bfd8765a61d5d6fe5659a4727c334811d
Ewald Horn <ewaldhorn@gmail.com>
2024-07-15 14:41:28 +0200

    Test some rendering

--- languages/dart/web/particle_effects/web/main.dart
+++ languages/dart/web/particle_effects/web/main.dart
@@ -1,5 +1,8 @@
 import 'package:web/web.dart' as web;
 
+late web.HTMLCanvasElement mainCanvas;
+late web.CanvasRenderingContext2D context2d;
+
 // ----------------------------------------------------------------- setHeading
 void setHeading() {
   final headingElement =
@@ -15,8 +18,47 @@ void insertHRBefore(String element) {
   web.document.body?.insertBefore(hrElement, outputElement);
 }
 
+// ------------------------------------------------------------- setupVariables
+void setupVariables() {
+  mainCanvas =
+      web.document.getElementById('mainCanvas') as web.HTMLCanvasElement;
+  context2d = mainCanvas.context2D;
+}
+
+// ----------------------------------------------------------------- drawSquare
+void drawSquare(
+  web.CanvasRenderingContext2D ctx,
+  (int, int) startXY,
+  (int, int) sizeWH,
+) {
+  final (x, y) = startXY;
+  final (w, h) = sizeWH;
+
+  ctx.beginPath();
+  ctx
+    ..moveTo(x, y)
+    ..lineTo(x + w, y)
+    ..lineTo(x + w, y + h)
+    ..lineTo(x, y + h)
+    ..lineTo(x, y)
+    ..stroke();
+}
+
+// ---- fillCanvasWithPyramid
+void fillCanvasWithPyramid(
+    web.HTMLCanvasElement canvas, web.CanvasRenderingContext2D ctx) {
+  final midX = canvas.width ~/ 2;
+  final midY = canvas.height ~/ 2;
+
+  for (int i = 1; i < midX; i += 3) {
+    drawSquare(ctx, (midX - i, midY - i), (i * 2, i * 2));
+  }
+}
+
 // ----------------------------------------------------------------------- main
 void main() {
   setHeading();
   insertHRBefore('output');
+  setupVariables();
+  fillCanvasWithPyramid(mainCanvas, context2d);
 }
