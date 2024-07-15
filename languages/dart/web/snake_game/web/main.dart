944e60b5fb841e7e69b6c65c86519e260d6e964f
Ewald Horn <ewaldhorn@gmail.com>
2024-07-15 09:57:03 +0200

    Getting some basic infrastructure in place

--- languages/dart/web/snake_game/web/main.dart
+++ languages/dart/web/snake_game/web/main.dart
@@ -1,5 +1,11 @@
+import 'dart:js_interop';
+
 import 'package:web/web.dart' as web;
 
+// ----------------------------------------------------------- Global Constants
+const int cellSize = 10;
+
+// ----------------------------------------------------------- Global Variables
 late web.HTMLCanvasElement canvas;
 late web.CanvasRenderingContext2D ctx;
 
@@ -9,7 +15,23 @@ void initGame() {
   ctx = canvas.getContext('2d') as web.CanvasRenderingContext2D;
 }
 
+// ------------------------------------------------------------------- drawCell
+void drawCell(({int x, int y}) coords, String color) {
+  ctx
+    ..fillStyle = color.toJS
+    ..strokeStyle = 'white'.toJS;
+
+  final num x = coords.x * cellSize;
+  final num y = coords.y * cellSize;
+
+  ctx
+    ..fillRect(x, y, cellSize, cellSize)
+    ..strokeRect(x, y, cellSize, cellSize);
+}
+
 // ----------------------------------------------------------------------- main
 void main() {
   initGame();
+
+  drawCell((x: 10, y: 10), "salmon");
 }
