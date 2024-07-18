ed20f6223b3a85c7c3ea2c19f2caa975db680ac5
Ewald Horn <ewaldhorn@gmail.com>
2024-07-18 13:32:54 +0200

    Adding user functions

--- languages/dart/learning/server/dart_rest_api/bin/pages/users.dart
+++ languages/dart/learning/server/dart_rest_api/bin/pages/users.dart
@@ -29,3 +29,54 @@ Response getUserById(Request request) {
     headers: {'Content-Type': 'application/json'},
   );
 }
+
+// ------------------------------------------------------------------- addUser
+Future<Response> addUser(Request request) async {
+  final payload = await request.readAsString();
+  final data = jsonDecode(payload);
+
+  // mimic a database insert
+  data['id'] = (users.length + 1).toString();
+  users.add(data);
+
+  return Response.ok(
+    {
+      'message': '${data['id']} added successfully',
+      'status': 'success',
+    },
+    headers: {'Content-Type': 'application/json'},
+  );
+}
+
+// ----------------------------------------------------------------- updateUser
+Future<Response> updateUser(Request request) async {
+  final id = request.params['id'];
+  final existingUserIndex = users.indexWhere((u) => u['id'] == id);
+
+  if (existingUserIndex == -1) {
+    return Response.notFound('User not found');
+  }
+
+  final payload = await request.readAsString();
+
+  final updateData = jsonDecode(payload);
+
+  // Update the user data accordingly
+  users[existingUserIndex] = updateData;
+
+  return Response.ok(
+    jsonEncode(users[existingUserIndex]),
+    headers: {'Content-Type': 'application/json'},
+  );
+}
+
+// ----------------------------------------------------------------- deleteUser
+Response deleteUser(Request request) {
+  final id = request.params['id'];
+  final userIndex = users.indexWhere((u) => u['id'] == id);
+  if (userIndex == -1) {
+    return Response.notFound('User not found');
+  }
+  users.removeAt(userIndex);
+  return Response.ok('User deleted successfully');
+}
--- languages/dart/learning/server/dart_rest_api/bin/server_helpers.dart
+++ languages/dart/learning/server/dart_rest_api/bin/server_helpers.dart
@@ -5,10 +5,13 @@ import 'pages/pages.dart' as pages;
 
 // ----------------------------------------------------------- Configure routes
 final router = Router(notFoundHandler: pages.oopsHandler)
+  ..delete('/api/v1/users/<id>', pages.deleteUser)
   ..get('/', pages.homePageHandler)
   ..get('/users/list', pages.getUsers)
   ..get('/api/v1/users/<id>', pages.getUserById)
-  ..get('/echo/<message>', pages.echoHandler);
+  ..get('/echo/<message>', pages.echoHandler)
+  ..post('/api/v1/users', pages.addUser)
+  ..put('/api/v1/users/<id>', pages.updateUser);
 
 // ------------------------------------ Configure a pipeline that logs requests
 final handler = Pipeline().addMiddleware(logRequests()).addHandler(router.call);
