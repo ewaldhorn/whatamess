4a999adc91387d3ae4df6a8b94ea5217cf97e433
Ewald Horn <ewaldhorn@gmail.com>
2024-07-19 17:17:19 +0200

    Button handlers in place

--- languages/dart/web/slotter/web/main.dart
+++ languages/dart/web/slotter/web/main.dart
@@ -1,3 +1,5 @@
+import 'dart:js_interop';
+
 import 'package:web/web.dart' as web;
 
 // -------------------------------------------------------------------- Globals
@@ -11,7 +13,36 @@ void initHelp() {
   helpElement.innerText = fruits.join(' ');
 }
 
+// ----------------------------------------------------------------- setupSlots
+void setupSlots() {
+  final slotsElement =
+      web.document.querySelector('.slots') as web.HTMLDivElement;
+}
+
+// -------------------------------------------------------- setupButtonHandlers
+void setupButtonHandlers() {
+  final spinButton =
+      web.document.getElementById('spin') as web.HTMLButtonElement;
+  spinButton.addEventListener('click', handleSpinClicked.toJS);
+
+  final clearButton =
+      web.document.getElementById('clear') as web.HTMLButtonElement;
+  clearButton.addEventListener('click', handleClearClicked.toJS);
+}
+
+// ---------------------------------------------------------- handleSpinClicked
+void handleSpinClicked(web.Event event) {
+  web.console.log('Spin'.toJS);
+}
+
+// --------------------------------------------------------- handleClearClicked
+void handleClearClicked(web.Event event) {
+  web.console.log('Clear'.toJS);
+}
+
 // ----------------------------------------------------------------------- main
 void main() {
   initHelp();
+  setupSlots();
+  setupButtonHandlers();
 }
