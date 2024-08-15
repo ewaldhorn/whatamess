471c2a1d33a73aec992eb14dc45078a320a75af7
Ewald Horn <ewaldhorn@gmail.com>
2024-08-15 18:12:24 +0200

    Housekeeping

--- languages/dart/learning/enums/todo.dart
+++ languages/dart/learning/enums/todo.dart
@@ -1,3 +1,5 @@
+// FreeCodeCamp Article
+// https://www.freecodecamp.org/news/how-to-use-enhanced-enums-in-dart/
 // ====================================================================== ENUMS
 enum Priority { low, medium, high }
 
@@ -15,8 +17,42 @@ enum Day {
   final String abbreviation;
 }
 
+enum Month {
+  January("Jan"),
+  February("Feb"),
+  March("Mar"),
+  April("Apr"),
+  May("May"),
+  June("Jun"),
+  July("Jul"),
+  August("Aug"),
+  September("Sep"),
+  October("Oct"),
+  November("Nov"),
+  December("Dec");
+
+  final String abbreviation;
+
+  const Month(this.abbreviation);
+
+  Month operator +(int other) {
+    // Ensure the result stays within 0-11 range
+    int result = (this.index + other) % 12;
+    return Month.values[result];
+  }
+}
+
 // ======================================================================= MAIN
 void main() {
   Day today = Day.friday;
   print('So today is ${today.name} ("${today.abbreviation}").');
+  print('');
+
+  Month thisMonth = Month.July;
+  print('Current month is ${thisMonth.name}.');
+
+  Month nextMonth = thisMonth + 1;
+  print('Next month is ${nextMonth.name}.');
+
+  print('And then the next month is ${(nextMonth + 1).name}.');
 }
--- languages/go/courses/udemy/intro_to_testing/section3/primeapp/main.go
+++ languages/go/courses/udemy/intro_to_testing/section3/primeapp/main.go
@@ -26,18 +26,23 @@ func main() {
 	sayGoodbye()
 }
 
-// ------------------------------------------------------------------------------------------------
-func sayWelcome() { fmt.Println("\nHello! Pick a number.\nOr (Q)uit : "); prompt() }
+// ----------------------------------------------------------------- sayWelcome
+func sayWelcome() {
+	fmt.Println("\nHello! Pick a number.\nOr (Q)uit : ")
+	prompt()
+}
 
-// ------------------------------------------------------------------------------------------------
-func sayGoodbye() { fmt.Println("\nThat was fun, let's do it again soon!") }
+// ----------------------------------------------------------------- sayGoodbye
+func sayGoodbye() {
+	fmt.Println("\nThat was fun, let's do it again soon!")
+}
 
-// ------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------- prompt
 func prompt() {
 	fmt.Print("-> ")
 }
 
-// ------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------- readUserInput
 func readUserInput(in io.Reader, signal chan bool) {
 	scanner := bufio.NewScanner(in)
 
@@ -54,7 +59,7 @@ func readUserInput(in io.Reader, signal chan bool) {
 	}
 }
 
-// ------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------- checkNumbers
 func checkNumbers(scanner *bufio.Scanner) (string, bool) {
 	scanner.Scan()
 
@@ -73,7 +78,7 @@ func checkNumbers(scanner *bufio.Scanner) (string, bool) {
 	return msg, false
 }
 
-// ------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------- isPrime
 func isPrime(number int) (bool, string) {
 	if number < 2 {
 		return false, fmt.Sprintf("%d is not a prime number.", number)
@@ -82,7 +87,8 @@ func isPrime(number int) (bool, string) {
 	// use mod to figure out if it's a prime
 	for i := 2; i <= number/2; i++ {
 		if number%i == 0 {
-			return false, fmt.Sprintf("%d is not a prime number, divisible by %d.", number, i)
+			return false,
+				fmt.Sprintf("%d is not a prime number, divisible by %d.", number, i)
 		}
 	}
 
