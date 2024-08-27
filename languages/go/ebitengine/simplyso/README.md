6a58c02c8e38a41da8787c79d57719396fd4a3b7
Ewald Horn <ewaldhorn@gmail.com>
2024-08-27 16:43:08 +0200

    Add README and player asset

---
+++ languages/go/ebitengine/simplyso/README.md
@@ -0,0 +1,22 @@
+# SimplySo Experimental Ebitengine Game
+
+Just playing with Go and Ebitengine a bit.
+
+## Game assets
+
+By Kenny, of course!
+```text
+###############################################################################
+
+	Space Shooter (Redux, plus fonts and sounds) by Kenney Vleugels (www.kenney.nl)
+
+			------------------------------
+
+			        License (CC0)
+	       http://creativecommons.org/publicdomain/zero/1.0/
+
+	You may use these graphics in personal and commercial projects.
+	Credit (Kenney or www.kenney.nl) would be nice but is not mandatory.
+
+###############################################################################
+```
--- languages/go/ebitengine/simplyso/main.go
+++ languages/go/ebitengine/simplyso/main.go
@@ -1,21 +1,52 @@
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"embed"
+	"image"
+	_ "image/png"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+//go:embed assets/*
+var assets embed.FS
+
+var PlayerSprite = mustLoadImage("assets/player.png")
 
 type Game struct{}
 
+// -------------------------------------------------------------- mustLoadImage
+func mustLoadImage(name string) *ebiten.Image {
+	f, err := assets.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+}
+
+// --------------------------------------------------------------------- Update
 func (g *Game) Update() error {
 	return nil
 }
 
+// ----------------------------------------------------------------------- Draw
 func (g *Game) Draw(screen *ebiten.Image) {
 
 }
 
+// --------------------------------------------------------------------- Layout
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return outsideWidth, outsideHeight
 }
 
+// ======================================================================= main
 func main() {
 	g := &Game{}
 
