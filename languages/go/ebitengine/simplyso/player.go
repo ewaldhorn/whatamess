ca60c2598801ee95229e50a7b3891ea34163cdde
Ewald Horn <ewaldhorn@gmail.com>
2024-08-28 09:39:19 +0200

    Add rotation via keyboard

--- languages/go/ebitengine/simplyso/player.go
+++ languages/go/ebitengine/simplyso/player.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 type Player struct {
 	position Vector2D
+	rotation float64
 	sprite   *ebiten.Image
 }
 
@@ -24,10 +29,29 @@ func NewPlayer() *Player {
 	}
 }
 
-func (p *Player) Update() {}
+func (p *Player) Update() {
+	speed := math.Pi / float64(ebiten.TPS())
+
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.rotation -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.rotation += speed
+	}
+
+}
 
 func (p *Player) Draw(screen *ebiten.Image) {
+	bounds := p.sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
 	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-halfW, -halfH)
+	op.GeoM.Rotate(p.rotation)
+	op.GeoM.Translate(halfW, halfH)
+
 	op.GeoM.Translate(p.position.X, p.position.Y)
+
 	screen.DrawImage(p.sprite, op)
 }
