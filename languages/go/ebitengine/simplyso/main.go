17c41a60969ba0d921678af2dd4a508030192d1f
Ewald Horn <ewaldhorn@gmail.com>
2024-08-28 09:33:32 +0200

    Working on creating a player

--- languages/go/ebitengine/simplyso/main.go
+++ languages/go/ebitengine/simplyso/main.go
@@ -1,73 +1,39 @@
 package main
 
 import (
-	"embed"
-	"image"
-	_ "image/png"
-	"math"
-
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-//go:embed assets/*
-var assets embed.FS
-
-var PlayerSprite = mustLoadImage("assets/player.png")
-
-type Vector2D struct {
-	X float64
-	Y float64
-}
+const (
+	ScreenWidth  = 800
+	ScreenHeight = 600
+)
 
 type Game struct {
-	playerPosition Vector2D
-}
-
-// -------------------------------------------------------------- mustLoadImage
-func mustLoadImage(name string) *ebiten.Image {
-	f, err := assets.Open(name)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	img, _, err := image.Decode(f)
-	if err != nil {
-		panic(err)
-	}
-
-	return ebiten.NewImageFromImage(img)
-}
-
-// ------------------------------------------------------------------ toRadians
-func toRadians(degrees float64) float64 {
-	return degrees * math.Pi / 180.0
+	player *Player
 }
 
 // --------------------------------------------------------------------- Update
 func (g *Game) Update() error {
 
-	speed := float64(120 / ebiten.TPS())
-	g.playerPosition.X += speed
+	g.player.Update()
 
 	return nil
 }
 
 // ----------------------------------------------------------------------- Draw
 func (g *Game) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(g.playerPosition.X, g.playerPosition.Y)
-	screen.DrawImage(PlayerSprite, op)
+	g.player.Draw(screen)
 }
 
 // --------------------------------------------------------------------- Layout
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return outsideWidth, outsideHeight
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return ScreenWidth, ScreenHeight
 }
 
 // ======================================================================= main
 func main() {
-	g := &Game{playerPosition: Vector2D{X: 100, Y: 100}}
+	g := &Game{player: NewPlayer()}
 
 	err := ebiten.RunGame(g)
 	if err != nil {
--- languages/go/ebitengine/simplyso/player.go
+++ languages/go/ebitengine/simplyso/player.go
@@ -8,9 +8,19 @@ type Player struct {
 }
 
 func NewPlayer() *Player {
+	sprite := PlayerSprite
+	bounds := sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
+	pos := Vector2D{
+		X: ScreenWidth/2 - halfW,
+		Y: ScreenHeight/2 - halfH,
+	}
+
 	return &Player{
-		position: Vector2D{X: 100, Y: 100},
-		sprite:   PlayerSprite,
+		position: pos,
+		sprite:   sprite,
 	}
 }
 
