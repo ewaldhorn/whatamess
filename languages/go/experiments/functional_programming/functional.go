a10d1d8f0d95d1516fc1701f8f62212036bdeee2
Ewald Horn <ewaldhorn@gmail.com>
2024-09-09 14:38:14 +0200

    Add filter

--- languages/go/experiments/functional_programming/functional.go
+++ languages/go/experiments/functional_programming/functional.go
@@ -1,19 +1,23 @@
 package main
 
-import "fmt"
-
 /*
 Experiments with functional programming in Go.
+
+Bespoke Map, Filter, Reduce functions.
 */
+
+import "fmt"
+
 // ----------------------------------------------------------------------------
 type mapFunction[E any] func(E) E
+type keepFunction[E any] func(E) bool
 
 // ----------------------------------------------------------------------------
-func Map[S ~[]E, E any](s S, apply mapFunction[E]) (result S) {
-	result = make(S, len(s))
+func Map[S ~[]E, E any](source S, apply mapFunction[E]) (result S) {
+	result = make(S, len(source))
 
-	for idx := range s {
-		result[idx] = apply(s[idx])
+	for idx := range source {
+		result[idx] = apply(source[idx])
 	}
 
 	return
@@ -25,13 +29,33 @@ func doubleUp(in int) int {
 }
 
 // ----------------------------------------------------------------------------
+func Filter[SourceType ~[]E, E any](source SourceType, mustKeep keepFunction[E]) (result SourceType) {
+	result = SourceType{}
+
+	for _, element := range source {
+		if mustKeep(element) {
+			result = append(result, element)
+		}
+	}
+
+	return
+}
+
 // ----------------------------------------------------------------------------
+func IsDivisiblebyTwo(num int) bool {
+	return num%2 == 0
+}
+
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
 func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	fmt.Println("We start with:", numbers)
-	fmt.Println("Doubled:", Map(numbers, doubleUp))                         // calling a mapping function
-	fmt.Println("Tripled:", Map(numbers, func(s int) int { return s * 3 })) // anonymous mapping function
+	fmt.Println("We start with :", numbers)
+	fmt.Println("Doubled (Map) :", Map(numbers, doubleUp))                         // calling a mapping function
+	fmt.Println("Tripled (Map) :", Map(numbers, func(s int) int { return s * 3 })) // anonymous mapping function
+	fmt.Println("We still have :", numbers)
+	fmt.Println("Evens (Filter):", Filter(numbers, IsDivisiblebyTwo))
+	fmt.Println("Odds (Filter) :", Filter(numbers, func(in int) bool { return in%2 != 0 }))
+	fmt.Println("We still have :", numbers)
 }
