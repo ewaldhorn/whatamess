e19a4624ac36de46da1df245bde1faee3816f7f1
Ewald Horn <ewaldhorn@gmail.com>
2024-09-09 17:46:56 +0200

    Fixing formatting issue

--- languages/go/courses/udemy/intro_to_testing/section7/webapp/cmd/web/setup_test.go
+++ languages/go/courses/udemy/intro_to_testing/section7/webapp/cmd/web/setup_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 // Setup for all tests
-
 var app application
 
+// ----------------------------------------------------------------------------
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
