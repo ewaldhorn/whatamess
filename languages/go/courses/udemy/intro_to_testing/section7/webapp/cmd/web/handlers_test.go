35c9f44cbbc2001b844f80fdc69a178a13fdfc8b
Ewald Horn <ewaldhorn@gmail.com>
2024-09-10 13:33:22 +0200

    Super basic sessions tests working

--- languages/go/courses/udemy/intro_to_testing/section7/webapp/cmd/web/handlers_test.go
+++ languages/go/courses/udemy/intro_to_testing/section7/webapp/cmd/web/handlers_test.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
+
+// ----------------------------------------------------------------------------
+func getContext(req *http.Request) (requestContext context.Context) {
+	requestContext = context.WithValue(req.Context(), contextUserKey, "unknown")
+	return
+}
+
+// ----------------------------------------------------------------------------
+func addContextAndSessionToRequest(req *http.Request, app application) *http.Request {
+	req = req.WithContext(getContext(req))
+	ctx, _ := app.Session.Load(req.Context(), req.Header.Get("X-Session"))
+	return req.WithContext(ctx)
+}
 
 // ----------------------------------------------------------------------------
 func Test_application_handlers(t *testing.T) {
@@ -35,5 +51,33 @@ func Test_application_handlers(t *testing.T) {
 		if rsp.StatusCode != test.expectedStatusCode {
 			t.Errorf("%s failed with a status code of %d, expected %d", test.name, rsp.StatusCode, test.expectedStatusCode)
 		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+func Test_Home(t *testing.T) {
+	// create request to get home page
+	req, _ := http.NewRequest("GET", "/", nil)
+	req = addContextAndSessionToRequest(req, app)
+
+	responseRecorder := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(app.Home)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// check response values is as expected
+	if responseRecorder.Code != http.StatusOK {
+		t.Errorf("Test_Home returned wrong status code. Expected %d, got %d", http.StatusOK, responseRecorder.Code)
+	}
+
+	requestBody, err := io.ReadAll(responseRecorder.Body)
+	if err != nil {
+		t.Errorf("Reading request body errored. %s", err.Error())
+	}
+
+	const lookFor = "<small>From Session:"
+
+	if !strings.Contains(string(requestBody), lookFor) {
+		t.Errorf("Could not find `%s` in the body", lookFor)
 	}
 }
