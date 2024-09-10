cb67d88bf463a4c4fbc9860f49027213d218da54
Ewald Horn <ewaldhorn@gmail.com>
2024-09-10 16:36:25 +0200

    Cleanup

--- languages/go/ebitengine/mapper/src/main.go
+++ languages/go/ebitengine/mapper/src/main.go
@@ -8,23 +8,31 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-// See https://ebitengine.org/en/tour/hello_world.html for the original.
-// I took some slight liberties with the code for this talk.
+// ----------------------------------------------------------------------------
+const (
+	SCREEN_WIDTH  int = 1024
+	SCREEN_HEIGHT int = 768
+)
 
+// ----------------------------------------------------------------------------
 type Game struct{}
 
+// ----------------------------------------------------------------------------
 func (g *Game) Update() error {
 	return nil
 }
 
+// ----------------------------------------------------------------------------
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello Ebiten!")
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 100, 50
+// ----------------------------------------------------------------------------
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return SCREEN_WIDTH, SCREEN_HEIGHT
 }
 
+// ----------------------------------------------------------------------------
 func main() {
 	ebiten.SetWindowSize(1024, 768)
 	ebiten.SetWindowTitle("Mapper Experiment")
