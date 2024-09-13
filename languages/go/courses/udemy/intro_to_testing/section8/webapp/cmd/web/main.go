fbc2c16e4b59bbb5ce81f70c189a9fbd8f4bc534
Ewald Horn <ewaldhorn@gmail.com>
2024-09-13 14:22:32 +0200

    Very basic Database connection working

--- languages/go/courses/udemy/intro_to_testing/section8/webapp/cmd/web/main.go
+++ languages/go/courses/udemy/intro_to_testing/section8/webapp/cmd/web/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 
@@ -9,6 +11,8 @@ import (
 
 // ----------------------------------------------------------------------------
 type application struct {
+	DSN     string
+	DB      *sql.DB
 	Session *scs.SessionManager
 }
 
@@ -17,10 +21,23 @@ func main() {
 	const port = "9000"
 
 	app := application{}
+
+	// read command-line "dsn" flag, with a sane default if it is not found
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UCT+2 connect_timeout=5", "Postgres connection")
+	flag.Parse()
+
+	dbConnection, err := app.connectToDatabase()
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+
+	app.DB = dbConnection
+
 	app.Session = getSession()
 
 	log.Print("Starting server on:", port)
-	err := http.ListenAndServe(string(":"+port), app.routes())
+
+	err = http.ListenAndServe(string(":"+port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
