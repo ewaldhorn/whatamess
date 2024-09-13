5801f5c337f3e65e9fe31aae25dee1aca6f9020c
Ewald Horn <ewaldhorn@gmail.com>
2024-09-13 10:51:39 +0200

    Cleaning up code smells

--- languages/go/courses/udemy/intro_to_testing/section7/webapp/cmd/web/handlers_test.go
+++ languages/go/courses/udemy/intro_to_testing/section7/webapp/cmd/web/handlers_test.go
@@ -38,9 +38,6 @@ func Test_application_handlers(t *testing.T) {
 	testServer := httptest.NewTLSServer(routes)
 	defer testServer.Close()
 
-	pathToTemplates = "./../../templates/"
-	pathToPages = pathToTemplates + "pages"
-
 	for _, test := range tests {
 		rsp, err := testServer.Client().Get(testServer.URL + test.url)
 		if err != nil {
@@ -53,34 +50,6 @@ func Test_application_handlers(t *testing.T) {
 		}
 	}
 }
-
-// ----------------------------------------------------------------------------
-// func Test_Home(t *testing.T) {
-// 	// create request to get home page
-// 	req, _ := http.NewRequest("GET", "/", nil)
-// 	req = addContextAndSessionToRequest(req, app)
-
-// 	responseRecorder := httptest.NewRecorder()
-
-// 	handler := http.HandlerFunc(app.Home)
-// 	handler.ServeHTTP(responseRecorder, req)
-
-// 	// check response values is as expected
-// 	if responseRecorder.Code != http.StatusOK {
-// 		t.Errorf("Test_Home returned wrong status code. Expected %d, got %d", http.StatusOK, responseRecorder.Code)
-// 	}
-
-// 	requestBody, err := io.ReadAll(responseRecorder.Body)
-// 	if err != nil {
-// 		t.Errorf("Reading request body errored. %s", err.Error())
-// 	}
-
-// 	const lookFor = "<small>From Session:"
-
-// 	if !strings.Contains(string(requestBody), lookFor) {
-// 		t.Errorf("Could not find `%s` in the body", lookFor)
-// 	}
-// }
 
 // ----------------------------------------------------------------------------
 func Test_Home_Improved(t *testing.T) {
--- languages/go/courses/udemy/intro_to_testing/section7/webapp/cmd/web/setup_test.go
+++ languages/go/courses/udemy/intro_to_testing/section7/webapp/cmd/web/setup_test.go
@@ -10,6 +10,10 @@ var app application
 
 // ----------------------------------------------------------------------------
 func TestMain(m *testing.M) {
+	// set up correct default test paths
+	pathToTemplates = "./../../templates/"
+	pathToPages = pathToTemplates + "pages"
+
 	app.Session = getSession()
 	os.Exit(m.Run())
 }
