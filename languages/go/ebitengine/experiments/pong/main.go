9a1b6b7f5b287c8abb367af311966b4ca6bafe5c
Ewald Horn <ewaldhorn@gmail.com>
2024-09-20 17:01:37 +0200

    Basic animation working

--- languages/go/ebitengine/experiments/pong/main.go
+++ languages/go/ebitengine/experiments/pong/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 // ----------------------------------------------------------------------------
@@ -24,6 +26,14 @@ type Paddle struct {
 	Object
 }
 
+func (p *Paddle) MoveOnKeyPress() {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		p.Y += paddleSpeed
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		p.Y -= paddleSpeed
+	}
+}
+
 // ----------------------------------------------------------------------------
 type Ball struct {
 	Object
@@ -31,27 +41,65 @@ type Ball struct {
 	dydt int
 }
 
+func (b *Ball) Move() {
+	b.X += b.dxdt
+	b.Y += b.dydt
+}
+
 // ----------------------------------------------------------------------------
 type Game struct {
-	padded    Paddle
+	paddle    Paddle
 	ball      Ball
 	score     int
 	highScore int
 }
 
-// ----------------------------------------------------------------------------
 func (g Game) Layout(outsideW, outsideH int) (int, int) {
 	return SCREEN_WIDTH, SCREEN_HEIGHT
 }
 
-// ----------------------------------------------------------------------------
 func (g Game) Draw(screen *ebiten.Image) {
+	vector.DrawFilledRect(screen, float32(g.paddle.X), float32(g.paddle.Y), float32(g.paddle.W), float32(g.paddle.H), color.White, true)
+
+	vector.DrawFilledRect(screen, float32(g.ball.X), float32(g.ball.Y), float32(g.ball.W), float32(g.ball.H), color.White, true)
+
+	// TODO display score and highscore
+	// scoreStr := fmt.Sprintf("Score: %d", g.score)
+	// highscoreStr := fmt.Sprintf("High Score: %d", g.highScore)
+	// ops := &text.DrawOptions{}
 
 }
 
-// ----------------------------------------------------------------------------
 func (g *Game) Update() error {
+	g.paddle.MoveOnKeyPress()
+	g.ball.Move()
+
+	g.CollideWithWall()
+	g.CollideWithPaddle()
+
 	return nil
+}
+
+func (g *Game) Reset() {
+	g.ball.X = 0
+	g.ball.Y = 0
+	g.score = 0
+}
+
+func (g *Game) CollideWithWall() {
+	if g.ball.X >= SCREEN_WIDTH {
+		g.Reset()
+	} else if g.ball.X <= 0 {
+		g.ball.dxdt = ballSpeed
+	} else if g.ball.Y <= 0 || g.ball.Y >= SCREEN_HEIGHT {
+		g.ball.dydt *= -1
+	}
+}
+
+func (g *Game) CollideWithPaddle() {
+	if g.ball.X >= g.paddle.X && g.ball.Y >= g.paddle.Y && g.ball.Y <= g.paddle.Y+g.paddle.H {
+		g.ball.dxdt *= -1
+	}
 }
 
 // ----------------------------------------------------------------------------
@@ -60,6 +108,8 @@ func main() {
 	ebiten.SetWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 	g := &Game{}
+	g.paddle = Paddle{Object: Object{X: 600, Y: 200, W: 15, H: 100}}
+	g.ball = Ball{Object: Object{X: 0, Y: 0, W: 15, H: 15}, dxdt: ballSpeed, dydt: ballSpeed}
 
 	err := ebiten.RunGame(g)
 	if err != nil {
