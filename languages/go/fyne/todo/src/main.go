4b5fe4939b18c912ca4bcf06b2bbb1a6187de5c3
Ewald Horn <ewaldhorn@gmail.com>
2024-09-22 22:21:39 +0200

    Working on displaying list

--- languages/go/fyne/todo/src/main.go
+++ languages/go/fyne/todo/src/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"todoapp/src/models"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -12,26 +14,60 @@ import (
 // ------------------------------------------------------------- Screen Globals
 // Yes yes, it should be in a struct, but this is a single page app...
 var inputWidget *widget.Entry
+var addButton *widget.Button
 
 // ----------------------------------------------------------------------------
 func createApplicationWindow(app fyne.App, title string) fyne.Window {
-	w := app.NewWindow(title)
+	newWindow := app.NewWindow(title)
 
-	w.Resize(fyne.NewSize(640, 480))
-	w.CenterOnScreen()
+	newWindow.Resize(fyne.NewSize(640, 480))
+	newWindow.CenterOnScreen()
 
-	return w
+	return newWindow
 }
 
 // ----------------------------------------------------------------------------
 func createInputSection() fyne.CanvasObject {
+	addButton = widget.NewButton("Add", func() { fmt.Println("Addind") })
+	addButton.Disable()
+
 	inputWidget = widget.NewEntry()
 	inputWidget.PlaceHolder = "Add new todo..."
+	inputWidget.OnChanged = func(txt string) {
+		addButton.Disable()
+		if len(strings.TrimSpace(txt)) > 2 {
+			addButton.Enable()
+		}
+	}
 
 	return container.NewGridWithColumns(2,
 		inputWidget,
-		widget.NewButton("Add", func() { fmt.Println("Add was clicked!") }),
+		addButton,
 	)
+}
+
+// ----------------------------------------------------------------------------
+func createTodoListSection() fyne.CanvasObject {
+	data := []models.Todo{
+		models.NewTodo("Some stuff"),
+		models.NewTodo("Some more stuff"),
+		models.NewTodo("Some other things"),
+	}
+
+	return widget.NewList(
+		// func that returns the number of items in the list
+		func() int {
+			return len(data)
+		},
+		// func that returns the component structure of the List Item
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		// func that is called for each item in the list and allows
+		// you to show the content on the previously defined ui structure
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i].Description)
+		})
 }
 
 // ----------------------------------------------------------------------------
@@ -45,9 +81,7 @@ func createMainScreen() fyne.CanvasObject {
 		nil, // Left
 
 		// the rest will take all the rest of the space
-		container.NewCenter(
-			widget.NewLabel("Content will end up here"),
-		),
+		createTodoListSection(),
 	)
 }
 
