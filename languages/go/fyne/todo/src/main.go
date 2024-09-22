2bf20063a5eb3d8a5f664696c4475f517e8afda9
Ewald Horn <ewaldhorn@gmail.com>
2024-09-22 17:51:07 +0200

    Refactoring for some cleaner code

--- languages/go/fyne/todo/src/main.go
+++ languages/go/fyne/todo/src/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
-	"todoapp/src/models"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+// ------------------------------------------------------------- Screen Globals
+// Yes yes, it should be in a struct, but this is a single page app...
+var inputWidget *widget.Entry
 
 // ----------------------------------------------------------------------------
 func createApplicationWindow(app fyne.App, title string) fyne.Window {
@@ -21,30 +24,43 @@ func createApplicationWindow(app fyne.App, title string) fyne.Window {
 }
 
 // ----------------------------------------------------------------------------
+func createInputSection() fyne.CanvasObject {
+	inputWidget = widget.NewEntry()
+	inputWidget.PlaceHolder = "Add new todo..."
+
+	return container.NewGridWithColumns(2,
+		inputWidget,
+		widget.NewButton("Add", func() { fmt.Println("Add was clicked!") }),
+	)
+}
+
+// ----------------------------------------------------------------------------
+func createMainScreen() fyne.CanvasObject {
+	return container.NewBorder(
+		nil, // TOP of the container
+
+		// this will be a the BOTTOM of the container
+		createInputSection(),
+		nil, // Right
+		nil, // Left
+
+		// the rest will take all the rest of the space
+		container.NewCenter(
+			widget.NewLabel("Content will end up here"),
+		),
+	)
+}
+
+// ----------------------------------------------------------------------------
 func main() {
 	a := app.New()
 	w := createApplicationWindow(a, "Todo App")
 
-	t := models.NewTodo("Show this on the window")
+	// t := models.NewTodo("Show this on the window")
 
 	w.SetContent(
-		container.NewBorder(
-			nil, // TOP of the container
-
-			// this will be a the BOTTOM of the container
-			container.NewGridWithColumns(2,
-				widget.NewEntry(),
-				widget.NewButton("Add", func() { fmt.Println("Add was clicked!") }),
-			),
-
-			nil, // Right
-			nil, // Left
-
-			// the rest will take all the rest of the space
-			container.NewCenter(
-				widget.NewLabel(t.String()),
-			),
-		),
+		createMainScreen(),
 	)
+
 	w.ShowAndRun()
 }
