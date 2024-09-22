579125b65a0ea76a517481d05d11e913ce0bfc84
Ewald Horn <ewaldhorn@gmail.com>
2024-09-22 17:40:43 +0200

    Basics of layout coming together

--- languages/go/fyne/todo/src/main.go
+++ languages/go/fyne/todo/src/main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"todoapp/src/models"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("TODO App")
+// ----------------------------------------------------------------------------
+func createApplicationWindow(app fyne.App, title string) fyne.Window {
+	w := app.NewWindow(title)
+
 	w.Resize(fyne.NewSize(640, 480))
 	w.CenterOnScreen()
 
+	return w
+}
+
+// ----------------------------------------------------------------------------
+func main() {
+	a := app.New()
+	w := createApplicationWindow(a, "Todo App")
+
 	t := models.NewTodo("Show this on the window")
 
-    w.SetContent(widget.NewLabel(t.String()))
+	w.SetContent(
+		container.NewBorder(
+			nil, // TOP of the container
 
+			// this will be a the BOTTOM of the container
+			container.NewGridWithColumns(2,
+				widget.NewEntry(),
+				widget.NewButton("Add", func() { fmt.Println("Add was clicked!") }),
+			),
+
+			nil, // Right
+			nil, // Left
+
+			// the rest will take all the rest of the space
+			container.NewCenter(
+				widget.NewLabel(t.String()),
+			),
+		),
+	)
 	w.ShowAndRun()
 }
