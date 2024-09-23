2e464dd28207431d2430ee143e45ccfb9e801240
Ewald Horn <ewaldhorn@gmail.com>
2024-09-23 10:37:19 +0200

    Tests can spin up a Docker instance with Postgres running

--- languages/go/courses/udemy/intro_to_testing/section9/webapp/pkg/repository/databaserepo/users_postgres_test.go
+++ languages/go/courses/udemy/intro_to_testing/section9/webapp/pkg/repository/databaserepo/users_postgres_test.go
@@ -2,6 +2,8 @@ package databaserepo
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"os"
 	"testing"
 
@@ -9,6 +11,7 @@ import (
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 )
 
 var (
@@ -17,7 +20,7 @@ var (
 	password = "postgres"
 	dbName   = "users_test"
 	port     = "5435"
-	dns      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
 )
 
 var resource *dockertest.Resource
@@ -25,6 +28,47 @@ var pool *dockertest.Pool
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	// connect to docker, fail if docker is not running
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("unable to connect to Docker instance: %s", err)
+	}
+
+	// set up docker options
+	options := dockertest.RunOptions{
+		Repository:   "postgres",
+		Tag:          "14.5",
+		Env:          []string{"POSTGRES_USER=" + user, "POSTGRES_PASSWORD=" + password, "POSTGRES_DB=" + dbName},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{"5432": {{HostIP: "0.0.0.0", HostPort: port}}}}
+
+	// get docker image
+	resource, err := pool.RunWithOptions(&options)
+	if err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not start resource: %s", err)
+	}
+
+	// start and wait for image
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			log.Println("Error:", err)
+			return err
+		}
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not connect to the database: %s", err)
+	}
+	// set up database with tables etc.
+
+	// run tests
 	code := m.Run()
+
+	// clean up
+
+	// all done
 	os.Exit(code)
 }
