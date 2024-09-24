a2c04aadfcea0e845b81a99dfb8149d441b712eb
Ewald Horn <ewaldhorn@gmail.com>
2024-09-24 14:03:30 +0200

    WIP:  Working on menu

--- languages/go/experiments/sqlite/hellosqlite/go.mod
+++ languages/go/experiments/sqlite/hellosqlite/go.mod
@@ -2,4 +2,14 @@ module yoyosql
 
 go 1.23.0
 
-require github.com/mattn/go-sqlite3 v1.14.23 // indirect
+require (
+	github.com/dixonwille/wmenu/v5 v5.1.0
+	github.com/mattn/go-sqlite3 v1.14.23
+)
+
+require (
+	github.com/daviddengcn/go-colortext v0.0.0-20180409174941-186a3d44e920 // indirect
+	github.com/dixonwille/wlog/v3 v3.0.1 // indirect
+	github.com/mattn/go-isatty v0.0.11 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
--- languages/go/experiments/sqlite/hellosqlite/main.go
+++ languages/go/experiments/sqlite/hellosqlite/main.go
@@ -5,16 +5,23 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/dixonwille/wmenu/v5"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	db, err := sql.Open("sqlite3", "./hellolite.db")
+// ----------------------------------------------------------------------------
+// Returns a handle to the database, fails if the database could not be opened
+func openDatabase(path string) *sql.DB {
+	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
+	return db
+}
+
+// ----------------------------------------------------------------------------
+func listAllAgents(db *sql.DB) {
 	rows, err := db.Query("select id, name, score from agents")
 	if err != nil {
 		log.Fatal(err)
@@ -31,6 +38,27 @@ func main() {
 		fmt.Println(id, name, score)
 	}
 	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// ----------------------------------------------------------------------------
+func main() {
+	db := openDatabase("./hellolite.db")
+	defer db.Close() // remember to close database when we are done
+
+	listAllAgents(db)
+
+	menu := wmenu.NewMenu("What is your favorite food?")
+	menu.Action(func(opts []wmenu.Opt) error { fmt.Printf(opts[0].Text + " is your favorite food."); return nil })
+	menu.Option("Pizza", nil, true, nil)
+	menu.Option("Ice Cream", nil, false, nil)
+	menu.Option("Tacos", nil, false, func(opt wmenu.Opt) error {
+		fmt.Printf("Tacos are great")
+		return nil
+	})
+	err := menu.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
