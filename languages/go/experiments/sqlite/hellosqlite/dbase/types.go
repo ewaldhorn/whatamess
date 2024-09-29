0af84c0727f2dcf16a0c16cd53863608fbe1257f
Ewald Horn <ewaldhorn@gmail.com>
2024-09-29 17:43:45 +0200

    Adding types

---
+++ languages/go/experiments/sqlite/hellosqlite/dbase/types.go
@@ -0,0 +1,11 @@
+package dbase
+
+// ----------------------------------------------------------------------------
+type agent struct {
+	id     int
+	name   string
+	time   string
+	email  string
+	score  int
+	region int
+}
