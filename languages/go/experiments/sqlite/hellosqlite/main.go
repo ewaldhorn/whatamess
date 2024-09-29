df0ad3e974eaa3c345825c3c66567c12e3ed5d85
Ewald Horn <ewaldhorn@gmail.com>
2024-09-29 17:22:42 +0200

    Improve menu ux

--- languages/go/experiments/sqlite/hellosqlite/main.go
+++ languages/go/experiments/sqlite/hellosqlite/main.go
@@ -18,8 +18,10 @@ func main() {
 	menu := wmenu.NewMenu("Select an action:")
 	menu.Action(func(options []wmenu.Opt) error { handleMenuSelection(options, db); return nil })
 
-	menu.Option("List all Agents", 0, true, nil)
-	menu.Option("Add an Agent", 1, false, nil)
+	menu.Option("List all Agents", 1, true, nil)
+	menu.Option("Add an Agent", 2, false, nil)
+	menu.Option("Update an Agent", 3, false, nil)
+	menu.Option("Remove an Agent", 4, false, nil)
 
 	err := menu.Run()
 	if err != nil {
@@ -30,9 +32,13 @@ func main() {
 // ----------------------------------------------------------------------------
 func handleMenuSelection(options []wmenu.Opt, db *sql.DB) {
 	switch options[0].Value {
-	case 0:
-		dbase.ListAllAgents(db)
 	case 1:
+		dbase.ListAllAgents(db)
+	case 2:
 		fmt.Println("Add an agent")
+	case 3:
+		fmt.Println("Update an agent")
+	default:
+		fmt.Printf("Option %v is not currently supported.\n", options[0].Value)
 	}
 }
