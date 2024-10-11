4ac7e19190aacc17252599dd8c72916b993f0f61
Ewald Horn <ewaldhorn@gmail.com>
2024-10-11 14:19:59 +0200

    Last homework

--- languages/typescript/learning/beginners_typescript/16-omit-and-pick.problem.ts
+++ languages/typescript/learning/beginners_typescript/16-omit-and-pick.problem.ts
@@ -7,5 +7,9 @@ interface User {
 }
 
 type MyType = Pick<User, "firstName" | "lastName">;
+type MyTypeII = Omit<User, "id">;
 
 type tests = [Expect<Equal<MyType, { firstName: string; lastName: string }>>];
+type tests2 = [
+  Expect<Equal<MyTypeII, { firstName: string; lastName: string }>>,
+];
---
+++ languages/typescript/learning/beginners_typescript/18-function-types-with-promises.problem.ts
@@ -0,0 +1,40 @@
+import { expect, it } from "vitest";
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+type CreateUserFn = () => Promise<string>;
+type GetUserFn = (id: string) => Promise<User>;
+
+const createThenGetUser = async (
+  createUser: CreateUserFn,
+  getUser: GetUserFn,
+  // createUser: () => Promise<string>,
+  // getUser: (id: string) => Promise<User>,
+): Promise<User> => {
+  const userId: string = await createUser();
+
+  const user = await getUser(userId);
+
+  return user;
+};
+
+it("Should create the user, then get them", async () => {
+  const user = await createThenGetUser(
+    async () => "123",
+    async (id) => ({
+      id,
+      firstName: "Matt",
+      lastName: "Pocock",
+    }),
+  );
+
+  expect(user).toEqual({
+    id: "123",
+    firstName: "Matt",
+    lastName: "Pocock",
+  });
+});
