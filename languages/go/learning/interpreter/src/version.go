6616d2762c6e2cf53f18f865fbf44f18d67c19fb
Ewald Horn <ewaldhorn@gmail.com>
2024-11-27 15:40:40 +0200

    Getting basic infrastructure in place

---
+++ languages/go/learning/interpreter/src/version.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const NAME = "MonkeyScript Interpreter"
+const VERSION = "0.0.1"
+
+// ----------------------------------------------------------------------------
+func reportVersion() {
+	fmt.Printf("%s v%s\n", NAME, VERSION)
+}
