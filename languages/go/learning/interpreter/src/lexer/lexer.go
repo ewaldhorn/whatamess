81234bf0a30b11a99e78dc88e50703378746cd52
Ewald Horn <ewaldhorn@gmail.com>
2024-11-27 16:49:05 +0200

    Working on lexer

--- languages/go/learning/interpreter/src/lexer/lexer.go
+++ languages/go/learning/interpreter/src/lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "interpreter/src/token"
+
 // ----------------------------------------------------------------------------
 type Lexer struct {
 	input        string
@@ -29,4 +31,44 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+// ----------------------------------------------------------------------------
+// NextToken processes and returns the next token in the input.
+// The token.Token struct contains both the type of token and the
+// literal value associated with it.
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		tok = newToken(token.ILLEGAL, l.ch)
+	}
+
+	l.readChar()
+	return tok
+}
+
+// ----------------------------------------------------------------------------
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
