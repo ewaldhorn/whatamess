cf11121a23fd4743727e71a7b5051de9cec6f236
Ewald Horn <ewaldhorn@gmail.com>
2024-12-14 16:21:50 +0200

    Add more middelware tests

--- languages/go/courses/udemy/intro_to_testing/section12/webapp/cmd/api/api-middleware_test.go
+++ languages/go/courses/udemy/intro_to_testing/section12/webapp/cmd/api/api-middleware_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"webapp/pkg/data"
 )
 
 // ----------------------------------------------------------------------------
@@ -33,4 +35,54 @@ func Test_app_enableCORS(t *testing.T) {
 			t.Errorf("%s did not expect a header!", test.name)
 		}
 	}
+}
+
+// ----------------------------------------------------------------------------
+func Test_app_authRequired(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	testUser := data.User{
+		ID: 1, FirstName: "Bob", LastName: "Soap", Email: "bob@soap.com",
+	}
+
+	tokens, err := app.generateTokenPair(&testUser)
+	if err != nil {
+		t.Errorf("failed to generate tokens: %s", err.Error())
+	}
+
+	tests := []struct {
+		name             string
+		token            string
+		expectAuthorized bool
+		setHeader        bool
+	}{
+		{"valid token", fmt.Sprintf("Bearer %s", tokens.Token), true, true},
+		{"no token", "", false, false},
+		{"invalid token", fmt.Sprintf("Bearer 1%s1", tokens.Token), false, true},
+		{"expired token", fmt.Sprintf("Bearer %s", createExpiredToken()), false, true},
+	}
+
+	for _, test := range tests {
+		req, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Errorf("%s did not expect to fail now: %s", test.name, err.Error())
+		}
+
+		if test.setHeader {
+			req.Header.Set("Authorization", test.token)
+		}
+
+		rr := httptest.NewRecorder()
+		handlerToTest := app.authRequired(nextHandler)
+		handlerToTest.ServeHTTP(rr, req)
+
+		if test.expectAuthorized && rr.Code == http.StatusUnauthorized {
+			t.Errorf("%s is unauthorized, this is unexpected", test.name)
+		}
+
+		if !test.expectAuthorized && rr.Code != http.StatusUnauthorized {
+			t.Errorf("%s should be unauthorized, it's not!", test.name)
+		}
+	}
+
 }
