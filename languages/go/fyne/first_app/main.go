f29ee34f57be49410eb31f6813d7f2a61da97e5c
Ewald Horn <ewaldhorn@gmail.com>
2024-12-17 11:34:33 +0200

    Start working on a text editor demo app

--- languages/go/fyne/first_app/main.go
+++ languages/go/fyne/first_app/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Crazy Dazy Text Editor")
 
 	w.SetContent(widget.NewLabel("Hello, Fyne!"))
 
+	w.Resize(fyne.NewSize(640, 480))
+	w.CenterOnScreen()
 	w.ShowAndRun()
 }
