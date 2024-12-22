3850de7c299c1a2c9ff462724e89b913857aaff0
Ewald Horn <ewaldhorn@gmail.com>
2024-12-22 15:51:07 +0200

    Add DOM manipulation methods in a separate module

--- languages/go/tinygo/wasm/calculator/src/main.go
+++ languages/go/tinygo/wasm/calculator/src/main.go
@@ -1,9 +1,33 @@
 package main
 
-import "syscall/js"
+import (
+	"smallgo/src/dom"
+	"strconv"
+)
 
+// ----------------------------------------------------------------------------
 func main() {
-	document := js.Global().Get("document")
-	document.Call("getElementById", "loading").Get("style").Call("setProperty", "display", "none")
-	document.Call("getElementById", "calc").Get("style").Call("setProperty", "display", "block")
+	// handle loading status update
+	dom.Hide("loading")
+	dom.Show("calc")
+
+	// now get the calculator ready for action
+	dom.SetValue("first-number", "value", "0")
+	dom.SetValue("second-number", "value", "0")
+	dom.SetValue("result", "value", "0")
+	performCalculation()
+}
+
+// ----------------------------------------------------------------------------
+func performCalculation() {
+	firstNumber, firstNumberErr := strconv.Atoi(dom.GetString("first-number", "value"))
+	secondNumber, secondNumberErr := strconv.Atoi(dom.GetString("second-number", "value"))
+
+	if firstNumberErr == nil && secondNumberErr == nil {
+		dom.SetValue("result", "value", strconv.Itoa(firstNumber+secondNumber))
+		dom.RemoveClass("result", "error")
+	} else {
+		dom.SetValue("result", "value", "ERR")
+		dom.AddClass("result", "error")
+	}
 }
