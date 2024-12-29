ba5ec424e8beca96bc657dcadf9a9333fae82620
Ewald Horn <ewaldhorn@gmail.com>
2024-12-29 16:17:28 +0200

    Set window title and size

--- languages/go/gio/egg_timer_tutorial/src/main.go
+++ languages/go/gio/egg_timer_tutorial/src/main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"gioui.org/app"
+	"gioui.org/io/system"
+	"gioui.org/unit"
+)
+
+// ----------------------------------------------------------------------------
+const (
+	MAIN_WIDTH  = 640
+	MAIN_HEIGHT = 480
 )
 
 // ----------------------------------------------------------------------------
 func main() {
-    go func() {
-        // create new window
-        w := new(app.Window)
+	go func() {
+		// create new window
+		mainWindow := new(app.Window)
 
-        // listen for events in the window
-        for {
-            w.Event()
-        }
-    }()
-    app.Main()
+		// set window properties
+		mainWindow.Option(app.Title("Egg Timer in Gio"))
+		mainWindow.Option(app.Size(unit.Dp(MAIN_WIDTH), unit.Dp(MAIN_HEIGHT)))
+
+		// center window on the desktop
+		mainWindow.Perform(system.ActionCenter) // doesn't work?
+
+		// listen for events in the window
+		for {
+			mainWindow.Event()
+		}
+	}()
+	app.Main()
 }
