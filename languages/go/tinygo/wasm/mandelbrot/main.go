03f4416838e1b010ab84229d57da827e8a9d84be
Ewald Horn <ewaldhorn@gmail.com>
2025-01-10 10:42:56 +0200

    Adding some comments

--- languages/go/tinygo/wasm/mandelbrot/main.go
+++ languages/go/tinygo/wasm/mandelbrot/main.go
@@ -2,17 +2,30 @@ package main
 
 import "github.com/ewaldhorn/tinycanvas/dom"
 
+// ----------------------------------------------------------------------------
+// The following functions are externally provided by the WASM runtime environment.
+
 //export setVersion
 func setVersion()
 
 // ----------------------------------------------------------------------------
+// Standard entry point for Go programs
 func main() {
+	// general bootstrapping
 	setupCallbacks()
 	setVersion()
 	dom.Hide("loading")
+
+	// now we should be ready to rock and roll
+
+	// prevent the app for closing - it stays running for the life of the page
+	// if we don't do this, it exits and all future calls into it will fail
+	ch := make(chan struct{})
+	<-ch
 }
 
 // ----------------------------------------------------------------------------
+// Set up the various callbacks used by the JavaScript side of things.
 func setupCallbacks() {
 	setVersionCallback()
 }
