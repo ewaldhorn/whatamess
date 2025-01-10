da265fa4e78df6250ba5c7486726ba1abd7b7e60
Ewald Horn <ewaldhorn@gmail.com>
2025-01-10 10:12:23 +0200

    Setting up foundations

---
+++ languages/go/tinygo/wasm/mandelbrot/go.mod
@@ -0,0 +1,5 @@
+module mandelbrotter
+
+go 1.23.4
+
+require github.com/ewaldhorn/tinycanvas v0.0.0-20250109154018-fdf68615ecf6
---
+++ languages/go/tinygo/wasm/mandelbrot/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/ewaldhorn/tinycanvas/dom"
+
+// ----------------------------------------------------------------------------
+func main() {
+	dom.Hide("loading")
+}
