cfb9993e0bd6b3dff8bdaa04a7cedecb28d552a7
Ewald Horn <ewaldhorn@gmail.com>
2025-01-10 10:26:49 +0200

    Looking into getting the basics working

--- languages/go/tinygo/wasm/mandelbrot/main.go
+++ languages/go/tinygo/wasm/mandelbrot/main.go
@@ -2,7 +2,17 @@ package main
 
 import "github.com/ewaldhorn/tinycanvas/dom"
 
+//export setVersion
+func setVersion()
+
 // ----------------------------------------------------------------------------
 func main() {
+	setupCallbacks()
+	setVersion()
 	dom.Hide("loading")
+}
+
+// ----------------------------------------------------------------------------
+func setupCallbacks() {
+	setVersionCallback()
 }
