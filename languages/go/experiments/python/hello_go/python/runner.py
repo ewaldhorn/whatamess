e15ae3c8b4a2688a3e5de2fe920c0705e92fab07
Ewald Horn <ewaldhorn@gmail.com>
2025-01-25 10:20:26 +0200

    Go and Python

--- languages/go/experiments/python/hello_go/python/runner.py
+++ languages/go/experiments/python/hello_go/python/runner.py
@@ -1,6 +1,10 @@
 import ctypes
 
 lib = ctypes.CDLL("../bin/thelib.so")
+
+print()
 lib.HelloFromGo()
+print("Go says the big number is:", f"{lib.CalculateLargeNumber():,}".replace(",", " "))
 
 print("And this is Python!")
+print()
--- languages/go/experiments/python/hello_go/src/main.go
+++ languages/go/experiments/python/hello_go/src/main.go
@@ -10,5 +10,16 @@ func HelloFromGo() {
 	fmt.Println("This is Go, answering the call.")
 }
 
+//export CalculateLargeNumber
+func CalculateLargeNumber() int64 {
+	var answer int64 = 0
+
+	for i := range 5_555_555 {
+		answer += int64(i)
+	}
+
+	return answer
+}
+
 // ----------------------------------------------------------------------------
 func main() {}
