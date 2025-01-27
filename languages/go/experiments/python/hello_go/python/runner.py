12406b7d2a38a326a02f374507a8ed809194fcd7
Ewald Horn <ewaldhorn@gmail.com>
2025-01-27 11:14:57 +0200

    Add some readability

--- languages/go/experiments/python/hello_go/python/runner.py
+++ languages/go/experiments/python/hello_go/python/runner.py
@@ -5,9 +5,16 @@ lib = ctypes.CDLL("../bin/thelib.so")
 # Important to set the correct return type, otherwise we get wrong answers
 lib.CalculateLargeNumber.restype = ctypes.c_longlong
 
+# header
 print()
+msg = "Python calling into a Go shared library."
+print(msg)
+print("-"*len(msg))
+
+# content
 bigNumber = lib.CalculateLargeNumber()
 print("Go says the big number is      :", bigNumber)
 
+# footer
 print("And this is Python saying it is:",f"{bigNumber:,}".replace(",", " "))
 print()
