2e29a8a8582f102f9ea3eb0dda3e2af3ff020652
Ewald Horn <ewaldhorn@gmail.com>
2025-01-29 11:11:52 +0200

    Step 3

--- languages/go/courses/udemy/programming_with_go/section7/void_functions/main.go
+++ languages/go/courses/udemy/programming_with_go/section7/void_functions/main.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 // ----------------------------------------------------------------------------
-func main() {
-	player1 := "Asmongold"
-	printPlayerInformation(player1, 30)
-
-	player2 := "Lowko"
-	printPlayerInformation(player2, 30)
+type Player struct {
+	name   string
+	health int
 }
 
 // ----------------------------------------------------------------------------
-func printPlayerInformation(player string, hps int) {
-	fmt.Printf("Player %s has %d hps\n", player, hps)
+func main() {
+	player1 := Player{name: "Asmongold", health: 30}
+	printPlayerInformation(player1)
+
+	player2 := Player{name: "Lowko", health: 30}
+	printPlayerInformation(player2)
+}
+
+// ----------------------------------------------------------------------------
+func printPlayerInformation(player Player) {
+	fmt.Printf("Player %s has %d hps\n", player.name, player.health)
 	fmt.Println("The battle is about to begin!")
 }
