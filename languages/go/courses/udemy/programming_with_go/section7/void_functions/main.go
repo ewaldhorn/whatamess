60909244411f8d93ea2189bb162e5af4bc6b2d48
Ewald Horn <ewaldhorn@gmail.com>
2025-01-29 11:16:50 +0200

    Step 4

--- languages/go/courses/udemy/programming_with_go/section7/void_functions/main.go
+++ languages/go/courses/udemy/programming_with_go/section7/void_functions/main.go
@@ -4,16 +4,18 @@ import "fmt"
 
 // ----------------------------------------------------------------------------
 type Player struct {
-	name   string
-	health int
+	name            string
+	health          int
+	attack, defense int
+	luck            int
 }
 
 // ----------------------------------------------------------------------------
 func main() {
-	player1 := Player{name: "Asmongold", health: 30}
+	player1 := Player{name: "Asmongold", health: 30, attack: 8, defense: 5, luck: 4}
 	printPlayerInformation(player1)
 
-	player2 := Player{name: "Lowko", health: 30}
+	player2 := Player{name: "Lowko", health: 30, attack: 8, defense: 5, luck: 3}
 	printPlayerInformation(player2)
 }
 
