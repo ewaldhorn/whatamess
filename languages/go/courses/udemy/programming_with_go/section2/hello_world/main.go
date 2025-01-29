a24845ae634ebe82b9d7233b12ed3a664acea6af
Ewald Horn <ewaldhorn@gmail.com>
2025-01-29 10:26:57 +0200

    Refresh course material

--- languages/go/courses/udemy/programming_with_go/section2/hello_world/main.go
+++ languages/go/courses/udemy/programming_with_go/section2/hello_world/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
-	"time"
 )
 
+// ----------------------------------------------------------------------------
 func main() {
 	fmt.Println("Alas, here we go again!")
 	problem_2()
@@ -16,37 +14,4 @@ func main() {
 	problem_6()
 	problem_7()
 	problem_8()
-}
-
-func problem_2() {
-	fmt.Println("Errr, it's Ewald?")
-}
-
-func problem_3() {
-	fmt.Println("I'm Ewald.\nI do things with computers.\nSometimes it works.")
-}
-
-func problem_4() {
-	for i := range 10 {
-		fmt.Printf("%d ", i+1)
-	}
-	fmt.Println()
-}
-
-func problem_5() {
-	fmt.Printf("2 ** 11 is %.0f\n", math.Pow(2.0, 11.0))
-}
-
-func problem_6() {
-	for i := range 1000 {
-		fmt.Println(i + 1)
-	}
-}
-
-func problem_7() {
-	fmt.Println("Your lucky number is:", rand.Intn(11))
-}
-
-func problem_8() {
-	fmt.Println("The current date is", time.Now())
 }
