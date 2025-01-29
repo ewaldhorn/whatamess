85af1e505d477d3e362ad8ca76a00fd87920f3a7
Ewald Horn <ewaldhorn@gmail.com>
2025-01-29 11:08:25 +0200

    Step 1

---
+++ languages/go/courses/udemy/programming_with_go/section7/void_functions/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	player1 := "Asmongold"
+	fmt.Printf("Player %s has 30 hps\n", player1)
+	fmt.Println("The battle is about to begin!")
+
+	player2 := "Lowko"
+	fmt.Printf("Player %s has 30 hps\n", player2)
+	fmt.Println("The battle is about to begin!")
+
+}
