e921776914032450b8ab916cdb1bd49da0d9cb75
Ewald Horn <ewaldhorn@gmail.com>
2025-02-06 17:16:43 +0200

    add a lifo struct

---
+++ languages/go/experiments/queues/lifo.go
@@ -0,0 +1,22 @@
+package main
+
+type Lifo struct {
+	items []interface{}
+}
+
+func NewLifo() *Lifo {
+	return &Lifo{items: make([]interface{}, 0)}
+}
+
+func (l *Lifo) Push(x interface{}) {
+	l.items = append([]interface{}{x}, l.items...)
+}
+
+func (l *Lifo) Pop() interface{} {
+	if len(l.items) == 0 {
+		return nil
+	}
+	x := l.items[0]
+	l.items = l.items[1:]
+	return x
+}
