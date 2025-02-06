122ace177599c7b996389989d2b741c151eec2d6
Ewald Horn <ewaldhorn@gmail.com>
2025-02-06 17:16:32 +0200

    Add a fifo struct

---
+++ languages/go/experiments/queues/fifo.go
@@ -0,0 +1,22 @@
+package main
+
+type Fifo struct {
+	items []interface{}
+}
+
+func NewFifo() *Fifo {
+	return &Fifo{items: make([]interface{}, 0)}
+}
+
+func (f *Fifo) Push(x interface{}) {
+	f.items = append(f.items, x)
+}
+
+func (f *Fifo) Pop() interface{} {
+	if len(f.items) == 0 {
+		return nil
+	}
+	x := f.items[0]
+	f.items = f.items[1:]
+	return x
+}
