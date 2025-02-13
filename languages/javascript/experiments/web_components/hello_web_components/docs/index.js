ed7fc327125527aab332108915baf6184022fb85
Ewald Horn <ewaldhorn@gmail.com>
2025-02-13 14:38:07 +0200

    Add the header component to the page

--- languages/javascript/experiments/web_components/hello_web_components/docs/index.js
+++ languages/javascript/experiments/web_components/hello_web_components/docs/index.js
@@ -1,6 +1,5 @@
 class b extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"})}connectedCallback(){this.render()}render(){this.shadowRoot.innerHTML=`
         <style>
-          /* NoFuss Footer Element */
             footer {
               display: block;
               background-color: #333;
@@ -31,6 +30,7 @@ class b extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"
               text-decoration: none;
             }
           </style>
+
           <footer>
             <p>&copy; 2025 NoFuss Solutions</p>
             <nav>
@@ -41,4 +41,48 @@ class b extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"
               </ul>
             </nav>
           </footer>
-        `}}var c=()=>{customElements.define("nofuss-footer",b)};c();
+        `}}class c extends HTMLElement{constructor(){super();this.attachShadow({mode:"open"})}connectedCallback(){this.render()}render(){this.shadowRoot.innerHTML=`
+        <style>
+            header {
+              display: block;
+              background-color: #333;
+              color: #fff;
+              padding: 20px;
+              text-align: center;
+            }
+
+            header {
+              max-width: 1000px;
+              margin: 0 auto;
+            }
+
+            header nav ul {
+              list-style: none;
+              margin: 0;
+              padding: 0;
+              display: flex;
+              justify-content: space-around;
+            }
+
+            header nav ul li {
+              margin-right: 20px;
+            }
+
+            header nav a {
+              color: #fff;
+              text-decoration: none;
+            }
+          </style>
+
+          <header>
+            <h1>NoFuss Solutions</h1>
+            <h2>Web Components Experiment</h2>
+            <nav>
+              <ul>
+                <li><a href="https://nofuss.co.za/" target="_blank">Home</a></li>
+                <li><a href="https://nofuss.co.za/about/" target="_blank">About</a></li>
+                <li><a href="https://nofuss.co.za/contact/" target="_blank">Contact</a></li>
+              </ul>
+            </nav>
+          </header>
+        `}}var i=()=>{customElements.define("nofuss-header",c),customElements.define("nofuss-footer",b)};i();
--- languages/javascript/experiments/web_components/hello_web_components/src/index.js
+++ languages/javascript/experiments/web_components/hello_web_components/src/index.js
@@ -1,7 +1,9 @@
 import { NoFussFooter } from "./nofuss_footer.js";
+import { NoFussHeader } from "./nofuss_header.js";
 
 // ----------------------------------------------------------------------------
 const loadCustomElements = () => {
+  customElements.define("nofuss-header", NoFussHeader);
   customElements.define("nofuss-footer", NoFussFooter);
 };
 
