2cc549d52b4e5583371c763639e86dba24525fc7
Ewald Horn <ewaldhorn@gmail.com>
2025-02-14 12:42:25 +0200

    Added some extra tests

--- languages/javascript/experiments/strings/vowel_count.js
+++ languages/javascript/experiments/strings/vowel_count.js
@@ -15,4 +15,11 @@ const getVowelCount = (string) => {
     return count;
 }
 
-console.log("Vowel count for 'hello' is expected to be 2:", getVowelCount("hello") === 2);
+console.log("Vowel count for 'hello' is expected to be 2        :", getVowelCount("hello") === 2);
+console.log("Vowel count for 'world' is expected to be 1        :", getVowelCount("world") === 1);
+console.log("Vowel count for 'AEIOU' is expected to be 5        :", getVowelCount("AEIOU") === 5);
+console.log("Vowel count for 'bcdfg' is expected to be 0        :", getVowelCount("bcdfg") === 0);
+console.log("Vowel count for 'rhythm' is expected to be 0       :", getVowelCount("rhythm") === 0);
+console.log("Vowel count for 'aEiOu' is expected to be 5        :", getVowelCount("aEiOu") === 5);
+console.log("Vowel count for an empty string is expected to be 0:", getVowelCount("") === 0);
+
