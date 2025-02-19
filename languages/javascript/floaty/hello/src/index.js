78fabf3288308ad23ac84822e30a588644793b6b
Ewald Horn <ewaldhorn@gmail.com>
2025-02-19 10:16:25 +0200

    Bouncing

--- languages/javascript/floaty/hello/src/index.js
+++ languages/javascript/floaty/hello/src/index.js
@@ -1,115 +1,32 @@
 import { Engine } from "https://floaty.dev/engine-v1.js";
+import { Points } from "./points";
 
-var engine = new Engine();
+// ----------------------------------------------------------------------------
+//                                                            START YOUR ENGINE
+let engine = new Engine();
 engine.expose();
 
-var sprites = {
-  // this is where your sprites go
-};
-
-var sounds = {
-  // this is where your sounds go
-};
-
-class Stars {
-  limit = 50;
-
-  init(engine) {
-    this.engine = engine;
-    this.stars = [];
-
-    for (var i = 0; i < this.limit; i++) {
-      this.stars.push(
-        new Star({
-          x: Math.floor(rnd(128)),
-          y: Math.floor(rnd(128)),
-          type: Math.floor(rnd(3)),
-          engine: this.engine,
-        }),
-      );
-    }
-  }
-
-  update() {
-    this.stars = this.stars.filter((star) => star.y < 128);
-
-    while (this.stars.length < this.limit) {
-      this.stars.push(
-        new Star({
-          x: Math.floor(rnd(128)),
-          y: 0,
-          type: Math.floor(rnd(3)),
-          engine: this.engine,
-        }),
-      );
-    }
-
-    for (var star of this.stars) {
-      star.update();
-    }
-  }
-
-  draw() {
-    for (var star of this.stars) {
-      star.draw();
-    }
-  }
-}
-class Star {
-  x = 0;
-  y = 0;
-  type = 0;
-
-  constructor({ x, y, type, engine }) {
-    this.x = x;
-    this.y = y;
-    this.type = type;
-    this.engine = engine;
-  }
-
-  update() {
-    if (this.type === 0) {
-      this.y += 1.5;
-    }
-
-    if (this.type === 1) {
-      this.y += 1.0;
-    }
-
-    if (this.type === 2) {
-      this.y += 0.5;
-    }
-  }
-
-  draw() {
-    if (this.type === 0) {
-      pset(this.x, this.y, 6);
-    }
-
-    if (this.type === 1) {
-      pset(this.x, this.y, 13);
-    }
-
-    if (this.type === 2) {
-      pset(this.x, this.y, 1);
-    }
-  }
-}
+// ----------------------------------------------------------------------------
+//                                                           SPRITES AND SOUNDS
+let sprites = {};
+let sounds = {};
+/*
+  This little Floaty gamelet doesn't use sounds or sprites, so empty objects
+  are fine.
+*/
 
 let mustClearScreen = true;
 
-var stars = new Stars();
+var points = new Points();
 
 async function init() {
-  stars.init(this);
+  points.init(this);
 
   await this.load();
-
-  mode = "start";
 }
 
 function update() {
-  stars.update();
+  points.update();
 }
 
 function draw() {
@@ -117,7 +34,7 @@ function draw() {
     cls();
   }
 
-  stars.draw();
+  points.draw();
 
   // Clear the screen
   if (btnp("c")) {
---
+++ languages/javascript/floaty/hello/src/points.js
@@ -0,0 +1,86 @@
+import { isRandomTrue } from "./utils";
+
+// ----------------------------------------------------------------------------
+export class Points {
+  limit = 50;
+
+  init(engine) {
+    this.engine = engine;
+    this.points = [];
+
+    for (var i = 0; i < this.limit; i++) {
+      this.points.push(
+        new Point({
+          x: 3 + Math.floor(rnd(120)),
+          y: 3 + Math.floor(rnd(120)),
+          engine: this.engine,
+        }),
+      );
+    }
+  }
+
+  update() {
+    this.points = this.points.filter((point) => point.y < 128);
+
+    while (this.points.length < this.limit) {
+      this.points.push(
+        new Point({
+          x: 3 + Math.floor(rnd(120)),
+          y: 3 + Math.floor(rnd(120)),
+          engine: this.engine,
+        }),
+      );
+    }
+
+    for (var point of this.points) {
+      point.update();
+    }
+  }
+
+  draw() {
+    for (var point of this.points) {
+      point.draw();
+    }
+  }
+}
+
+// ----------------------------------------------------------------------------
+class Point {
+  x = 0;
+  y = 0;
+  xv = 0;
+  yv = 0;
+
+  constructor({ x, y, engine }) {
+    this.x = x;
+    this.y = y;
+    this.engine = engine;
+
+    this.xv = 1 + Math.floor(Math.random() * 2);
+    this.yv = 1 + Math.floor(Math.random() * 2);
+
+    if (isRandomTrue()) {
+      this.xv *= -1;
+    }
+
+    if (isRandomTrue()) {
+      this.yv *= -1;
+    }
+  }
+
+  update() {
+    this.x += this.xv;
+    this.y += this.yv;
+
+    if (this.x < 2 || this.x > 126) {
+      this.xv *= -1;
+    }
+    if (this.y < 2 || this.y > 126) {
+      this.yv *= -1;
+    }
+  }
+
+  draw() {
+    pset(this.x, this.y, 12);
+  }
+}
