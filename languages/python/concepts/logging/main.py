22a534689aee28f0c3b93cd95e27cc20c7fe9ec9
Ewald Horn <ewaldhorn@gmail.com>
2025-02-22 09:35:55 +0200

    Refreshing Python logging memories

---
+++ languages/python/concepts/logging/main.py
@@ -0,0 +1,10 @@
+import logging
+
+# ensure everything is logged, not just warnings and above, as per default
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("This is a debug message")
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
