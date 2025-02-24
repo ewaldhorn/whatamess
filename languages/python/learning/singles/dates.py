d2c892ba14df686e97365961ae5d01732840f34c
Ewald Horn <ewaldhorn@gmail.com>
2025-02-24 21:29:05 +0200

    Formatting

--- languages/python/learning/singles/dates.py
+++ languages/python/learning/singles/dates.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 
 
+# -----------------------------------------------------------------------------
 def display_current_time_raw():
     print(datetime.now())
 
 
+# -----------------------------------------------------------------------------
 def say_hello(who):
     print(f"Hello there {who}.")
 
 
+# -----------------------------------------------------------------------------
 def get_time_str():
     current_hour = datetime.now().hour
 
@@ -21,11 +24,13 @@ def get_time_str():
             return "Good Evening"
 
 
+# -----------------------------------------------------------------------------
 def say_hello_time_sensitive(who):
     time_str = get_time_str()
     print(f"{time_str} {who}!")
 
 
+# -----------------------------------------------------------------------------
 display_current_time_raw()
 say_hello("Bob")
 say_hello_time_sensitive("Tina")
