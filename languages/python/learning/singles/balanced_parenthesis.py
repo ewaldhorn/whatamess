81d67654521f5973c956628142a9d6a11b1efac3
Ewald Horn <ewaldhorn@gmail.com>
2025-02-24 16:34:27 +0200

    Add better tests

--- languages/python/learning/singles/balanced_parenthesis.py
+++ languages/python/learning/singles/balanced_parenthesis.py
@@ -10,8 +10,26 @@
 #   The length of the input string will not exceed 100 characters.
 
 
+def is_odd(n):
+    return n % 2 != 0
+
+
 def balanced_parentheses(input):
-    return False
+    if len(input) == 0:
+        return True
+
+    if is_odd(len(input)):
+        return False
+
+    balance = 0
+    for char in input:
+        if char == "(":
+            balance += 1
+        elif char == ")":
+            if balance == 0:
+                return False
+            balance -= 1
+    return balance == 0
 
 
 # tests
@@ -39,7 +57,7 @@ class TestBalancedParentheses(unittest.TestCase):
         self.assertFalse(balanced_parentheses("(())))))"))
 
     def test_large_inputs(self):
-        large_balanced = "(((()" + ")" * 100 + ")"
+        large_balanced = "(" + "((((()()(((((())))))))))" * 100 + ")"
         large_unbalanced = "(()" + ")" * 100 + ")"
         self.assertTrue(balanced_parentheses(large_balanced))
         self.assertFalse(balanced_parentheses(large_unbalanced))
