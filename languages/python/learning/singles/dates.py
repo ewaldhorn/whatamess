a9bcff4be449900790361f2369e01cd496246603
Ewald Horn <ewaldhorn@gmail.com>
2025-02-24 21:22:12 +0200

    Before actual time check done

--- languages/python/learning/singles/dates.py
+++ languages/python/learning/singles/dates.py
@@ -9,5 +9,15 @@ def say_hello(who):
     print(f"Hello there {who}.")
 
 
+def get_time_str():
+    return "Good morning"
+
+
+def say_hello_time_sensitive(who):
+    time_str = get_time_str()
+    print(f"{time_str} {who}!")
+
+
 display_current_time_raw()
 say_hello("Bob")
+say_hello_time_sensitive("Tina")
