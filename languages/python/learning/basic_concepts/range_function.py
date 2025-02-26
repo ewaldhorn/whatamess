d807323351614964eaa6f44427ecc1b1cbcf3e8f
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 15:33:52 +0200

    Add starts and steps to ranges

--- languages/python/learning/basic_concepts/range_function.py
+++ languages/python/learning/basic_concepts/range_function.py
@@ -1,5 +1,16 @@
 # Exploring Python's range function a bit.
 
+print("Just a range of numbers, from 0 to < 10")
 nums = range(10)
+print(nums)
+print(list(nums))
+
+print("\nSame thing, but starting at 1")
+nums = range(1,10)
+print(nums)
+print(list(nums))
+
+print("\nSame thing, but with a step of 2")
+nums = range(1,10,2)
 print(nums)
 print(list(nums))
