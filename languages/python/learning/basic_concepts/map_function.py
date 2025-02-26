b7bda4b411ef3269a26f4e21b743ab8db9f50f9e
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 15:55:25 +0200

    Use map to apply a function to a collection

--- languages/python/learning/basic_concepts/map_function.py
+++ languages/python/learning/basic_concepts/map_function.py
@@ -3,3 +3,10 @@
 numbers = [i+1 for i in range(10)]
 print("List of numbers          :", numbers)
 print("List of numbers (doubled):", list(map(lambda x: x*2, numbers)))
+
+def tripler(n):
+    return n * 3
+
+tripled_numbers = map(tripler, numbers)
+print("List of numbers :", numbers)
+print("Numbers tripled :", list(tripled_numbers))
