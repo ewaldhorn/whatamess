c393cf4dffd8902f0ce8b85454b01872e5640193
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 13:11:02 +0200

    Add a seperator for strings in print

---
+++ languages/python/learning/basic_concepts/print_function.py
@@ -0,0 +1,5 @@
+# Exploring the print function in Python beyond the basic usage.
+
+numbers = [i+1 for i in range(10)]
+print("List of numbers", numbers)
+print("List of numbers", numbers, sep=" => ")
