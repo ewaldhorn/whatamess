5245bde1be28818300642a0d537823ba68a2a427
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 16:24:23 +0200

    Also looking at sorted()

--- languages/python/learning/basic_concepts/sum_function.py
+++ languages/python/learning/basic_concepts/sum_function.py
@@ -1,5 +1,10 @@
 # Refresh Python sum function
 
-numbers = {1,4,5,2,3}
+numbers = [1,4,5,2,3]
 print(sum(numbers))
 print(sum(numbers,-8)) # give a starting point, default is 0
+
+sorted_numbers = sorted(numbers)
+print("The numbers are:", numbers)
+print("Sorted are     :", sorted_numbers)
+print("Descending are :", sorted(numbers,reverse=True))
