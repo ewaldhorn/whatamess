efe9e3940305319740137c7202cdd5d4dd48f63d
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 12:11:45 +0200

    Python imports

--- languages/python/learning/basic_concepts/main.py
+++ languages/python/learning/basic_concepts/main.py
@@ -10,7 +10,7 @@ x = (1, 2)
 # x[0] = 1 # won't work, can't assign to tuple
 y = x
 x = (1, 2, 3)
-print("Immutables:", x, y)
+print("Immutable:", x, y)
 
 # Mutable
 # list
@@ -19,4 +19,9 @@ print("Immutables:", x, y)
 a = [1, 2]
 b = a
 a[0] = 4
-print("Mutables:", a, b)
+print("Mutable:", a, b)
+
+print("\n")
+
+from some_library import summer
+print("The sum of 4 and 5 is:", summer(4, 5))
---
+++ languages/python/learning/basic_concepts/some_library.py
@@ -0,0 +1,5 @@
+def summer(left,right):
+    return left+right
+
+if __name__ == "__main__":
+    print(summer(4,5))
