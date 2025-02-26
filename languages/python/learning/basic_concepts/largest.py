89b21fb87ec8b3c6eb366b7a0dd10752597d9267
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 10:07:05 +0200

    Copy is also an option

--- languages/python/learning/basic_concepts/largest.py
+++ languages/python/learning/basic_concepts/largest.py
@@ -7,17 +7,30 @@ def get_largest(numbers,n):
 def get_largest_safe(numbers,n):
   return sorted(numbers)[-n:]
 
-sample = [2,3,4,1,34,123,321,1]
+# we can also make a copy of the list
+def get_largest_copy(numbers,n):
+  myList = numbers.copy()
+  myList.sort()
+  return myList[-n:]
 
+sample = [2,3,4,1,34,123,321,1]
+print("\nOops")
 print("Sample:",sample)
 result = get_largest(sample,3)
 print("Result:",result)
 print("Sample:",sample)
 
-print("\n\n")
+print("\n\nSorted")
 
 otherSample = [2,3,4,1,34,123,321,1]
 print("Sample:",otherSample)
 result = get_largest_safe(otherSample,3)
+print("Result:",result)
+print("Sample:",otherSample)
+
+print("\n\nCopy")
+
+print("Sample:",otherSample)
+result = get_largest_copy(otherSample,3)
 print("Result:",result)
 print("Sample:",otherSample)
