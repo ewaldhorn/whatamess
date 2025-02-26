409230ff46afcd01c8777b8f254f34fda1556b64
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 10:01:15 +0200

    Solution for not changing passed in list

--- languages/python/learning/basic_concepts/largest.py
+++ languages/python/learning/basic_concepts/largest.py
@@ -1,6 +1,11 @@
+# .sort() sorts the list in-place
 def get_largest(numbers,n):
   numbers.sort()
   return numbers[-n:]
+
+# .sorted() returns a new, sorted copy of the list
+def get_largest_safe(numbers,n):
+  return sorted(numbers)[-n:]
 
 sample = [2,3,4,1,34,123,321,1]
 
@@ -8,3 +13,11 @@ print("Sample:",sample)
 result = get_largest(sample,3)
 print("Result:",result)
 print("Sample:",sample)
+
+print("\n\n")
+
+otherSample = [2,3,4,1,34,123,321,1]
+print("Sample:",otherSample)
+result = get_largest_safe(otherSample,3)
+print("Result:",result)
+print("Sample:",otherSample)
