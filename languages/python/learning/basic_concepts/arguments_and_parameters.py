a9f10ea27bdf59975cfda069e8f7e339369bc839
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 11:35:11 +0200

    Expand arguments and parameters refresher

--- languages/python/learning/basic_concepts/arguments_and_parameters.py
+++ languages/python/learning/basic_concepts/arguments_and_parameters.py
@@ -1,8 +1,11 @@
 def some_function(left, right, swap = False):
     if swap:
         left, right = right, left   
+
     print("Left:",left,"Right:",right)
 
 some_function(4,5)
 some_function(right=5,left=4)
 some_function(right=5,left=4, swap=True)
+some_function(10,11)
+some_function(10,11,True)
