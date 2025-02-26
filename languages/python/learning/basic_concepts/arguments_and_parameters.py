a3c6be68806e4136e7d3ac60b3213bd657c886e4
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 11:41:39 +0200

    Variadic arguments

--- languages/python/learning/basic_concepts/arguments_and_parameters.py
+++ languages/python/learning/basic_concepts/arguments_and_parameters.py
@@ -4,8 +4,29 @@ def some_function(left, right, swap = False):
 
     print("Left:",left,"Right:",right)
 
+
+def sum_them_all(*nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total   
+
 some_function(4,5)
 some_function(right=5,left=4)
 some_function(right=5,left=4, swap=True)
 some_function(10,11)
 some_function(10,11,True)
+
+print("\n")
+nums = [1]
+print("The sum of",nums,"is",sum_them_all(*nums))
+nums = [1,1]
+print("The sum of",nums,"is",sum_them_all(*nums))
+nums = [1,2]
+print("The sum of",nums,"is",sum_them_all(*nums))
+nums = [1,2,3]
+print("The sum of",nums,"is",sum_them_all(*nums))
+nums = [1,1,2,2]
+print("The sum of",nums,"is",sum_them_all(*nums))
+nums = [1,2,1,2]
+print("The sum of",nums,"is",sum_them_all(*nums))
