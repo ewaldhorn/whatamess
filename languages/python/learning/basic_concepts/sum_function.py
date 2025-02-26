03b5d86194763b7d65713896f8e4b6b519905b6e
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 16:28:54 +0200

    Sorting through objects

--- languages/python/learning/basic_concepts/sum_function.py
+++ languages/python/learning/basic_concepts/sum_function.py
@@ -8,3 +8,8 @@ sorted_numbers = sorted(numbers)
 print("The numbers are:", numbers)
 print("Sorted are     :", sorted_numbers)
 print("Descending are :", sorted(numbers,reverse=True))
+
+contestants = [{"name":"Joe", "score":11},{"name":"Sandra","score":9},{"name":"Bob","score":12},{"name":"Alice","score":8}]
+print("Contestants",contestants)
+print("Sorted by name",sorted(contestants,key=lambda x: x["name"]))
+print("Sorted by score",sorted(contestants,key=lambda x: x["score"],reverse=True))
