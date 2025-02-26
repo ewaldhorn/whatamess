a9bc79092ca6dacc76bd721fc76ad4c46adb5423
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 11:34:10 +0200

    Add optional parameters

--- languages/python/learning/basic_concepts/arguments_and_parameters.py
+++ languages/python/learning/basic_concepts/arguments_and_parameters.py
@@ -1,5 +1,8 @@
-def some_function(left, right):
+def some_function(left, right, swap = False):
+    if swap:
+        left, right = right, left   
     print("Left:",left,"Right:",right)
 
 some_function(4,5)
 some_function(right=5,left=4)
+some_function(right=5,left=4, swap=True)
