7abf54491808ba4011b9e321861640f36863e56d
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 11:01:26 +0200

    Creating lists using list comprehension

--- languages/python/learning/basic_concepts/list_comprehensions.py
+++ languages/python/learning/basic_concepts/list_comprehensions.py
@@ -1,1 +1,9 @@
 # Just refreshing my list comprehension in Python.
+
+# create a list of numbers from 1..10, i+1 to account for ranges being 0-based
+numbers = [i+1 for i in range(10)]
+print("List of numbers", numbers)
+
+# create a list of lists using list comprehension
+lists = [[j+1 for j in range(3)] for i in range(5)]
+print("List of lists", lists)
