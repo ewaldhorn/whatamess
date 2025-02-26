4fecec6dcd823a32d4472b04561a66644c8dc128
Ewald Horn <ewaldhorn@gmail.com>
2025-02-26 16:14:48 +0200

    Filter works with a function or a lambda

--- languages/python/learning/basic_concepts/filter_function.py
+++ languages/python/learning/basic_concepts/filter_function.py
@@ -3,3 +3,8 @@
 numbers = [i+1 for i in range(10)]
 print("List of numbers", numbers)
 print("Numbers greater than 5", list(filter(lambda x: x > 5, numbers)))
+
+def is_odd(b):
+    return b % 2 == 1
+
+print("Odd numbers", list(filter(is_odd, numbers)))
