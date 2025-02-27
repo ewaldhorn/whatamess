5a455af410eb0a6ac8e5990b964a7e97a85b109b
Ewald Horn <ewaldhorn@gmail.com>
2025-02-27 13:06:42 +0200

    Also add the ability to ban folk

--- languages/python/learning/smalls/pinger/decisions.py
+++ languages/python/learning/smalls/pinger/decisions.py
@@ -1,6 +1,13 @@
+nopes = ["tony", "giselle"]
+
+
 # -----------------------------------------------------------------------------
 def grant_access(name: str, age: int, has_id: bool) -> bool:
     if not has_id:
+        return False
+
+    if name.lower() in nopes:
+        print(f"You've been banned {name}!")
         return False
 
     if age >= 18:
@@ -15,6 +22,8 @@ def main() -> None:
     assert grant_access("Niel", 19, False) is False
     assert grant_access("Daniel", 17, True) is False
     assert grant_access("Michelle", 28, False) is False
+    assert grant_access("Giselle", 28, True) is False
+    assert grant_access("Tony", 25, True) is False
     assert grant_access("Sandra", 19, True) is True
     assert grant_access("Dirko", 33, True) is True
 
