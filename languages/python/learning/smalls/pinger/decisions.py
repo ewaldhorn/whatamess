7d2f020e6e2d8b9f12d4b2cefdaecc6c0091ebb5
Ewald Horn <ewaldhorn@gmail.com>
2025-02-27 12:57:33 +0200

    Add tests

--- languages/python/learning/smalls/pinger/decisions.py
+++ languages/python/learning/smalls/pinger/decisions.py
@@ -1,11 +1,22 @@
 # -----------------------------------------------------------------------------
-def grant_access() -> bool:
+def grant_access(name: str, age: int, has_id: bool) -> bool:
+    if not has_id:
+        return False
+
+    if age >= 18:
+        print(f"Welcome {name}!")
+        return True
+
     return False
 
 
 # -----------------------------------------------------------------------------
 def main() -> None:
-    pass
+    assert grant_access("Niel", 19, False) is False
+    assert grant_access("Daniel", 17, True) is False
+    assert grant_access("Michelle", 28, False) is False
+    assert grant_access("Sandra", 19, True) is True
+    assert grant_access("Dirko", 33, True) is True
 
 
 # ================================================================= ENTRY POINT
