31a6eddea3b481b73a478bcd95c5e774d639a42d
Ewald Horn <ewaldhorn@gmail.com>
2025-02-27 13:30:54 +0200

    Add type hints to make intention clear and provide editor hints when things go off the rails

--- languages/python/learning/smalls/pinger/types.py
+++ languages/python/learning/smalls/pinger/types.py
@@ -1,6 +1,8 @@
-def upper_case(items):
+# -----------------------------------------------------------------------------
+def upper_case(items: list[str]) -> list[str]:
     return [item.upper() for item in items]
 
 
+# ================================================================= ENTRY POINT
 if __name__ == "__main__":
-    print(upper_case(["ted", "bill", "10", 20, False]))
+    print(upper_case(["ted", "bill", "10"]))
