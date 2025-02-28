058f440832c284fbae8497511857bf625c36acb5
Ewald Horn <ewaldhorn@gmail.com>
2025-02-28 09:57:08 +0200

    Configure Py linte

---
+++ languages/python/learning/smalls/pinger/linting.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------------------------
+def linting():
+    # print "This is linting!" # gives lint errors
+    print("This is linting!")
+    x = 1
+    y = 2
+    print(x + y)
+
+
+# ================================================================= ENTRY POINT
+if __name__ == "__main__":
+    linting()
