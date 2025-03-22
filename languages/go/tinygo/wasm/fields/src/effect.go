4a58521fb041aa3e9d6637e172b72fb08d164745
Ewald Horn <ewaldhorn@gmail.com>
2025-03-22 17:58:20 +0200

    Be able to create many particles at once

--- languages/go/tinygo/wasm/fields/src/effect.go
+++ languages/go/tinygo/wasm/fields/src/effect.go
@@ -2,13 +2,16 @@ package main
 
 // ----------------------------------------------------------------------------
 type Effect struct {
-	width, height int
-	particles     []Particle
+	width, height   int
+	particlesWanted int
+	particles       []Particle
 }
 
 // ----------------------------------------------------------------------------
 func (e *Effect) init() {
-	e.particles = append(e.particles, *NewParticle(e))
+	for _ = range e.particlesWanted {
+		e.particles = append(e.particles, *NewParticle(e))
+	}
 }
 
 // ----------------------------------------------------------------------------
@@ -21,9 +24,10 @@ func (e *Effect) render() {
 // ----------------------------------------------------------------------------
 func NewEffect(width, height int) *Effect {
 	newEffect := Effect{
-		width:     width,
-		height:    height,
-		particles: []Particle{},
+		width:           width,
+		height:          height,
+		particlesWanted: 50,
+		particles:       []Particle{},
 	}
 
 	newEffect.init()
