25526b415a3c668ad835803f4aae5c5c87891f51
Ewald Horn <ewaldhorn@gmail.com>
2025-03-22 17:42:36 +0200

    Fix Main

---
+++ languages/go/tinygo/wasm/fields/src/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/ewaldhorn/dommie/dom"
+	"github.com/ewaldhorn/tinycanvas/tinycanvas"
+)
+
+// ------------------------------------------ Externally provided by JavaScript
+
+//export bootstrap
+func bootstrap()
+
+//export animateCanvasOne
+func animateCanvasOne()
+
+// -------------------------------------------------------------------- GLOBALS
+var canvasOne *tinycanvas.TinyCanvas
+
+// ----------------------------------------------------------------------------
+func main() {
+	setCallbacks()
+	dom.Hide("loading")
+	dom.Show("controls")
+	dom.Show("information")
+	bootstrap()
+	canvasOne = tinycanvas.NewTinyCanvas(800, 600)
+	initEffects()
+
+	performDemoOnCanvasOne()
+
+	// prevent the app for closing - it stays running for the life of the webpage
+	ch := make(chan struct{})
+	<-ch
+}
+
+// ----------------------------------------------------------------------------
+func setCallbacks() {
+	setVersionCallback()
+	setRefreshCallback()
+}
+
+// ----------------------------------------------------------------------------
+func setRefreshCallback() {
+	dom.AddEventListener("refreshButton", "click", func() {
+		if canvasOne != nil {
+			performDemoOnCanvasOne()
+		}
+	})
+}
