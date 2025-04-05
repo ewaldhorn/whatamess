20425906b2978da886da17d491144b5c34f49cbe
Ewald Horn <ewaldhorn@gmail.com>
2025-04-05 11:03:24 +0200

    Reverse

---
+++ languages/dart/learning/diff_from_python/reverse.dart
@@ -0,0 +1,5 @@
+void main() {
+  final str = 'Hello, World!';
+  final reversed = str.split('').reversed.join('');
+  print(reversed); // prints '!dlroW ,olleH'
+}
---
+++ languages/dart/learning/diff_from_python/reverse.py
@@ -0,0 +1,3 @@
+str = 'Hello, World!'
+reversed_str = str[::-1]  # extracts '!dlroW ,olleH'
+print(reversed_str)
