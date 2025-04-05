6f5f151691bed28b2e9560dfcff6637a0a954b54
Ewald Horn <ewaldhorn@gmail.com>
2025-04-05 11:05:11 +0200

    Words to string

---
+++ languages/dart/learning/diff_from_python/words_to_string.dart
@@ -0,0 +1,5 @@
+void main() {
+  final words = ['Hello,', 'World!'];
+  final str = words.join(' ');
+  print(str); // prints 'Hello, World!'
+}
---
+++ languages/dart/learning/diff_from_python/words_to_string.py
@@ -0,0 +1,3 @@
+words = ['Hello,', 'World!']
+str = ' '.join(words)  # joins words into a single string
+print(str)  # prints 'Hello, World!'
