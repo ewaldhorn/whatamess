0660d958d828c941c531c85a1b75f755eff8f64f
Ewald Horn <ewaldhorn@gmail.com>
2025-04-05 11:03:56 +0200

    Find substring

---
+++ languages/dart/learning/diff_from_python/contains_substring.dart
@@ -0,0 +1,5 @@
+void main() {
+  final str = 'Hello, World!';
+  final contains = str.contains('World');
+  print(contains); // prints true
+}
---
+++ languages/dart/learning/diff_from_python/contains_substring.py
@@ -0,0 +1,3 @@
+str = 'Hello, World!'
+contains = 'World' in str  # checks if 'World' is in str
+print(contains)  # prints True
