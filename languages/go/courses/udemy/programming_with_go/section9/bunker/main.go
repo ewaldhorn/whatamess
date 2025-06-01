b5d34bf6e03d9be218e41015d8c13e255b8b918d
Ewald Horn <ewaldhorn@gmail.com>
2025-06-01 18:05:44 +0200

    Move screen rendering calls into a function

--- languages/go/courses/udemy/programming_with_go/section9/bunker/main.go
+++ languages/go/courses/udemy/programming_with_go/section9/bunker/main.go
@@ -9,10 +9,17 @@ import (
 // ----------------------------------------------------------------------------
 func displayHelloWorld(screen tcell.Screen) {
 	w, h := screen.Size()
-	screen.Clear()
 	style := tcell.StyleDefault.Foreground(tcell.ColorCadetBlue).Background(tcell.ColorOrangeRed)
 	displayText(screen, w/2-7, h/2, style, "Hello, World!")
 	displayText(screen, w/2-9, h/2+1, tcell.StyleDefault, "Press ESC to exit.")
+}
+
+// ----------------------------------------------------------------------------
+func renderGameScreen(screen tcell.Screen) {
+	screen.Clear()
+
+	displayHelloWorld(screen)
+	display(screen, 5, 5, 10, 5, '*')
 
 	screen.Show()
 }
@@ -20,18 +27,13 @@ func displayHelloWorld(screen tcell.Screen) {
 // ----------------------------------------------------------------------------
 func main() {
 	screen := initScreen()
-
-	displayHelloWorld(screen)
-	display(screen, 5, 5, 10, 5, '*')
-	screen.Show()
+	renderGameScreen(screen) // not sure if this is needed, I almost think not?
 
 	for {
 		switch ev := screen.PollEvent().(type) {
 		case *tcell.EventResize:
 			screen.Sync()
-			displayHelloWorld(screen)
-			display(screen, 5, 5, 10, 5, '*')
-			screen.Show()
+			renderGameScreen(screen)
 		case *tcell.EventKey:
 			if ev.Key() == tcell.KeyEscape {
 				screen.Fini()
