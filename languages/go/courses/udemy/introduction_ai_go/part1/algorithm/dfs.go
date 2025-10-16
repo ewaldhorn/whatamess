fde02c3b6cc9095f80fe5332348594fb959ad24b
Ewald Horn <ewaldhorn@gmail.com>
2025-10-16 17:09:30 +0200

    Working on neighbours

--- languages/go/courses/udemy/introduction_ai_go/part1/algorithm/dfs.go
+++ languages/go/courses/udemy/introduction_ai_go/part1/algorithm/dfs.go
@@ -2,8 +2,12 @@ package algorithm
 
 import (
 	"fmt"
+	"math/rand"
 	"mazes/maze"
 	"mazes/node"
+	"mazes/point"
+	"mazes/solution"
+	"slices"
 )
 
 // ------------------------------------------------------------------------------------------------
@@ -53,4 +57,97 @@ func (d *DepthFirstSearch) Remove() (*node.Node, error) {
 	}
 
 	return nil, fmt.Errorf("Frontier is empty, can't remove anything!")
+}
+
+// ------------------------------------------------------------------------------------------------
+func (d *DepthFirstSearch) Neighbours(n *node.Node) []*node.Node {
+	y := n.State.Y
+	x := n.State.X
+
+	candidates := []*node.Node{
+		{State: point.Point{Y: y - 1, X: x}, Parent: n, Action: "up"},
+		{State: point.Point{Y: y + 1, X: x}, Parent: n, Action: "down"},
+		{State: point.Point{Y: y, X: x - 1}, Parent: n, Action: "left"},
+		{State: point.Point{Y: y, X: x + 1}, Parent: n, Action: "right"},
+	}
+
+	var neighbours []*node.Node
+
+	for _, c := range candidates {
+		if 0 <= c.State.Y && c.State.Y < d.Game.Height {
+			if 0 <= c.State.X && c.State.X < d.Game.Width {
+				if !d.Game.Walls[c.State.Y][c.State.X].IsSolid {
+					neighbours = append(neighbours, c)
+				}
+			}
+		}
+	}
+
+	// randomise order
+	for i := range neighbours {
+		j := rand.Intn(i + 1)
+		neighbours[i], neighbours[j] = neighbours[j], neighbours[i]
+	}
+
+	return neighbours
+}
+
+// ------------------------------------------------------------------------------------------------
+func (d *DepthFirstSearch) Solve() {
+	fmt.Println("Starting to solve maze using Depth First Search...")
+
+	d.Game.NumExplored = 0
+
+	start := node.Node{State: d.Game.Start, Parent: nil, Action: ""}
+
+	d.Add(&start)
+	d.Game.CurrentNode = &start
+
+	for {
+		if d.IsEmpty() {
+			return
+		}
+
+		currentNode, err := d.Remove()
+		if err != nil {
+			fmt.Println("Error solving maze:", err)
+			return
+		}
+
+		if d.Game.Debug {
+			fmt.Println("Removed", currentNode.State)
+			fmt.Println("-------")
+			fmt.Println()
+		}
+
+		d.Game.CurrentNode = currentNode
+		d.Game.NumExplored += 1
+
+		// check for solution found
+		if d.Game.End == currentNode.State {
+			var actions []string
+			var cells []point.Point
+			for {
+				if currentNode.Parent != nil {
+					actions = append(actions, currentNode.Action)
+					cells = append(cells, currentNode.State)
+					currentNode = currentNode.Parent
+				} else {
+					break
+				}
+			}
+
+			slices.Reverse(actions)
+			slices.Reverse(cells)
+
+			d.Game.Solution = solution.Solution{Actions: actions, Cells: cells}
+			d.Game.Explored = append(d.Game.Explored, currentNode.State)
+		}
+
+		d.Game.Explored = append(d.Game.Explored, currentNode.State)
+
+		for _, x := range d.Neighbours(currentNode) {
+
+		}
+	}
 }
