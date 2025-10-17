1d856afb17f7217ae13e3dd60559fed7dbdd0157
Ewald Horn <ewaldhorn@gmail.com>
2025-10-17 18:25:06 +0200

    Work on image generation

--- languages/go/courses/udemy/introduction_ai_go/part1/go.mod
+++ languages/go/courses/udemy/introduction_ai_go/part1/go.mod
@@ -1,3 +1,7 @@
 module mazes
 
 go 1.25.1
+
+require golang.org/x/image v0.32.0
+
+require github.com/StephaneBunel/bresenham v0.0.0-20211027152503-ec76d7b8e923 // indirect
---
+++ languages/go/courses/udemy/introduction_ai_go/part1/imager/image.go
@@ -0,0 +1,142 @@
+package imager
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"mazes/maze"
+	"mazes/point"
+	"mazes/wall"
+	"os"
+
+	"github.com/StephaneBunel/bresenham"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
+)
+
+// ------------------------------------------------------------------------------------------------
+const cellSize = 60
+
+// ------------------------------------------------------------------------------------------------
+var (
+	green     = color.RGBA{G: 255, A: 255}
+	darkGreen = color.RGBA{R: 1, G: 100, B: 32, A: 255}
+	red       = color.RGBA{R: 255, A: 255}
+	yellow    = color.RGBA{R: 255, G: 255, B: 101, A: 255}
+	gray      = color.RGBA{R: 125, G: 125, B: 125, A: 125}
+	orange    = color.RGBA{R: 255, G: 140, B: 25, A: 255}
+	blue      = color.RGBA{R: 14, G: 118, B: 173, A: 255}
+)
+
+// ------------------------------------------------------------------------------------------------
+func RenderMazeToDisk(m *maze.Maze, filename ...string) {
+	fmt.Printf("Generating image: `%s`\n", filename[0])
+
+	// calculate image dimensions
+	width := cellSize * (m.Width - 1)
+	height := cellSize * m.Height
+
+	// ensure filename is valid
+	var outFile = "image.png"
+	if len(filename) > 0 {
+		outFile = filename[0]
+	}
+
+	// set output dimensions
+	upLeft := image.Point{}
+	bottomRight := image.Point{X: width, Y: height}
+
+	// create output image
+	img := image.NewRGBA(image.Rectangle{Min: image.Point(upLeft), Max: image.Point(bottomRight)})
+	draw.Draw(img, img.Bounds(), &image.Uniform{C: color.Black}, image.Point{}, draw.Src)
+
+	// draw the actual blocks
+	drawBlocks(img, m)
+
+	// draw the grid
+	drawGrid(img, m)
+
+	f, _ := os.Create(outFile)
+	_ = png.Encode(f, img)
+}
+
+// ------------------------------------------------------------------------------------------------
+func drawBlocks(img *image.RGBA, m *maze.Maze) {
+	// Precompute expensive lookups
+	solutionSet := make(map[point.Point]bool)
+	exploredSet := make(map[point.Point]bool)
+
+	for _, p := range m.Solution.Cells {
+		solutionSet[p] = true
+	}
+
+	for _, p := range m.Explored {
+		exploredSet[p] = true
+	}
+
+	// now check and render each block
+	for i, row := range m.Walls {
+		for j, col := range row {
+			p := point.Point{X: j, Y: i}
+			x, y := j*cellSize, i*cellSize
+
+			var fillColor color.Color
+
+			switch {
+			case col.IsSolid:
+				fillColor = color.Black
+			case p.X == m.Start.X && p.Y == m.Start.Y:
+				fillColor = darkGreen
+			case p.X == m.End.X && p.Y == m.End.Y:
+				fillColor = red
+			case solutionSet[p]:
+				fillColor = green
+			case col.State == m.CurrentNode.State:
+				fillColor = orange
+			case exploredSet[p]:
+				fillColor = yellow
+			default:
+				fillColor = color.White
+			}
+
+			drawSquare(col, p, img, fillColor, cellSize, x, y)
+		}
+	}
+}
+
+// ------------------------------------------------------------------------------------------------
+func drawGrid(img *image.RGBA, m *maze.Maze) {
+	for y, _ := range m.Walls {
+		bresenham.DrawLine(img, 0, y*cellSize, m.Width*cellSize, y*cellSize, gray)
+	}
+
+	for x, _ := range m.Walls[0] {
+		bresenham.DrawLine(img, x*cellSize, 0, x*cellSize, m.Height*cellSize, gray)
+	}
+
+	bresenham.DrawLine(img, m.Width*cellSize-cellSize-1, 0, m.Width*cellSize-cellSize-1, m.Height*cellSize, gray)
+	bresenham.DrawLine(img, 0, m.Height*cellSize-1, m.Width*cellSize, m.Height*cellSize-1, gray)
+}
+
+// ------------------------------------------------------------------------------------------------
+func drawSquare(col wall.Wall, p point.Point, img *image.RGBA, c color.Color, size, x, y int) {
+	patch := image.NewRGBA(image.Rect(0, 0, size, size))
+
+	draw.Draw(patch, patch.Bounds(), &image.Uniform{C: c}, image.Point{}, draw.Src)
+
+	if !col.IsSolid {
+		printLocation(p, color.Black, patch)
+	}
+
+	draw.Draw(img, image.Rect(x, y, x+size, y+size), patch, image.Point{}, draw.Src)
+}
+
+// ------------------------------------------------------------------------------------------------
+func printLocation(p point.Point, c color.Color, patch *image.RGBA) {
+	point := fixed.Point26_6{X: fixed.I(6), Y: fixed.I(40)}
+	d := &font.Drawer{Dst: patch, Src: image.NewUniform(c), Face: basicfont.Face7x13, Dot: point}
+	d.DrawString(fmt.Sprintf("[%d %d]", p.X, p.Y))
+}
