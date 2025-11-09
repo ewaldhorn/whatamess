6ac3916cc2d75b5e20078377224a7c82d09716e5
Ewald Horn <ewaldhorn@gmail.com>
2025-11-09 16:33:50 +0200

    Getting some foundations in place

--- languages/typescript/experiments/uppity/src/game.ts
+++ languages/typescript/experiments/uppity/src/game.ts
@@ -1,11 +1,26 @@
 import globals from "./globals.ts";
 
 // ------------------------------------------------------------------------------------------------
+let ctx: CanvasRenderingContext2D | null;
+
+// ------------------------------------------------------------------------------------------------
 const setGameInformation = () => {
   document.title = `${globals.GAME_NAME} v${globals.VERSION}`;
 };
 
 // ------------------------------------------------------------------------------------------------
+const initCanvas = () => {
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  ctx = canvas.getContext("2d");
+};
+
+// ------------------------------------------------------------------------------------------------
 export const initGame = () => {
   setGameInformation();
+  initCanvas();
+
+  if (ctx) {
+  } else {
+    alert("Could not load 2D rendering context - unable to continue");
+  }
 };
