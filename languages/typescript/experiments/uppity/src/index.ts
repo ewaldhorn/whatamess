ead2fab825cd27d19e4dc8e94b98c2436d36cda7
Ewald Horn <ewaldhorn@gmail.com>
2025-12-01 09:13:14 +0200

    Adding some additional logging

--- languages/typescript/experiments/uppity/src/index.ts
+++ languages/typescript/experiments/uppity/src/index.ts
@@ -2,6 +2,7 @@ import { initGame } from "./game";
 
 // ------------------------------------------------------------------------------------------------
 window.addEventListener("DOMContentLoaded", () => {
+  console.log("Prelaunch message.");
   initGame();
   console.log("Should have rendered something.");
 });
