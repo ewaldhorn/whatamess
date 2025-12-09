ab62c6ee7fc3604bad2b1d42783d7ac1e8001f1c
Ewald Horn <ewaldhorn@gmail.com>
2025-12-09 22:35:22 +0200

    Bun and P5

---
+++ languages/typescript/with_p5/sound/index.ts
@@ -0,0 +1,17 @@
+import p5 from "p5";
+
+const sketch = function (p: p5) {
+  p.setup = function () {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background("black");
+    p.noLoop();
+  };
+
+  p.draw = function () {
+    p.strokeWeight(5);
+    p.fill("red");
+    p.rect(p.width / 2, p.height / 2, 100, 100);
+  };
+};
+
+new p5(sketch);
