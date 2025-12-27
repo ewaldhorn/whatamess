858a6bb8701e64aa792dfb4399be8b0fb5b6f846
Ewald Horn <ewaldhorn@gmail.com>
2025-12-27 21:38:44 +0200

    Almost done

--- languages/go/courses/udemy/go_ebitengine/section2/asteroids/src/game.go
+++ languages/go/courses/udemy/go_ebitengine/section2/asteroids/src/game.go
@@ -1,10 +1,8 @@
 package main
 
-import (
-	"github.com/hajimehoshi/ebiten/v2"
-)
+import "github.com/hajimehoshi/ebiten/v2"
 
-// ----------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 const (
 	GAME_VERSION = "0.01"
 	GAME_NAME    = "PEW PEWSTER"
@@ -12,43 +10,32 @@ const (
 	GAME_HEIGHT  = 800
 )
 
-// ----------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 type Game struct {
 	sceneManager *SceneManager
 	input        Input
-	player       *Player
 }
 
-// ----------------------------------------------------------------------------
-func NewGame() *Game {
-	g := &Game{}
-
-	g.player = NewPlayer(g)
-
-	return g
-}
-
-// ----------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 func (g *Game) Update() error {
 	if g.sceneManager == nil {
 		g.sceneManager = &SceneManager{}
-		// g.sceneManager.GoToScene(NewGameScence())
+		g.sceneManager.GoToScene(NewGameScene())
 	}
 
 	g.input.Update()
 	if err := g.sceneManager.Update(&g.input); err != nil {
 		return err
 	}
-
 	return nil
 }
 
-// ----------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.sceneManager.Draw(screen)
 }
 
-// ----------------------------------------------------------------------------
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+// ------------------------------------------------------------------------------------------------
+func (g *Game) Layout(_, _ int) (screenWidth, screenHeight int) {
 	return GAME_WIDTH, GAME_HEIGHT
 }
