54bd6a28203566252124823c1e3aca634215cc51
Ewald Horn <ewaldhorn@gmail.com>
2026-08-08 15:41:05 +0200

    Get javascript ready, load wasm

--- languages/odin/experiments/wasm_buffer/web/index.js
+++ languages/odin/experiments/wasm_buffer/web/index.js
@@ -1,1 +1,22 @@
-alert("Yo")
+let mem;
+
+const importObject = {
+  app_env: {
+    app_init: (w, h) => {
+      const cv = document.getElementById("app").appendChild(document.createElement("canvas"));
+      cv.width = w;
+      cv.height = h;
+      window._ctx = cv.getContext("2d");
+      window._ctx.fillStyle="#339933";
+      window._ctx.fillRect(0,0,w,h);
+    },
+    update_update: (a) => {
+      console.log("Got back:", a);
+    },
+  },
+};
+
+WebAssembly.instantiateStreaming(fetch("application.wasm"), importObject).then((wasm) => {
+  mem = wasm.instance.exports.memory;
+  wasm.instance.exports.bootup();   // JS -> Odin
+});
